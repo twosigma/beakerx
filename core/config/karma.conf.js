aac03d5d90b53a1b4748785ddc7c4b05a7c6afbf
anglee <ang.lee@gmail.com>
2014-02-13 11:09:38 -0500

    moved unit test to a 'unit' folder

--- core/config/karma.conf.js
+++ core/config/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
             'src/main/web/vendor/bower_components/angular-mocks/angular-mocks.js',
             'src/main/web/vendor/bower_components/underscore/underscore.js',
             'src/main/web/js/module/bkNotebookCellModelManager.js',
-            'test/main/web/js/module/bkNotebookCellModelManager.spec.js'
+            'test/unit/main/web/js/module/bkNotebookCellModelManager.spec.js'
         ],
 
 

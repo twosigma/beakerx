01e02081c434558563cec740c3686895a13f2ae7
anglee <ang.lee@gmail.com>
2014-02-13 14:41:12 -0500

    Initial impl of bkNotebookVersionManager including logic to convert v1 notebook to v2

--- core/src/main/web/js/module/bkNotebookVersionManager.js
+++ core/src/main/web/js/module/bkNotebookVersionManager.js
@@ -19,11 +19,174 @@
  */
 (function () {
     'use strict';
+
+    var bkNbV1Converter = (function () {
+        // in v1, cell level by definition is the count of steps away from "root" in the tree
+        var getSectionCellLevel = function (cell, tagMap) {
+            var getParentId = function (cId) {
+                var pId = null;
+                _(tagMap).find(function (v, k) {
+                    if (_(v).contains(cId)) {
+                        pId = k;
+                        return true;
+                    }
+                });
+                return pId;
+            };
+            var level = 0;
+            var parentId = getParentId(cell.id);
+            while (parentId) {
+                ++level;
+                parentId = getParentId(parentId);
+            }
+            return level;
+        };
+        var convertCodeCell = function (cell) {
+            return {
+                "id": cell.id,
+                "type": "code",
+                "evaluator": cell.evaluator,
+                "input": cell.input,
+                "output": cell.output
+            };
+        };
+        var convertSectionCell = function (cell, tagMap) {
+            return {
+                "id": cell.id,
+                "type": "section",
+                "level": getSectionCellLevel(cell, tagMap),
+                "title": cell.title,
+                "collapsed": cell.collapsed
+            };
+        };
+        var convertTextCell = function (cell) {
+            return {
+                "id": cell.id,
+                "type": "text",
+                "body": cell.body
+            }
+        };
+        var convertMarkdownCell = function (cell) {
+            return {
+                "id": cell.id,
+                "type": "markdown",
+                "body": cell.body,
+                "mode": cell.mode
+            }
+        };
+        var convertCell = function (cell, tagMap, tagMap2) {
+            var retCell;
+            switch (cell.class[0]) {
+                case "code":
+                    retCell = convertCodeCell(cell);
+                    break;
+                case "section":
+                    retCell = convertSectionCell(cell, tagMap);
+                    break;
+                case "text":
+                    retCell = convertTextCell(cell);
+                    break;
+                case "markdown":
+                    retCell = convertMarkdownCell(cell);
+                    break;
+            }
+            if (_(tagMap2.initialization).contains(cell.id)) {
+                retCell.initialization = true;
+            }
+            return retCell;
+        };
+        var getCellIds = function (cells, tagMap) {
+            var cellIds = [];
+            var cId, children;
+            var stack = ["root"];
+            while (!_.isEmpty(stack)) {
+                cId = stack.pop();
+                cellIds.push(cId);
+                if (tagMap.hasOwnProperty(cId)) {
+                    children = _(tagMap[cId]).clone();
+                    if (!_(children).isEmpty()) {
+                        stack = stack.concat(children.reverse());
+                    }
+                }
+            }
+            return cellIds;
+        };
+        var generateCellMap = function (cells) {
+            var cellMap = {};
+            cells.forEach(function (cell) {
+                cellMap[cell.id] = cell;
+            });
+            return cellMap;
+        };
+        var convertCells = function (cells, tagMap, tagMap2) {
+            var cellIds = getCellIds(cells, tagMap);
+            var cellMap = generateCellMap(cells);
+            var v2Cells = _(cellIds).chain()
+                .filter(function (id) {
+                    return id !== "root";
+                })
+                .map(function (id) {
+                    return cellMap[id];
+                })
+                .map(function (cell) {
+                    return convertCell(cell, tagMap, tagMap2);
+                })
+                .value();
+            return v2Cells;
+        };
+
+        return {
+            convert: function (notebookV1) {
+                var notebookV2 = {
+                    beaker: "2",
+                    evaluators: notebookV1.evaluators,
+                    cells: convertCells(notebookV1.cells, notebookV1.tagMap, notebookV1.tagMap2),
+                    locked: notebookV1.locked
+                };
+                return notebookV2;
+            }
+        };
+    })();
+
+
     var module = angular.module('M_bkNotebookVersionManager', []);
     module.factory('bkNotebookVersionManager', function () {
         return {
             open: function (notebook) {
-                return "notebook opened";
+                if (_.isEmpty(notebook)) {
+                    return {
+                        "beaker": "2",
+                        "evaluators": [],
+                        "cells": []
+                    };
+                }
+                // if notebook is a string, parse it to js object
+                if (angular.isString(notebook)) {
+                    try {
+                        notebook = angular.fromJson(notebook);
+                        // TODO, to be removed. Load a corrupted notebook.
+                        if (angular.isString(notebook)) {
+                            notebook = angular.fromJson(notebook);
+                        }
+                    } catch (e) {
+                        console.error(e);
+                        console.error("This is not a valid Beaker notebook JSON");
+                        console.error(notebook);
+                        window.alert("Not a valid Beaker notebook");
+                        return;
+                    }
+                }
+
+                //check version and see if need conversion
+                if (notebook.beaker === "1") {
+                    notebook = bkNbV1Converter.convert(notebook);
+                } else if (notebook.beaker === "2"){
+                    // good, "2" is the current version
+                } else {
+                    throw "Unknown Beaker notebook version";
+                }
+
+                return notebook;
             }
         };
     });
--- core/test/unit/main/web/js/module/bkNotebookCersionManager.spec.js
+++ core/test/unit/main/web/js/module/bkNotebookCersionManager.spec.js
@@ -20,9 +20,162 @@ describe("M_bkNotebookVersionManager", function() {
         bkNotebookVersionManager = _bkNotebookVersionManager_;
     }));
 
+    var bkNbV1 = {
+        "beaker": "1",
+        "evaluators": [
+            {
+                "name": "Html",
+                "plugin": "./plugin/evaluator/html.js",
+                "shellID": null
+            },
+            {
+                "name": "Latex",
+                "plugin": "./plugin/evaluator/latex.js",
+                "shellID": null
+            },
+            {
+                "name": "IPython",
+                "plugin": "IPython",
+                "shellID": "595CD9D6B74B4CD88C073D44E5CB6DAF",
+                "imports": "",
+                "supplementalClassPath": ""
+            },
+            {
+                "name": "JavaScript",
+                "plugin": "JavaScript",
+                "shellID": null,
+                "jsSetting2": "",
+                "jsSetting1": ""
+            }
+        ],
+        "cells": [
+            {
+                "id": "root",
+                "class": [
+                    "notebook",
+                    "container"
+                ]
+            },
+            {
+                "id": "section001",
+                "class": [
+                    "section",
+                    "container"
+                ],
+                "title": "Hello Beaker",
+                "collapsed": false
+            },
+            {
+                "id": "code001",
+                "class": [
+                    "code"
+                ],
+                "evaluator": "IPython",
+                "input": {
+                    "body": ""
+                },
+                "output": {
+                    "selectedType": "Hidden"
+                }
+            }
+        ],
+        "tagMap": {
+            "root": [
+                "section001"
+            ],
+            "section001": [
+                "code001"
+            ]
+        },
+        "tagMap2": {
+            "initialization": ["code001"],
+            "IPython": [
+                "code001"
+            ]
+        }
+    };
+    var bkNbV2 = {
+        "beaker": "2",
+        "evaluators": [
+            {
+                "name": "Html",
+                "plugin": "./plugin/evaluator/html.js",
+                "shellID": null
+            },
+            {
+                "name": "Latex",
+                "plugin": "./plugin/evaluator/latex.js",
+                "shellID": null
+            },
+            {
+                "name": "IPython",
+                "plugin": "IPython",
+                "shellID": "595CD9D6B74B4CD88C073D44E5CB6DAF",
+                "imports": "",
+                "supplementalClassPath": ""
+            },
+            {
+                "name": "JavaScript",
+                "plugin": "JavaScript",
+                "shellID": null,
+                "jsSetting2": "",
+                "jsSetting1": ""
+            }
+        ],
+        "cells": [
+            {
+                "id": "section001",
+                "type": "section",
+                "level": 1,
+                "title": "Hello Beaker",
+                "collapsed": false
+            },
+            {
+                "id": "code001",
+                "type": "code",
+                "evaluator": "IPython",
+                "input": {
+                    "body": ""
+                },
+                "output": {
+                    "selectedType": "Hidden"
+                },
+                initialization: true
+            }
+        ]
+    };
+    var emptyNotebook = {
+        "beaker": "2",
+        "evaluators": [],
+        "cells": []
+    };
+
     describe("bkNotebookVersionManager", function () {
-        it("should open a notebook", function () {
-            expect(bkNotebookVersionManager.open()).toBe("notebook opened");
+        it("should open a v1 notebook", function () {
+            var bkNb = bkNotebookVersionManager.open(bkNbV1);
+            expect(bkNb.beaker).toBe("2");
+            expect(bkNb.evaluators).toEqual(bkNbV1.evaluators);
+            expect(bkNb).toEqual(bkNbV2);
+        });
+
+        it("should open a v2 notebook", function () {
+            expect(bkNotebookVersionManager.open(bkNbV2)).toBe(bkNbV2);
+        });
+
+        it("should open a notebook JSON", function () {
+            expect(bkNotebookVersionManager.open(angular.toJson(bkNbV2))).toEqual(bkNbV2);
+            expect(bkNotebookVersionManager.open(angular.toJson(angular.toJson(bkNbV2)))).toEqual(bkNbV2);
+        });
+
+        it("should open a empty notebook", function () {
+            expect(bkNotebookVersionManager.open()).toEqual(emptyNotebook);
+            expect(bkNotebookVersionManager.open("")).toEqual(emptyNotebook);
+            expect(bkNotebookVersionManager.open({})).toEqual(emptyNotebook);
+        });
+
+        it("should throw when version is not recognizable", function () {
+            expect(function () { bkNotebookVersionManager.open({beaker: -1}) })
+                .toThrow("Unknown Beaker notebook version");
         });
     });
 });

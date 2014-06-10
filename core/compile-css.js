597621bc4bc48e08fa7b1cccc3f27c55e2235257
Sam Saccone <samccone@gmail.com>
2014-06-10 16:37:42 -0400

    Replace app.css with app.scss

--- core/compile-css.js
+++ core/compile-css.js
@@ -1,11 +1,12 @@
 var fs        = require('fs');
 var sass      = require('node-sass');
-var path      = require('path');
+var Path      = require('path');
 
-var rootPath  = path.join(__dirname, "/src/main/web/app/");
+var rootPath  = Path.join(__dirname, "/src/main/web/app/");
 
 var css = sass.renderSync({
-  data: fs.readFileSync(path.join(rootPath, "app.scss"), "utf8")
+  data: fs.readFileSync(Path.join(rootPath, "app.scss"), "utf8"),
+  includePaths: [rootPath]
 });
 
 fs.writeFileSync(path.join(rootPath, "app.css"), css);

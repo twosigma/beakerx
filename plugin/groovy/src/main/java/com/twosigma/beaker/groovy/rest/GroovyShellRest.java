8b98ce168941b4614c1f1e6c385132bd707e1cd8
Scott Draves <spot@draves.org>
2014-07-28 16:15:19 -0400

    remove unnecessary custom class loader

--- plugin/groovy/src/main/java/com/twosigma/beaker/groovy/rest/GroovyShellRest.java
+++ plugin/groovy/src/main/java/com/twosigma/beaker/groovy/rest/GroovyShellRest.java
@@ -120,14 +120,8 @@ public class GroovyShellRest {
       @FormParam("imports") String classPathes) {
   }
 
-  private void newEvaluator(String id)
-    throws MalformedURLException
-  {
-    URLClassLoader classLoader = new URLClassLoader(new URL[] {
-	// XXX use relative or config to find
-        new URL("file:///Users/spot/beaker-notebook/plugin/jvm/build/libs/jvm.jar")});
-
-    this.shells.put(id, new GroovyShell(classLoader));
+  private void newEvaluator(String id) {
+    this.shells.put(id, new GroovyShell());
   }
 
   private GroovyShell getEvaluator(String shellId) {

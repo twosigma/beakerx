5919aa0aaf96105d2a8d6bcd5c503d31278f1167
Sam Saccone <samccone@gmail.com>
2014-07-31 11:40:34 -0400

    Add template precompile step

--- core/gulpfile.js
+++ core/gulpfile.js
@@ -1,6 +1,9 @@
 var gulp      = require('gulp');
 var sass      = require('gulp-sass');
+var template  = require('gulp-template-compile');
+var concat    = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
+
 var Path      = require('path');
 var rootPath  = Path.join(__dirname, "/src/main/web/app/");
 
@@ -8,14 +11,33 @@ function handleError(e) {
   console.log('\u0007', e.message);
 }
 
-gulp.task("compile", function() {
+gulp.task("compileScss", function() {
   gulp.src(Path.join(rootPath, "app.scss"))
   .pipe(sass().on('error', handleError))
   .pipe(minifyCSS())
   .pipe(gulp.dest(rootPath))
 });
 
-gulp.task("watch", function() {
-  var watchPath = rootPath + "/**/*.scss";
-  gulp.watch(watchPath, ["compile"])
+gulp.task("compileTemplates", function() {
+  gulp.src(rootPath + "/template/**/*.html")
+  .pipe(template({
+    name: function (file) {
+      return file.relative.split(".")[0];
+    }
+  }))
+  .pipe(concat('templates.js'))
+  .pipe(gulp.dest(rootPath));
 });
+
+gulp.task("watchScss", function() {
+  var watchPath = rootPath + "/**/*.scss";
+  gulp.watch(watchPath, ["compileScss"])
+});
+
+gulp.task("watchTemplates", function() {
+  var watchPath = rootPath + "/template/**/*.html";
+  gulp.watch(watchPath, ["compileTemplates"])
+});
+
+gulp.task("watch", ["watchScss", "watchTemplates"]);
+gulp.task("compile", ["compileScss", "compileTemplates"]);

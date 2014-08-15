twosigma/beakerx

commit a4146e8b3d40fb104e1834270b8eb66b73a23452

Extract inline menu html

core/src/main/web/app/template/opennotebook.html
@@ -1,0 +1,33 @@
+<!--
+
+  Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+<div class="modal-header">
+   <h1>Open <span ng-show="getStrategy().treeViewfs.showSpinner"><i class="fa fa-refresh fa-spin"></i></span></h1>
+</div>
+<div class="modal-body">
+   <tree-view rooturi="/" fs="getStrategy().treeViewfs"></tree-view>
+   <tree-view rooturi="<%- homedir %>" fs="getStrategy().treeViewfs"></tree-view>
+</div>
+<div class="modal-footer">
+"   <div class=text-left>Enter a file path (e.g. /Users/...) or URL (e.g. http://...):</div>"
+   <p><input id="openFileInput" class="input-xxlarge" ng-model="getStrategy().input" ng-keypress="getStrategy().close($event, close)" focus-start /></p>
+   <span style="float:left;">
+     <input type="checkbox" style="vertical-align:top;" ng-model="getStrategy().treeViewfs.applyExtFilter">
+     <span ng-click="getStrategy().treeViewfs.applyExtFilter = !getStrategy().treeViewfs.applyExtFilter">show .bkr files only</span>
+   </span>
+   <button ng-click="close()" class="btn">Cancel</button>
+   <button ng-click="close(getStrategy().getResult())" class="btn btn-primary">Open</button>
+</div>

core/src/main/web/plugin/menu/controlpanel.js
@@ -37,20 +37,6 @@ define(function(require, exports, module) {
 
   var menuItemsDeferred = bkHelper.newDeferred();
   bkHelper.getHomeDirectory().then(function(homeDir) {
-    var treeViewChooserTemplate = '<div class="modal-header">' +
-        '   <h1>Open <span ng-show="getStrategy().treeViewfs.showSpinner"><i class="fa fa-refresh fa-spin"></i></span></h1>' +
-        '</div>' +
-        '<div class="modal-body">' +
-        '   <tree-view rooturi="/" fs="getStrategy().treeViewfs"></tree-view>' +
-        '   <tree-view rooturi="' + homeDir + '" fs="getStrategy().treeViewfs"></tree-view>' +
-        '</div>' +
-        '<div class="modal-footer">' +
-        "   <div class='text-left'>Enter a file path (e.g. /Users/...) or URL (e.g. http://...):</div>" +
-        '   <p><input id="openFileInput" class="input-xxlarge" ng-model="getStrategy().input" ng-keypress="getStrategy().close($event, close)" focus-start /></p>' +
-        '   <span style="float:left;"><input type="checkbox" style="vertical-align:top;" ng-model="getStrategy().treeViewfs.applyExtFilter"> show .bkr files only</span>' +
-        '   <button ng-click="close()" class="btn">Cancel</button>' +
-        '   <button ng-click="close(getStrategy().getResult())" class="btn btn-primary">Open</button>' +
-        '</div>';
     var toAdd = [
       { parent: "File", items: fileMenuItems },
       {
@@ -65,7 +51,7 @@ define(function(require, exports, module) {
                   function(originalUrl) {
                     bkHelper.openNotebook(originalUrl);
                   },
-                  treeViewChooserTemplate,
+                  JST['openmenumodal']({homeDir: homeDir}),
                   bkHelper.getFileSystemFileChooserStrategy()
               );
             }

core/src/main/web/plugin/menu/file_filesys.js
@@ -33,23 +33,7 @@ define(function(require, exports, module) {
             action: function() {
               bkHelper.showModalDialog(
                   bkHelper.openNotebook,
-                  '<div class="modal-header">' +
-                      '   <h1>Open <span ng-show="getStrategy().treeViewfs.showSpinner"><i class="fa fa-refresh fa-spin"></i></span></h1>' +
-                      '</div>' +
-                      '<div class="modal-body">' +
-                      '   <tree-view rooturi="/" fs="getStrategy().treeViewfs"></tree-view>' +
-                      '   <tree-view rooturi="' + homeDir + '" fs="getStrategy().treeViewfs"></tree-view>' +
-                      '</div>' +
-                      '<div class="modal-footer">' +
-                      "   <div class='text-left'>Enter a file path (e.g. /Users/...) or URL (e.g. http://...):</div>" +
-                      '   <p><input id="openFileInput" class="input-xxlarge" ng-model="getStrategy().input" ng-keypress="getStrategy().close($event, close)" focus-start /></p>' +
-                      '   <span style="float:left;">' +
-                      '     <input type="checkbox" style="vertical-align:top;" ng-model="getStrategy().treeViewfs.applyExtFilter">' +
-                      '     <span ng-click="getStrategy().treeViewfs.applyExtFilter = !getStrategy().treeViewfs.applyExtFilter">show .bkr files only</span>' +
-                      '   </span>' +
-                      '   <button ng-click="close()" class="btn">Cancel</button>' +
-                      '   <button ng-click="close(getStrategy().getResult())" class="btn btn-primary">Open</button>' +
-                      '</div>', // template
+                  JST['openmenumodal']({homeDir: homeDir}),
                   bkHelper.getFileSystemFileChooserStrategy()
               );
             }

core/src/main/web/app/helpers/core.js
@@ -410,7 +410,7 @@
   module.directive('fileActionDialog', function() {
     return {
       scope: { actionName: '@', inputId: '@', close: '=' },
-      templateUrl: "./app/template/fileactiondialog.html",
+      template: JST['fileactiondialog'](),
       link: function(scope, element, attrs) {
         element.find('input').focus();
       }

commit 2a117bd1a34583bfa5e96d7526dd1e1cfa6d914d

Fix file open dialogs regressed in a4146e8

core/src/main/web/app/template/opennotebook.html
@@ -22,7 +22,7 @@
    <tree-view rooturi="<%- homedir %>" fs="getStrategy().treeViewfs"></tree-view>
 </div>
 <div class="modal-footer">
-"   <div class=text-left>Enter a file path (e.g. /Users/...) or URL (e.g. http://...):</div>"
+   <div class=text-left>Enter a file path (e.g. /Users/...) or URL (e.g. http://...):</div>
    <p><input id="openFileInput" class="input-xxlarge" ng-model="getStrategy().input" ng-keypress="getStrategy().close($event, close)" focus-start /></p>
    <span style="float:left;">
      <input type="checkbox" style="vertical-align:top;" ng-model="getStrategy().treeViewfs.applyExtFilter">

core/src/main/web/plugin/menu/controlpanel.js
@@ -51,7 +51,7 @@ define(function(require, exports, module) {
                   function(originalUrl) {
                     bkHelper.openNotebook(originalUrl);
                   },
-                  JST['openmenumodal']({homeDir: homeDir}),
+                  JST['opennotebook']({homedir: homeDir}),
                   bkHelper.getFileSystemFileChooserStrategy()
               );
             }

core/src/main/web/plugin/menu/file_filesys.js
@@ -33,7 +33,7 @@ define(function(require, exports, module) {
             action: function() {
               bkHelper.showModalDialog(
                   bkHelper.openNotebook,
-                  JST['openmenumodal']({homeDir: homeDir}),
+                  JST['opennotebook']({homedir: homeDir}),
                   bkHelper.getFileSystemFileChooserStrategy()
               );
             }
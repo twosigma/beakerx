8844f5bda386876747a74bb5d03f5354c2230da0
Scott Draves <spot@draves.org>
2014-09-11 22:59:17 -0400

    ctrl-n and shift-ctrl-n in control panel open new notebooks

--- core/src/main/web/app/mainapp/mainapp.js
+++ core/src/main/web/app/mainapp/mainapp.js
@@ -370,6 +370,9 @@
             getNotebookModel: function() {
               return bkSessionManager.getRawNotebookModel();
             },
+            newNotebook: function() {
+		console.log("new notebook");
+            },
             saveNotebook: function() {
               saveStart();
               var thenable;
@@ -519,6 +522,11 @@
             e.preventDefault();
             _impl.saveNotebook();
             return false;
+// this is for the notebook lets start with control panel
+//          } else if (e.ctrlKey && e.which === 78) { // Ctrl + n
+//	    e.preventDefault();
+//	    _impl.newNotebook();
+//	    return false;
           } else if (e.ctrlKey && e.which === 90) { // Ctrl + z
             bkUtils.fcall(function() {
               bkSessionManager.undo();

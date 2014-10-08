1f9fb6b7a04fed08f550d4b0b015de1534e46638
Davide Pasetto <dpasetto69@gmail.com>
2014-10-08 10:18:12 -0400

    More formatting fix plus some error checking

--- core/src/main/web/app/helpers/helper.js
+++ core/src/main/web/app/helpers/helper.js
@@ -279,18 +279,18 @@
       updateDocumentModelFromDOM: function(id) {
           // 1) find the cell that contains elem
           var elem = $("#" + id).closest("bk-cell");
-          if (elem === undefined) {
-            console.log("ERROR: cannot find an Html cell containing the element"+id+".");
+          if (elem === undefined || elem[0] === undefined) {
+            console.log("ERROR: cannot find an Html cell containing the element '" + id + "'.");
             return;
           }
           var cellid = elem[0].getAttribute("cellid");
           if (cellid === undefined) {
-            console.log("ERROR: cannot find an Html cell containing the element"+id+".");
+            console.log("ERROR: cannot find an Html cell containing the element '" + id + "'.");
             return;
           }
           var body = elem.find( "bk-output-display[type='Html'] div div" );
-          if (body === undefined) {
-            console.log("ERROR: cannot find an Html cell containing the element"+id+".");
+          if (body === undefined || body[0] === undefined) {
+            console.log("ERROR: cannot find an Html cell containing the element '" + id + "'.");
             return;
           }
 
@@ -300,7 +300,7 @@
           // 3) set the result.object to that string.
           var cell = bkCoreManager.getNotebookCellManager().getCell(cellid);          
           if (cell === undefined) {
-            console.log("ERROR: cannot find an Html cell containing the element"+id+".");
+            console.log("ERROR: cannot find an Html cell containing the element '" + id + "'.");
             return;
           }
 
@@ -308,7 +308,7 @@
           if (res.innertype === "Html") {
             res.object = newOutput;
           } else {
-            console.log("ERROR: cannot find an Html cell containing the element"+id+".");
+            console.log("ERROR: cannot find an Html cell containing the element '" + id + "'.");
           }
       },
 

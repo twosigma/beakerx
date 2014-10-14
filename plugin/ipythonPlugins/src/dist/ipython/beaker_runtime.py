65589f66f4be520d6f8f9a2d48d77ed8d7ac8c64
Scott Draves <spot@draves.org>
2014-10-14 11:53:12 -0400

    sys module not needed anymore

--- plugin/ipythonPlugins/src/dist/ipython/beaker_runtime.py
+++ plugin/ipythonPlugins/src/dist/ipython/beaker_runtime.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys, os, urllib, urllib2, json, pandas, yaml
+import os, urllib, urllib2, json, pandas, yaml
 
 # should be inner class to Beaker
 class DataFrameEncoder(json.JSONEncoder):

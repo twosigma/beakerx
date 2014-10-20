a7d8e5212add80395418e5e6665df94103ad0519
Davide Pasetto <dpasetto69@gmail.com>
2014-10-20 14:20:37 -0400

    transform only cell body

--- core/src/main/web/app/utils/basic/angularutils.js
+++ core/src/main/web/app/utils/basic/angularutils.js
@@ -29,33 +29,41 @@
         $rootScope.$$phase || $rootScope.$apply();
       },
       toPrettyJson: function(angularBoundJsObj) {
+        if(angularBoundJsObj.cells !== undefined) {
+          for (var i=0; i < angularBoundJsObj.cells.length; i++) {
+            if (angularBoundJsObj.cells[i].body !== undefined && typeof angularBoundJsObj.cells[i].body === "string") {
+              angularBoundJsObj.cells[i].body = angularBoundJsObj.cells[i].body.split("\n");
+            }
+            if (angularBoundJsObj.cells[i].input !== undefined && angularBoundJsObj.cells[i].input.body !== undefined && typeof angularBoundJsObj.cells[i].input.body === "string") {
+              angularBoundJsObj.cells[i].input.body = angularBoundJsObj.cells[i].input.body.split("\n");
+            }
+          }
+        }
         function cleanup(key, value) {
           if (key === '$$hashKey') return undefined;
-          if (key === "body" && typeof value === "string") {
-            return value.split("\n");
-          }
           return value;
         };
-        return JSON.stringify(angularBoundJsObj, cleanup, 4) + "\n";
+        var ret = JSON.stringify(angularBoundJsObj, cleanup, 4) + "\n";
+        this.removeStringArrays(angularBoundJsObj);
+        return ret;
       },
-      removeStringArrays: function(th,obj) {
-        $.each(obj, function(key, value) {
-          if ($.isPlainObject(value) && value.body !== undefined && $.isArray(value.body)) {
-            console.log("replace it");
-            var separator = '\n';
-            var str = value.body.join([separator]);
-            value.body = str;
-            return true;
+      removeStringArrays: function(obj) {
+        if(obj.cells !== undefined) {
+          for (var i=0; i < obj.cells.length; i++) {
+            if (obj.cells[i].body !== undefined && $.isArray(obj.cells[i].body)) {
+              var separator = '\n';
+              obj.cells[i].body = obj.cells[i].body.join([separator]);
+            }
+            if (obj.cells[i].input !== undefined && obj.cells[i].input.body !== undefined && $.isArray(obj.cells[i].input.body)) {
+              var separator = '\n';
+              obj.cells[i].input.body = obj.cells[i].input.body.join([separator]);
+            }
           }
-          if (value !== null && ($.isPlainObject(value) || $.isArray(value))) {
-            th.removeStringArrays(th,value);
-          }
-          return true;
-        });  
+        }
       },
       fromPrettyJson: function(jsonString) {
           var ret = angular.fromJson(jsonString);
-          this.removeStringArrays(this,ret);
+          this.removeStringArrays(ret);
           return ret;
       },
       httpGet: function(url, data) {

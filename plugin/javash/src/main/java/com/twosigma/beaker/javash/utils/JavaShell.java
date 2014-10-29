f2c6e59e6a3ace051c8ffdcd4b727eee2ac2b52f
Davide Pasetto <dpasetto69@gmail.com>
2014-10-29 16:28:10 -0400

    execute basic java code

--- plugin/javash/src/main/java/com/twosigma/beaker/javash/utils/JavaShell.java
+++ plugin/javash/src/main/java/com/twosigma/beaker/javash/utils/JavaShell.java
@@ -15,19 +15,26 @@
  */
 package com.twosigma.beaker.javash.utils;
 import java.util.List;
+import java.util.Arrays;
 import com.twosigma.beaker.jvm.object.SimpleEvaluationObject;
 import org.abstractmeta.toolbox.compilation.compiler.JavaSourceCompiler;
 import org.abstractmeta.toolbox.compilation.compiler.impl.JavaSourceCompilerImpl;
 import java.lang.reflect.*;
+import java.nio.file.*;
+import java.nio.file.FileSystems;
 
 public class JavaShell {
   private String shellId;
-  private String classPath;
-  private String imports;
+  private String packageId;
+  private List<String> classPath;
+  private List<String> imports;
   private String outDir;
-
+  private JavaSourceCompiler javaSourceCompiler;
+  
   public JavaShell(String id) {
     shellId = id;
+    javaSourceCompiler = new JavaSourceCompilerImpl();
+    packageId = "com.twosigma.beaker.javash."+shellId.split("-")[0];
   }
 
   public String getShellId() { return shellId; }
@@ -37,26 +44,52 @@ public class JavaShell {
   public void exit() { }
 
   public void setShellOptions(String cp, String in, String od) {
-    classPath = cp;
-    imports = in;
+    classPath = Arrays.asList(cp.split("\\s+"));
+    imports = Arrays.asList(in.split("\\s+"));
     outDir = od;
-    // DIPO HERE update?
+    if(outDir==null || outDir.isEmpty()) {
+      outDir = "/tmp/pippero"; //Files.createTempDirectory(java.nio.file.Filesystems.getDefault().getPath("/tmp"),"javash").toString();
+    }
     System.err.println("   classp "+classPath);
     System.err.println("   imports "+imports); 
     System.err.println("   outdir "+outDir); 
   }
 
+
+  /*
+   * 1) check if the code contains package declaration
+   *   -> YES -> write package declaration to out code -> remove everything before and including the package declaration
+   *   -> NO -> write default package declaration to out code
+   * 2) add default imports to out code
+   * 3) check if code contains import statements
+   *    -> YES -> add imports to out code -> remove everything before and including the last import
+   * 5) check if code contains a class declaration
+   *   -> NO -> wrap code in default class
+   *         -> execute code
+   *   -> YES -> compile code
+   *
+   *
+   * "(^|\\n)\\s*package\\s+([a-zA-Z]\\w*)(\\.[a-zA-Z]\\w*)*;"
+   *
+   */
+
+  
   public void evaluate(SimpleEvaluationObject seo, String code) {
-    JavaSourceCompiler javaSourceCompiler = new JavaSourceCompilerImpl();
     JavaSourceCompiler.CompilationUnit compilationUnit = javaSourceCompiler.createCompilationUnit();
-    String shellId = "ABC";
-    String javaSourceCode =  "package com.twosigma.beaker.javash."+shellId+";\n" +
+
+    compilationUnit.addClassPathEntries(classPath);
+
+    
+    
+    
+    String javaSourceCode =  "package "+packageId+";\n" +
       "public class Foo {\n" +
       "        public static Object beakerRun() {\n" + code +
       "        }\n" +
       "    }";
     System.err.println("code: "+javaSourceCode);
-    compilationUnit.addJavaSource("com.twosigma.beaker.javash."+shellId+".Foo", javaSourceCode);
+    
+    compilationUnit.addJavaSource(packageId+".Foo", javaSourceCode);
     try {
       ClassLoader classLoader = javaSourceCompiler.compile(compilationUnit);
       Class fooClass = classLoader.loadClass("com.twosigma.beaker.javash."+shellId+".Foo");

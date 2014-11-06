e497ccd68f1fc995e75101dc3ac558b87926671b
Davide Pasetto <dpasetto69@gmail.com>
2014-11-06 11:46:43 -0500

    implemented new test system

---
+++ test/tests/landingpageMenu.js
@@ -0,0 +1,35 @@
+describe('beaker landing menu test', function() {
+  var mainmenu = element.all(by.repeater('m in getMenus()'));
+  var submenu = element.all(by.repeater('item in getMenuItems() | isHidden')).filter(function(e,i) { return e.isDisplayed(); });
+  
+  it('should load', function() {
+    browser.get('http://localhost:8801/');
+    browser.waitForAngular();
+  });
+
+  it('should have title and menu', function() {
+    expect(browser.getTitle()).toEqual('Beaker');
+    expect(mainmenu.count()).toEqual(3);
+    expect(mainmenu.getText()).toEqual(['File', 'Settings', 'Help']);
+  });
+  
+  it('should have no menu displayed', function () {
+    expect(submenu.count()).toEqual(0);
+  });
+
+  it('File menu should have 3 items', function () {
+    mainmenu.get(0).click();
+    expect(submenu.count()).toEqual(3);
+  });
+
+  it('Settings menu should have 1 item', function () {
+    mainmenu.get(1).click();
+    expect(submenu.count()).toEqual(1);
+  });
+
+  it('Help menu should have 4 items', function () {
+    mainmenu.get(2).click();
+    expect(submenu.count()).toEqual(4);
+  });
+
+});

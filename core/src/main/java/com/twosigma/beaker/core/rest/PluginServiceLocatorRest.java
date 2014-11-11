59de18f3e0dcaded747dc62fe0a2cd75ffc55ed1
Davide Pasetto <dpasetto69@gmail.com>
2014-11-11 12:11:43 -0500

    Final fix for Windows version

--- core/src/main/java/com/twosigma/beaker/core/rest/PluginServiceLocatorRest.java
+++ core/src/main/java/com/twosigma/beaker/core/rest/PluginServiceLocatorRest.java
@@ -648,7 +648,7 @@ public class PluginServiceLocatorRest {
     nginxConfig = nginxConfig.replace("%(restart_id)s", restartId);
     nginxConfig = nginxConfig.replace("%(urlhash)s", urlHash);
     nginxConfig = nginxConfig.replace("%(static_dir)s", this.nginxStaticDir.replaceAll("\\\\", "/"));
-    nginxConfig = nginxConfig.replace("%(nginx_dir)s", this.nginxServDir);
+    nginxConfig = nginxConfig.replace("%(nginx_dir)s", this.nginxServDir.replaceAll("\\\\", "/"));
     java.nio.file.Path targetFile = Paths.get(this.nginxServDir, "conf/nginx.conf");
     writePrivateFile(targetFile, nginxConfig);
     return restartId;

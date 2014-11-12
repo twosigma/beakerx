3138127b18fea1b5196036de19f590c3817f7067
Scott Draves <spot@draves.org>
2014-11-12 10:09:09 -0500

    Revert "Revert "Ildipo/performance""

--- core/src/main/java/com/twosigma/beaker/core/module/config/DefaultBeakerConfig.java
+++ core/src/main/java/com/twosigma/beaker/core/module/config/DefaultBeakerConfig.java
@@ -103,7 +103,7 @@ public class DefaultBeakerConfig implements BeakerConfig {
       this.nginxBinDir = ""; // assuming nginx is available in PATH
     }
     this.nginxServDir = utils.createTempDirectory(this.dotDir, "nginx");
-    this.nginxStaticDir = this.installDir + "/src/main/web/static";
+    this.nginxStaticDir = this.installDir + "/src/main/web";
     this.nginxExtraRules = "";
     this.nginxPluginRules = new HashMap<>();
 

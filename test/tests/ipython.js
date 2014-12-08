1229f93342d88e25af801156539e3affb7e30a0b
Scott Draves <spot@draves.org>
2014-12-08 16:58:53 -0500

    add closing language manager

--- test/tests/ipython.js
+++ test/tests/ipython.js
@@ -1,19 +1,23 @@
 // http://stackoverflow.com/questions/24232511/how-can-i-set-up-common-functions-that-are-available-for-my-test-suites-with-pro
 
 describe('ipython test', function () {
+
   it('should load', function() {
     browser.get('http://localhost:8801/');
     browser.waitForAngular();
   });
+
   it('open a new notebook', function() {
     element(by.id('new-empty-notebook')).click();
     expect(browser.getTitle()).toEqual('New Notebook');
   });
+
   it('open language manager', function () {
     element(by.id('notebook-menu')).click();
     element(by.id('language-manager-menuitem')).click();
     expect(element(by.className('plugin-manager')).isDisplayed()).toBe(true);
   });
+
   it('load ipython', function () {
     expect(element(by.css('#IPython-button .plugin-known')).isDisplayed()).toBe(true);
     expect(element(by.css('#IPython-button .plugin-active')).isDisplayed()).toBe(false);
@@ -27,4 +31,10 @@ describe('ipython test', function () {
       return deferred.promise;
     });
   });
+
+  it('close language manager', function () {
+    element(by.id('language-manager-close-button')).click();
+    expect(element.all(by.className('plugin-manager')).count()).toEqual(0);
+  });
+
 });

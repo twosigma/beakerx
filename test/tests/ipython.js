9c3780f550a300763251211b2cfb7ea6fce5915b
Scott Draves <spot@draves.org>
2014-12-08 16:41:36 -0500

    add IDs to the buttons in the language manager, add test code to click on the button and then wait until the language loads

--- test/tests/ipython.js
+++ test/tests/ipython.js
@@ -1,6 +1,6 @@
 // http://stackoverflow.com/questions/24232511/how-can-i-set-up-common-functions-that-are-available-for-my-test-suites-with-pro
 
-describe('ipython test', function() {
+describe('ipython test', function () {
   it('should load', function() {
     browser.get('http://localhost:8801/');
     browser.waitForAngular();
@@ -9,11 +9,22 @@ describe('ipython test', function() {
     element(by.id('new-empty-notebook')).click();
     expect(browser.getTitle()).toEqual('New Notebook');
   });
-  it('open language manager', function() {
+  it('open language manager', function () {
     element(by.id('notebook-menu')).click();
     element(by.id('language-manager-menuitem')).click();
     expect(element(by.className('plugin-manager')).isDisplayed()).toBe(true);
   });
-  
-  
+  it('load ipython', function () {
+    expect(element(by.css('#IPython-button .plugin-known')).isDisplayed()).toBe(true);
+    expect(element(by.css('#IPython-button .plugin-active')).isDisplayed()).toBe(false);
+    element(by.id('IPython-button')).click();
+    browser.wait(function () {
+      var deferred = protractor.promise.defer();
+      element(by.css('#IPython-button .plugin-active')).isDisplayed()
+        .then(function (result) {
+          deferred.fulfill(result);
+        });
+      return deferred.promise;
+    });
+  });
 });

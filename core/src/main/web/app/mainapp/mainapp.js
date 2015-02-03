38bb64539b2d9f68ef260b80e9639df95df8f2e6
Jake Buob <jake@mojotech.com>
2015-02-03 14:13:58 -0500

    Reorder best solutions for scrolling hack.

--- core/src/main/web/app/mainapp/mainapp.js
+++ core/src/main/web/app/mainapp/mainapp.js
@@ -126,10 +126,10 @@
             // somehow the notebook is scrolled to the middle
             // this hack listens to the 'scroll' event and scrolls it to the top
             // A better solution is to do this when Angular stops firing and DOM updates finish.
-            // A even better solution would be to get rid of the unwanted scrolling in the first place.
             // A even even better solution is the session actually remembers where the scrolling was
             // and scroll to there and in the case of starting a new session (i.e. loading a notebook from file)
             // scroll to top.
+            // A even better solution would be to get rid of the unwanted scrolling in the first place.
             var listener = function(ev) {
               window.scrollTo(0, 0);
               window.removeEventListener('scroll', listener, false);

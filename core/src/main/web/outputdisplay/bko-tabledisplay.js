156dbc4c2c4bb82152947405363d61edbb54d457
Davide Pasetto <dpasetto69@gmail.com>
2015-02-03 16:03:12 -0500

    Initial shoot at replacing datatables with ui-grid

--- core/src/main/web/outputdisplay/bko-tabledisplay.js
+++ core/src/main/web/outputdisplay/bko-tabledisplay.js
@@ -19,10 +19,10 @@
  */
 (function() {
   'use strict';
-  beaker.bkoDirective('Table', ["bkCellMenuPluginManager", "bkDatatables", "bkUtils", function(bkCellMenuPluginManager, bkDatatables, bkUtils) {
+  beaker.bkoDirective('Table', ["bkCellMenuPluginManager", "bkUtils", '$interval', 'uiGridConstants', function(bkCellMenuPluginManager, bkUtils, $interval, uiGridConstants) {
     var CELL_TYPE = "bko-tabledisplay";
     return {
-      template: '<table datatable="" cellspacing="0" dt-options="dtOptions" dt-columns="dtColumns" class="compact row-border stripe hover"></table>',
+      template: '<div ui-grid="gridOptions" ui-grid-cellNav ui-grid-resize-columns ui-grid-exporter ui-grid-pinning ui-grid-selection style="witdth: 500px; height: 350px; "></div>',
       controller: function($scope) {
         $scope.getShareMenuPlugin = function() {
           return bkCellMenuPluginManager.getPlugin(CELL_TYPE);
@@ -36,10 +36,81 @@
         var data = scope.model.getCellModel().values;
         var columns = scope.model.getCellModel().columnNames;
 
+        var fakeI18n = function( title ){
+          var deferred = bkUtils.newDeferred();
+          $interval( function() {
+            deferred.resolve( 'col: ' + title );
+          }, 1000, 1);
+          return deferred.promise;
+        };
+        
+        scope.gridOptions = {
+            enableSorting: true,
+            enableFiltering: true,
+            columnDefs: [ ],
+            modifierKeysToMultiSelectCells: true,
+            enableRowSelection: true,
+            enableColumnResizing: true,
+            enableSelect: true,
+            enableSelectAll: true,
+            enableSelectionBatchEvent: false,
+            multiSelect: true,
+            selectionRowHeaderWidth: 35,
+            enableGridMenu: true,
+            showGridFooter: true,
+            showColumnFooter: true,
+            gridMenuShowHideColumns: false,
+            
+            gridMenuCustomItems: [
+                                  {
+                                    title: 'Clear Selection',
+                                    action: function ($event) {
+                                      scope.gridApi.selection.clearSelectedRows();
+                                      scope.gridApi.core.notifyDataChange(scope.gridApi.grid, uiGridConstants.dataChange.ALL );
+                                    }
+                                  }
+
+                                ]
+         
+        };
+        scope.gridOptions.onRegisterApi = function(gridApi){
+          scope.gridApi = gridApi;
+          gridApi.selection.on.rowSelectionChanged(scope,function(row) {
+            var msg = 'row selected ' + row.isSelected;
+            console.log(msg);
+          });
+        }
+        scope.gridOptions.rowIdentity = function(row) {
+          return row.beakerid;
+        };
+        scope.gridOptions.getRowIdentity = function(row) {
+          return row.beakerid;
+        };
+        
+        scope.gridOptions.data = [];
+        
+        var i,j;
+        for (j=0; j<columns.length; j++) {
+          var f = 'f'+j;
+          scope.gridOptions.columnDefs.push( { field: f, name: columns[j] } );
+        }
+        
+        for (i=0; i<data.length; i++) {
+          var d = data[i];
+          var t = {};
+          t.beakerid = i;
+          for (j=0; j<columns.length; j++) {
+            var f = 'f'+j;
+            t[f] = d[j];
+          }
+          scope.gridOptions.data.push(t);
+        }
+                             
         scope.getDumpState = function() {
           return scope.model.getDumpState();
         };
 
+        /*
         scope.dtOptions = bkDatatables.DTOptionsBuilder
         .fromFnPromise(function() {
           var deferred = bkUtils.newDeferred();
@@ -71,12 +142,14 @@
           scope.dtOptions.withOption('scrollY', 350);
           scope.dtOptions.withOption('scrollCollapse', true);
         }
-
+*/
+        
         scope.state = {};
         var savedstate = scope.model.getDumpState();
         if (savedstate !== undefined && savedstate.tablestate !== undefined) {
           scope.state = savedstate.tablestate;
         }
+        /*
         scope.dtOptions.withOption('stateSave', true);
         scope.dtOptions.withOption('stateSaveCallback',
             function (settings, data) {
@@ -122,7 +195,8 @@
         scope.$on('expand', function() {
           scope.dtOptions.dirty = true;
         });
-
+*/
+        
         scope.$watch('getDumpState()', function(result) {
           if (result !== undefined && result.tablestate === undefined) {
             scope.model.setDumpState({ tablestate : scope.state});

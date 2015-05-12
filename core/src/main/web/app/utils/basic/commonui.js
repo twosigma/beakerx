twosigma/beakerx

commit 92d2f9a753dc8bad3695aba283b2d48af6ec0390

Use dropdown-toggle as actual toggle on promoted dropdowns
Fix #1070

core/src/main/web/app/utils/basic/commonui.js
@@ -120,6 +120,7 @@
           dropdown.prependTo('body');
           dropdown.css('visibility', 'visible');
 
+          element.on('click', '.dropdown-toggle', hideDropdown);
           $(document).on('click.bs.dropdown.data-api', hideDropdown);
         };
 
@@ -129,6 +130,7 @@
           .css('visibility', 'hidden')
           .appendTo(element);
 
+          element.on('click', '.dropdown-toggle', showDropdown);
           $(document).off('click.bs.dropdown.data-api', hideDropdown);
         };
 

commit 7ec0827db18f88f59a6e6fa4d8b888097af4686a

Fix cell menu getting stuck open.
Fixes #1594

This also fixed an issue with a (N^2 + 1) binding leak on cell menu
click. This issue was due to an buggy implementation of a toggle
behavior and re-binding to existing DOM nodes over and over again without first unbinding.

The leaky bug was introduced in 92d2f9a7
PR: https://github.com/twosigma/beaker-notebook/pull/1073
ISSUE: To fix https://github.com/twosigma/beaker-notebook/issues/1070

core/src/main/web/app/utils/basic/commonui.js
@@ -80,6 +80,16 @@
       link: function(scope, element, attrs) {
         var dropdown = element.find('.dropdown-menu').first();
         var toggle = element.find('.dropdown-toggle').first();
+        element.on('click', '.dropdown-toggle', toggleDropdown);
+        $(document).on('click.bs.dropdown.data-api', hideDropdown);
+
+        function toggleDropdown() {
+          if ($(dropdown).is(':visible')) {
+            return hideDropdown();
+          }
+
+          showDropdown();
+        }
 
         var showDropdown = function() {
           window.requestAnimationFrame(function() {
@@ -95,9 +105,6 @@
             dropdown.prependTo(notebook);
             dropdown.css('visibility', 'visible');
           });
-
-          element.on('click', '.dropdown-toggle', hideDropdown);
-          $(document).on('click.bs.dropdown.data-api', hideDropdown);
         };
 
         var hideDropdown = function() {
@@ -105,12 +112,7 @@
           .hide()
           .css('visibility', 'hidden')
           .appendTo(element);
-
-          element.on('click', '.dropdown-toggle', showDropdown);
-          $(document).off('click.bs.dropdown.data-api', hideDropdown);
         };
-
-        element.on('click', '.dropdown-toggle', showDropdown);
 
         scope.$on('$destroy', function() {
           hideDropdown();
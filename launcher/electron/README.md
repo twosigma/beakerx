8fe02c3d8c4cc37fb0c1fd7380bf6560353bb1ae
Diego Torres Quintanilla <diegoalbertotorres@gmail.com>
2015-06-08 09:25:38 -0400

    Added license banner to main.js. Also added README to electron launcher.

---
+++ launcher/electron/README.md
@@ -0,0 +1,14 @@
+So far, the electron launcher can be used by running the following command:
+
+```sh
+electron ./launcher/electron
+```
+
+Electron pre-built binaries can be installed by running
+
+```sh
+npm install electron-prebuilt -g
+```
+
+Note that the server is not executed by this command, so you must start it through
+other means (`gradle run` for developers).
--- launcher/electron/main.js
+++ launcher/electron/main.js
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
@@ -19,13 +35,11 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-  	width: 800,
-  	height: 600,
+  	width: 1500,
+  	height: 1000,
   });
 
-  // and load the index.html of the app.
   mainWindow.loadUrl('http://127.0.0.1:8801/');
-  // mainWindow.loadUrl('http://0.0.0.0:8000');
 
   // Open the devtools.
   mainWindow.openDevTools();

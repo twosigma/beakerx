5e4a7f9a40a9e04de9e3db45727e485dd49578e2
Diego Torres Quintanilla <diegoalbertotorres@gmail.com>
2015-07-27 09:46:01 -0400

    Use jquery params function

--- core/src/main/web/app/helpers/helper.js
+++ core/src/main/web/app/helpers/helper.js
@@ -175,15 +175,7 @@
             if (format) {
               routeParams.format = format;
             }
-            var serialize = function(obj) {
-              var str = [];
-              for(var p in obj)
-                if (obj.hasOwnProperty(p)) {
-                  str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                }
-              return str.join("&");
-            }
-            bkHelper.openWindow(bkUtils.getBaseUrl() + '/open?' + serialize(routeParams), 'notebook');
+            bkHelper.openWindow(bkUtils.getBaseUrl() + '/open?' + jQuery.param(routeParams), 'notebook');
           });
         } else {
           var strategy = bkHelper.getFileSystemFileChooserStrategy();

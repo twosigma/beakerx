7d85c251c94a27961be1c98b8973cb8a1736e1f9
EgoshinME <megoshin@qulix.com>
2015-08-07 09:27:39 +0300

    EasyForm: api to set width and height for textarea. Fix textarea width in characters

--- core/src/main/web/outputdisplay/bko-plot/bko-easyform.js
+++ core/src/main/web/outputdisplay/bko-plot/bko-easyform.js
@@ -121,7 +121,8 @@
               efc.buildUI = function() {
                 var fixedSize = false;
                 if (!efc.getComponent().width
-                    || efc.getComponent().width < efc.constants.Components.TextField.MIN_WIDTH) {
+                    || parseInt(efc.getComponent().width)
+                      < efc.constants.Components.TextField.MIN_WIDTH) {
                   efc.getComponent().width = efc.constants.Components.TextField.MIN_WIDTH;
                 } else {
                   fixedSize = true;
@@ -161,22 +162,25 @@
               efc.buildUI = function() {
                 var fixedSize = false;
                 if (!efc.getComponent().height
-                    || efc.getComponent().height < efc.constants.Components.TextArea.MIN_HEIGHT) {
+                    || parseInt(efc.getComponent().height)
+                      < efc.constants.Components.TextArea.MIN_HEIGHT) {
                   efc.getComponent().height = efc.constants.Components.TextArea.MIN_HEIGHT;
                 }
                 if (!efc.getComponent().width
-                    || efc.getComponent().width < efc.constants.Components.TextArea.MIN_WIDTH) {
+                    || parseInt(efc.getComponent().width)
+                      < efc.constants.Components.TextArea.MIN_WIDTH) {
                   efc.getComponent().width = efc.constants.Components.TextArea.MIN_WIDTH;
                 } else {
                   fixedSize = true;
                 }
                 element.find('.easyform-label').text(efc.getComponent().label);
                 var textArea = element.find('.text-area');
-                textArea.attr('ng-model', scope.ngModelAttr)
-                    .attr('cols', efc.getComponent().width)
+                textArea
+                    .attr('ng-model', scope.ngModelAttr)
                     .attr('rows', efc.getComponent().height);
                 if (fixedSize) {
                   element.find('.easyform-component-container').addClass('fixed-size');
+                  textArea.css('width', parseInt(efc.getComponent().width) + 1.5 + 'ch');
                 }
               };
 

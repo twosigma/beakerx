467fdbb7394e3321eb6663ea80c9ca3a8e839501
Gabe Fernando <gef756@gmail.com>
2015-08-16 15:32:51 -0400

    FIX: force ipython to run on version 3.2.1
    The ipython notebook has separated out to the jupyter project, so go
    back to using version 3 until everything is fixed.

--- launcher/ubuntu/install-dependencies.sh
+++ launcher/ubuntu/install-dependencies.sh
@@ -57,13 +57,13 @@ sudo apt-get install -y libzmq3-dbg libzmq3-dev libzmq3
 
 # ipython
 sudo apt-get install -y python-pip python-dev python-yaml
-sudo pip install ipython jinja2 tornado pyzmq pandas jsonschema
+sudo pip install ipython==3.2.1 jinja2 tornado pyzmq pandas jsonschema
 sudo apt-get install -y python-matplotlib python-scipy
 
 # python3
 
-sudo apt-get install -y python-virtualenv python3-dev pkgconf libfreetype6-dev libfreetype6 libxft-dev libblas-dev liblapack-dev gfortran libyaml-dev 
-virtualenv ~/py3k -p python3 
+sudo apt-get install -y python-virtualenv python3-dev pkgconf libfreetype6-dev libfreetype6 libxft-dev libblas-dev liblapack-dev gfortran libyaml-dev
+virtualenv ~/py3k -p python3
 ~/py3k/bin/pip install ipython[notebook]==3.0.0
 ~/py3k/bin/pip install numpy matplotlib scipy jinja2 tornado pyzmq pandas pyaml
 

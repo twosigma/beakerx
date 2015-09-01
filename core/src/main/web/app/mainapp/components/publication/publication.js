a2f59c9e500561f620fe2865aa2fa453c9ac3ef6
bartek <bartek@mojotech.com>
2015-09-01 21:40:13 +0200

    Allow re-publishing notebooks as a different user
    This is needed for when a user downloads a notebook from Publications and then after making some changes he wants to re-publish it again as his publication.

--- core/src/main/web/app/mainapp/components/publication/publication.js
+++ core/src/main/web/app/mainapp/components/publication/publication.js
@@ -129,11 +129,14 @@
         if (wasPublished()) {
           bkPublicationApi.getPublication(notebook.metadata['publication-id'])
           .then(function(resp) {
-            $scope.model = resp.data;
-            $scope.model['category-id'] = resp.data.category['public-id'];
-            $scope.published = true;
-            $scope.title = 'Update Notebook';
-            $scope.saveButton = 'Update';
+            var pub = resp.data;
+            if (bkPublicationAuth.currentUser()['public-id'] == pub['author-id']) {
+              $scope.model = pub;
+              $scope.model['category-id'] = pub.category['public-id'];
+              $scope.published = true;
+              $scope.title = 'Update Notebook';
+              $scope.saveButton = 'Update';
+            }
           });
         }
 
@@ -162,10 +165,14 @@
       }
 
       $scope.publishAction = function() {
+        var action = $scope.published ? "update" : "create";
+        return $scope.publish(action);
+      };
+
+      $scope.publish = function(action) {
         $scope.saving = true;
-        var action = $scope.published ? updatePublication : createPublication;
         return bkHelper.saveNotebook()
-        .then(action)
+        .then(action == "update" ? updatePublication : createPublication)
         .then(function() {
           $scope.saving = false;
           $scope.close();

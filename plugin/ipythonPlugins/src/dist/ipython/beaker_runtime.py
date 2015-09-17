98f7fbae0ea59008dd8242b285319e9e039b6803
EgoshinME <megoshin@qulix.com>
2015-09-17 13:52:31 +0300

    #2333. Python Dates showing up as NaN

--- plugin/ipythonPlugins/src/dist/ipython/beaker_runtime.py
+++ plugin/ipythonPlugins/src/dist/ipython/beaker_runtime.py
@@ -314,8 +314,7 @@ class DataFrameEncoder(json.JSONEncoder):
             out = {}
             out['type'] = "TableDisplay"
             out['subtype'] = "TableDisplay"
-            out['hasIndex'] = "true"
-            out['columnNames'] = ['Index'] + obj.columns.tolist()
+            out['columnNames'] = obj.columns.tolist()
             vals = obj.values.tolist()
             idx = obj.index.tolist()
             for x in range(0,len(vals)):

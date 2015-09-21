021531908899be22ac0827ca9d46a9899d472413
Siarhei Shvaiko <siarhei.shvaiko@softeq.com>
2015-09-21 18:08:30 +0300

    DB requests caching

--- plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/SqlAutocomplete.java
+++ plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/SqlAutocomplete.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.twosigma.beaker.autocomplete.ClasspathScanner;
+import com.twosigma.beaker.sqlsh.autocomplete.db.DbCache;
 import com.twosigma.beaker.sqlsh.autocomplete.db.DbExplorerFactory;
 import com.twosigma.beaker.sqlsh.autocomplete.db.DbInfo;
 import com.twosigma.beaker.sqlsh.utils.JDBCClient;
@@ -25,15 +26,16 @@ public class SqlAutocomplete {
 			"reindex", "release", "rename", "replace", "restrict", "right", "rollback", "row", "savepoint", "select",
 			"set", "table", "temp", "temporary", "then", "to", "transaction", "trigger", "union", "unique", "update",
 			"using", "vacuum", "values", "view", "virtual", "when", "where", "with", "without" };
-
+	
 	private static final String PARAM_CHAR = "%";
 
 	private static final String[] PARAM_KEYS = { PARAM_CHAR + PARAM_CHAR +"beakerDB", PARAM_CHAR + PARAM_CHAR + "inputs"};
-
+	
 	private final JDBCClient jdbcClient;
 	private final String sessionId;
     private final Map<String, String> namedConnectionString;
-
+    private final DbCache cache; 
+	
 
 
 	public SqlAutocomplete(ClasspathScanner _cps, JDBCClient jdbcClient, String sessionId, Map<String, String> namedConnectionString) {
@@ -41,6 +43,8 @@ public class SqlAutocomplete {
 		this.jdbcClient = jdbcClient;
 		this.sessionId = sessionId;
 		this.namedConnectionString = namedConnectionString;
+		
+		this.cache = DbExplorerFactory.getDbCache(); 
 	}
 
 	private List<String> findKeys(final String key, final String[] keys){
@@ -57,59 +61,58 @@ public class SqlAutocomplete {
 				}
 			}
 		}
-
+		
 		return ret;
 	}
-
+	
 	private List<String> findSqlKeys(final String key){
 		return findKeys(key, SQL_KEYS);
 	}
-
+	
 	private List<String> findParamKeys(final String key){
 		return findKeys(key, PARAM_KEYS);
 	}
-
-
-
+	
+	
+	
 	private String findKey(final String txt, final int cur){
 		if (cur <= 0 || Character.isWhitespace(txt.charAt(cur - 1))) {
 			return "";
 		} else {
 			String res = "";
-
+			
 			int eos = cur - 1;
 
 			for (int i = eos; i >= 0; i--) {
-
+				
 				final boolean isIdentifier = Character.isUnicodeIdentifierPart(txt.charAt(i)) || PARAM_CHAR.charAt(0) == txt.charAt(i);
-
+				
 				if (isIdentifier) {
 					eos = i;
 				}
-
-
+					
+				
 				if (!isIdentifier || i == 0) {
-
+					
 					res = new String(txt.substring(eos, cur));
 					break;
 				}
 			}
-
+			
 			return res;
 		}
 	}
-
+	
 
 	public List<String> doAutocomplete(final String txt, final int cur) {
-
 		List<String> ret = new LinkedList<String>();
 
 		if (cur == 0) {
 			return findParamKeys(null);
 		}
-
+		
 		final String key = findKey(txt, cur);
-
+		
 		if (key != null && key.length() > 0 && key.startsWith(PARAM_CHAR)) {
 			return findParamKeys(key);
 		} else {
@@ -118,34 +121,34 @@ public class SqlAutocomplete {
 
 		final DbInfo dbInfo = DbExplorerFactory.getDbInfo(txt, jdbcClient, sessionId, namedConnectionString);
 		if (dbInfo != null) {
-
+			
 			List<String> dbRet = null;
-
+			
 			if (cur > key.length() + 1 && (txt.charAt(cur - key.length() - 1) == '.' || ".".equals(key))) {
-
+				
 				String fieldKey = key;
 				int searchTableIndex = cur - key.length() - 1;
-
+				
 				if (".".equals(key)) {
 					fieldKey = "";
 					searchTableIndex = cur - 1;
 				}
-
+				
 				final String tableName = findKey(txt, searchTableIndex);
-
+				
 				if (tableName != null) {
-					return dbInfo.getTableFieldNames(null, tableName, fieldKey);
+					return dbInfo.getTableFieldNames(cache, null, tableName, fieldKey);
 				}
-
+				
 			} else {
-				dbRet = dbInfo.getTableNames(null, key);
+				dbRet = dbInfo.getTableNames(cache, null, key);
 			}
-
+			
 			if (dbRet != null) {
 				ret.addAll(0, dbRet);
 			}
 		}
-
+		
 		return ret;
 	}
 }

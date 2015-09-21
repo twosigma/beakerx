11b3b0d9882211c723795128b67155b91b615444
Maksim <maksim.lavrik@softeq.com>
2015-09-21 10:54:14 +0300

    #2382 - CRLF to CR

--- plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbInfo.java
+++ plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbInfo.java
@@ -1,9 +1,1 @@
-package com.twosigma.beaker.sqlsh.autocomplete.db;
-
-import java.util.List;
-
-public interface DbInfo {
-
-	public List<String> getTableNames(final String schemaName, final String key);
-	public List<String> getTableFieldNames(final String schemaName, final String tableName, final String key);
-}
+package com.twosigma.beaker.sqlsh.autocomplete.db;import java.util.List;public interface DbInfo {	public List<String> getTableNames(final String schemaName, final String key);	public List<String> getTableFieldNames(final String schemaName, final String tableName, final String key);}

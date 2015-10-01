91d375a9e1a7b20423e32aeb2c5443b1deb507a0
Siarhei Shvaiko <siarhei.shvaiko@softeq.com>
2015-10-01 16:53:23 +0300

    Fixes

--- plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbExplorerFactory.java
+++ plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbExplorerFactory.java
@@ -1,17 +1,15 @@
 /*
- *  Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
+ * Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.twosigma.beaker.sqlsh.autocomplete.db;
@@ -19,46 +17,50 @@ package com.twosigma.beaker.sqlsh.autocomplete.db;
 import java.io.IOException;
 import java.util.Map;
 
-import javax.sql.DataSource;
-
 import com.twosigma.beaker.NamespaceClient;
 import com.twosigma.beaker.sqlsh.utils.BeakerParser;
 import com.twosigma.beaker.sqlsh.utils.DBConnectionException;
 import com.twosigma.beaker.sqlsh.utils.JDBCClient;
 
 public class DbExplorerFactory {
-	
-	private static final String VENDOR_JDBC_MYSQL = "jdbc:mysql:";
-//	private static final String VENDOR_JDBC_ORACLE = "jdbc:oracle:";
-//	private static final String VENDOR_JDBC_MSSQL = "jdbc:sqlserver:";
 
-	public static DbInfo getDbInfo(String txt, JDBCClient jdbcClient, String sessionId, String defaultConnectionString, Map<String, String> namedConnectionString) {
-		
-		final NamespaceClient namespaceClient = NamespaceClient.getBeaker(sessionId);
-		final BeakerParser beakerParser;
-		try {
-			beakerParser = new BeakerParser(txt, namespaceClient, defaultConnectionString, namedConnectionString);
-			
-			final String uri = beakerParser.getDbURI();
-			
-			if (uri != null) {
-				final DataSource ds = jdbcClient.getDataSource(uri);
+  private static final String VENDOR_JDBC_MYSQL = "jdbc:mysql:";
+  private static final String VENDOR_JDBC_H2 = "jdbc:h2:";
+  private static final String VENDOR_JDBC_ORACLE = "jdbc:oracle:";
+  private static final String VENDOR_JDBC_MSSQL = "jdbc:sqlserver:";
 
-				if (uri.startsWith(VENDOR_JDBC_MYSQL)) {
-					return new MysqlDbExplorer(ds);
-				}
-			}
+  public static DbInfo getDbInfo(String txt, JDBCClient jdbcClient, String sessionId,
+      String defaultConnectionString, Map<String, String> namedConnectionString) {
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (DBConnectionException e) {
-			e.printStackTrace();
-		}	
+    final NamespaceClient namespaceClient = NamespaceClient.getBeaker(sessionId);
+    final BeakerParser beakerParser;
+    try {
+      beakerParser =
+          new BeakerParser(txt, namespaceClient, defaultConnectionString, namedConnectionString);
 
-		return null;
-	}
+      final String uri = beakerParser.getDbURI();
 
-	public static DbCache getDbCache() {
-		return new DbRequestCache();
-	}
+      if (uri != null) {
+
+        if (uri.startsWith(VENDOR_JDBC_MYSQL)) {
+          return new MySqlDbExplorer(uri, jdbcClient);
+        } else if (uri.startsWith(VENDOR_JDBC_H2)) {
+          return new H2DbExplorer(uri, jdbcClient);
+        } else if (uri.startsWith(VENDOR_JDBC_ORACLE)) {
+          return new OracleDbExplorer(uri, jdbcClient);
+        } else if (uri.startsWith(VENDOR_JDBC_MSSQL)) {
+          return new MsSqlDbExplorer(uri, jdbcClient);
+        }
+      }
+
+    } catch (IOException | DBConnectionException e) {
+      System.out.print(e.getMessage() + "\n");
+    }
+
+    return null;
+  }
+
+  public static DbCache getDbCache() {
+    return new DbRequestCache();
+  }
 }
--- plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbRequestCache.java
+++ plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbRequestCache.java
@@ -1,136 +1,157 @@
 /*
- *  Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
+ * Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.twosigma.beaker.sqlsh.autocomplete.db;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DbRequestCache implements DbCache {
 
-	
-	private final ConcurrentHashMap<String, Map<String, Map<String, Map<String, List<String>>>>> fieldsCache;
-	private final ConcurrentHashMap<String, Map<String, Map<String, List<String>>>> tablesCache;
-	/**
-	 * 
-	 */
-	@SuppressWarnings("unused")
-	private static final long serialVersionUID = -8164467511255046521L;
 
-	public DbRequestCache() {
-		super();
-		fieldsCache = new ConcurrentHashMap<String, Map<String, Map<String, Map<String, List<String>>>>>();
-		tablesCache = new ConcurrentHashMap<String, Map<String, Map<String, List<String>>>>();
-	}
+  private final static String DEF_SCHEMA = "BKR_SHEMA_NULL";
 
-	
-	@Override
-	public List<String> getTableNames(Connection conn, String schemaName, String key) throws SQLException {
-		final String uri = conn.getMetaData().getURL();
-		final Map<String, Map<String, List<String>>> uriCache = tablesCache.get(uri);
+  private final ConcurrentHashMap<String, Map<String, Map<String, Map<String, List<String>>>>> fieldsCache;
+  private final ConcurrentHashMap<String, Map<String, Map<String, List<String>>>> tablesCache;
+  /**
+   * 
+   */
+  @SuppressWarnings("unused")
+  private static final long serialVersionUID = -8164467511255046521L;
 
-		if (uriCache == null) {
-			return null;
-		}
-		
-		final Map<String, List<String>> schemaCache =  uriCache.get(schemaName);
+  public DbRequestCache() {
+    super();
+    fieldsCache =
+        new ConcurrentHashMap<String, Map<String, Map<String, Map<String, List<String>>>>>();
+    tablesCache = new ConcurrentHashMap<String, Map<String, Map<String, List<String>>>>();
+  }
 
-		if (schemaCache == null) {
-			return null;
-		}
-		
-		return schemaCache.get(key);
-	}
+  private String checkSchema(Map<String, ?> map, final String schemaName) {
+    if (schemaName == null) {
+      if (map != null) {
+        Set<String> keys = map.keySet();
+        if (keys.size() == 1) {
+          for (String schema : keys) {
+            return schema;
+          }
+        }
+      }
 
-	@Override
-	public void putTableNames(Connection conn, String schemaName, String key, List<String> values) throws SQLException {
-		if (values != null) {
-			final String uri = conn.getMetaData().getURL();
-			
-			Map<String, Map<String, List<String>>> uriCache = new ConcurrentHashMap<String, Map<String, List<String>>>();
-			
-			if (tablesCache.putIfAbsent(uri, uriCache) != null) {
-				uriCache = tablesCache.get(uri);
-			}
-			
-			Map<String, List<String>> schemaCache = new ConcurrentHashMap<String, List<String>>();
-			
-			if (uriCache.putIfAbsent(schemaName, schemaCache) != null) {
-				schemaCache = uriCache.get(schemaName);
-			}
-			
-			schemaCache.put(key, values);
-			
-		}
-	}
+      return DEF_SCHEMA;
+    }
 
+    return schemaName;
+  }
 
-	@Override
-	public List<String> getTableFieldNames(Connection conn, String schemaName, String tableName, String key) throws SQLException {
-		final String uri = conn.getMetaData().getURL();
-		final Map<String, Map<String, Map<String, List<String>>>> uriCache = fieldsCache.get(uri);
+  @Override
+  public List<String> getTableNames(final String url, String schemaName, String key) {
+    final Map<String, Map<String, List<String>>> uriCache = tablesCache.get(url);
 
-		if (uriCache == null) {
-			return null;
-		}
-		
-		final Map<String, Map<String, List<String>>> schemaCache =  uriCache.get(schemaName);
+    if (uriCache == null) {
+      return null;
+    }
 
-		if (schemaCache == null) {
-			return null;
-		}
-		
-		final Map<String, List<String>> tablesCache =  schemaCache.get(tableName);
-		
-		if (tablesCache == null) {
-			return null;
-		}
-		
-		return tablesCache.get(key);
-			
-	}
+    final Map<String, List<String>> schemaCache = uriCache.get(checkSchema(uriCache, schemaName));
+
+    if (schemaCache == null) {
+      return null;
+    }
+
+    return schemaCache.get(key);
+  }
+
+  @Override
+  public void putTableNames(final String url, String schemaName, String key, List<String> values) {
+    if (values != null) {
+      Map<String, Map<String, List<String>>> uriCache =
+          new ConcurrentHashMap<String, Map<String, List<String>>>();
+
+      if (tablesCache.putIfAbsent(url, uriCache) != null) {
+        uriCache = tablesCache.get(url);
+      }
+
+      Map<String, List<String>> schemaCache = new ConcurrentHashMap<String, List<String>>();
+
+      schemaName = checkSchema(uriCache, schemaName);
+      if (((ConcurrentHashMap<String, Map<String, List<String>>>) uriCache).putIfAbsent(schemaName,
+          schemaCache) != null) {
+        schemaCache = uriCache.get(schemaName);
+      }
+
+      schemaCache.put(key, values);
+
+    }
+  }
 
 
-	@Override
-	public void putTableFieldNames(Connection conn, String schemaName, String tableName, String key, List<String> values) throws SQLException {
-		if (values != null) {
-			final String uri = conn.getMetaData().getURL();
-			
-			Map<String, Map<String, Map<String, List<String>>>> uriCache = new ConcurrentHashMap<String, Map<String, Map<String, List<String>>>>();
-			
-			if (fieldsCache.putIfAbsent(uri, uriCache) != null) {
-				uriCache = fieldsCache.get(uri);
-			}
-			
-			Map<String, Map<String, List<String>>> schemaCache = new ConcurrentHashMap<String, Map<String, List<String>>>();
-			
-			if (uriCache.putIfAbsent(schemaName, schemaCache) != null) {
-				schemaCache = uriCache.get(schemaName);
-			}
-			
-			Map<String, List<String>> tableCache = new ConcurrentHashMap<String, List<String>>();
-			
-			if (schemaCache.putIfAbsent(tableName, tableCache) != null) {
-				tableCache = schemaCache.get(tableName);
-			}
-			
-			tableCache.put(key, values);
-		}
-	}
+  @Override
+  public List<String> getTableFieldNames(final String url, String schemaName, String tableName,
+      String key) {
+    final Map<String, Map<String, Map<String, List<String>>>> uriCache = fieldsCache.get(url);
+
+    if (uriCache == null) {
+      return null;
+    }
+
+    final Map<String, Map<String, List<String>>> schemaCache =
+        uriCache.get(checkSchema(uriCache, schemaName));
+
+    if (schemaCache == null) {
+      return null;
+    }
+
+    final Map<String, List<String>> tablesCache = schemaCache.get(tableName);
+
+    if (tablesCache == null) {
+      return null;
+    }
+
+    return tablesCache.get(key);
+
+  }
+
+
+  @Override
+  public void putTableFieldNames(final String url, String schemaName, String tableName, String key,
+      List<String> values) {
+    if (values != null) {
+      Map<String, Map<String, Map<String, List<String>>>> uriCache =
+          new ConcurrentHashMap<String, Map<String, Map<String, List<String>>>>();
+
+      if (fieldsCache.putIfAbsent(url, uriCache) != null) {
+        uriCache = fieldsCache.get(url);
+      }
+
+      Map<String, Map<String, List<String>>> schemaCache =
+          new ConcurrentHashMap<String, Map<String, List<String>>>();
+
+      schemaName = checkSchema(uriCache, schemaName);
+      if (((ConcurrentHashMap<String, Map<String, Map<String, List<String>>>>) uriCache)
+          .putIfAbsent(schemaName, schemaCache) != null) {
+        schemaCache = uriCache.get(schemaName);
+      }
+
+      Map<String, List<String>> tableCache = new ConcurrentHashMap<String, List<String>>();
+
+      if (((ConcurrentHashMap<String, Map<String, List<String>>>) schemaCache)
+          .putIfAbsent(tableName, tableCache) != null) {
+        tableCache = schemaCache.get(tableName);
+      }
+
+      tableCache.put(key, values);
+    }
+  }
 }

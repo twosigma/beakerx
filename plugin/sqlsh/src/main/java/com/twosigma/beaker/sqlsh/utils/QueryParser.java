8b03bf8281714139a1e59a85684e4faa6bc1e85f
Maksim <maksim.lavrik@softeq.com>
2015-10-01 10:37:14 +0300

    for merge

--- plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/utils/QueryParser.java
+++ plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/utils/QueryParser.java
@@ -21,77 +21,77 @@ import java.util.Scanner;
 
 public class QueryParser {
 
-  public static List<String> split(String script) {
-    script = deleteInterval("/*", "*/", script);
+    public static List<String> split(String script) {
+        script = deleteInterval("/*", "*/", script);
 
-    Scanner scanner = new Scanner(script);
-    StringBuffer sb = new StringBuffer();
+        Scanner scanner = new Scanner(script);
+        StringBuffer sb = new StringBuffer();
 
-    while (scanner.hasNextLine()) {
-      String line = scanner.nextLine();
-      line.trim();
-      //ignore comments #
-      int commentIndex = line.indexOf('#');
-      if (commentIndex != -1) {
-        if (line.startsWith("#")) {
-          line = new String("");
-        } else {
-          line = line.substring(0, commentIndex - 1);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            line.trim();
+            //ignore comments #
+            int commentIndex = line.indexOf('#');
+            if (commentIndex != -1) {
+                if (line.startsWith("#")) {
+                    line = new String("");
+                } else {
+                    line = line.substring(0, commentIndex - 1);
+                }
+            }
+            //ignore comments --
+            commentIndex = line.indexOf("--");
+            if (commentIndex != -1) {
+                if (line.startsWith("--")) {
+                    line = new String("");
+                } else {
+                    line = line.substring(0, commentIndex - 1);
+                }
+            }
+            //ignore comments %%
+            commentIndex = line.indexOf("%%");
+            if (commentIndex != -1) {
+                if (line.startsWith("%%")) {
+                    line = new String("");
+                } else {
+                    line = line.substring(0, commentIndex - 1);
+                }
+            }
+
+            if (line != null) {
+                sb.append(line).append(" ");
+            }
         }
-      }
-      //ignore comments --
-      commentIndex = line.indexOf("--");
-      if (commentIndex != -1) {
-        if (line.startsWith("--")) {
-          line = new String("");
-        } else {
-          line = line.substring(0, commentIndex - 1);
+        scanner.close();
+
+        //ToDo replace to custom splitter - need for ignore 'xx;yy' etc.
+        String[] splittedQueries = sb.toString().split(";");
+        List<String> listOfQueries = new ArrayList<>();
+
+        for (int i = 0; i < splittedQueries.length; i++) {
+            if (!splittedQueries[i].trim().equals("") && !splittedQueries[i].trim().equals("\t")) {
+                listOfQueries.add(splittedQueries[i].trim());
+            }
         }
-      }
-      //ignore comments %%
-      commentIndex = line.indexOf("%%");
-      if (commentIndex != -1) {
-        if (line.startsWith("%%")) {
-          line = new String("");
-        } else {
-          line = line.substring(0, commentIndex - 1);
-        }
-      }
-
-      if (line != null) {
-        sb.append(line).append(" ");
-      }
-    }
-    scanner.close();
-
-    //ToDo replace to custom splitter - need for ignore 'xx;yy' etc.
-    String[] splittedQueries = sb.toString().split(";");
-    List<String> listOfQueries = new ArrayList<>();
-
-    for (int i = 0; i < splittedQueries.length; i++) {
-      if (!splittedQueries[i].trim().equals("") && !splittedQueries[i].trim().equals("\t")) {
-        listOfQueries.add(splittedQueries[i].trim());
-      }
-    }
-    return listOfQueries;
-  }
-
-  private static String deleteInterval(String start, String end, String source) {
-    int startIndex = source.indexOf(start);
-    while (startIndex >= 0) {
-      int endIndex = source.indexOf(end);
-      if (endIndex != -1) {
-        if (endIndex < startIndex) {
-          break;
-        }
-        source = source.substring(0, startIndex) + source.substring(endIndex + 2);
-      } else {
-        break;
-      }
-
-      startIndex = source.indexOf(start);
+        return listOfQueries;
     }
 
-    return source;
-  }
+    private static String deleteInterval(String start, String end, String source) {
+        int startIndex = source.indexOf(start);
+        while (startIndex >= 0) {
+            int endIndex = source.indexOf(end);
+            if (endIndex != -1) {
+                if (endIndex < startIndex) {
+                    break;
+                }
+                source = source.substring(0, startIndex) + source.substring(endIndex + 2);
+            } else {
+                break;
+            }
+
+            startIndex = source.indexOf(start);
+        }
+
+        return source;
+    }
 }

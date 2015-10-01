292ac288510ea2ebac6806de46c6191878090609
Scott Draves <spot@draves.org>
2015-10-01 14:41:39 -0400

    a big step closer to our style guide

--- plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbInfo.java
+++ plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbInfo.java
@@ -20,6 +20,7 @@ import java.util.List;
 
 public interface DbInfo {
 
-	public List<String> getTableNames(DbCache dbCache, final String schemaName, final String key);
-	public List<String> getTableFieldNames(DbCache dbCache, final String schemaName, final String tableName, final String key);
+  public List<String> getTableNames(DbCache dbCache, final String schemaName, final String key);
+  public List<String> getTableFieldNames(DbCache dbCache, final String schemaName,
+                                         final String tableName, final String key);
 }

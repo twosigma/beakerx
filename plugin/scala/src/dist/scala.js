d70e414d53c2e011573377aa669792b34a93f877
kpaharelau <Kanstantsin.Paharelau@artezio.com>
2015-10-13 14:31:17 +0300

    CategoryPlot
    CategoryPlotSerializer was added

--- plugin/scala/src/dist/scala.js
+++ plugin/scala/src/dist/scala.js
@@ -162,6 +162,8 @@ define(function(require, exports, module) {
                         "com.twosigma.beaker.chart.Filter",
                         "com.twosigma.beaker.chart.xychart.*",
                         "com.twosigma.beaker.chart.xychart.plotitem.*",
+                        "com.twosigma.beaker.chart.categoryplot.*",
+                        "com.twosigma.beaker.chart.categoryplot.plotitem.*",
                         "com.twosigma.beaker.easyform.*",
                         "com.twosigma.beaker.easyform.formitem.*"];
   var shellReadyDeferred = bkHelper.newDeferred();

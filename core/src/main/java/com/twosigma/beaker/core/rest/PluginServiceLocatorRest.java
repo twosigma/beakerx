ed5a0ec37aa42452c21fab5a230b006a302627c9
kpaharelau <Kanstantsin.Paharelau@artezio.com>
2015-11-16 12:00:34 +0300

    Try to avoid test timeout

--- core/src/main/java/com/twosigma/beaker/core/rest/PluginServiceLocatorRest.java
+++ core/src/main/java/com/twosigma/beaker/core/rest/PluginServiceLocatorRest.java
@@ -591,22 +591,16 @@ public class PluginServiceLocatorRest {
   {
     List<String> cmdBase = pythonBaseCommand(pluginId, command);
     cmdBase.add("--hash");
+    cmdBase.add(password);
+
     Process proc = Runtime.getRuntime().exec(listToArray(cmdBase), buildEnv(pluginId, null));
     BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(proc.getOutputStream()));
     new StreamGobbler(proc.getErrorStream(), "stderr", "ipython-hash", null, null).start();
-    bw.write("from IPython.lib import passwd\n");
-    // I have confirmed that this does not go into ipython history by experiment
-    // but it would be nice if there were a way to make this explicit. XXX
-    bw.write("print(passwd('" + password + "'))\n");
-    bw.close();
-    String line;
-    while ((line = br.readLine()) != null) {
-      if (line.contains("sha1:")) {
-        return line.substring(line.indexOf("sha1:"));
-      }
+    String hash = br.readLine();
+    if (null == hash) {
+      throw new RuntimeException("unable to get IPython hash");
     }
-    throw new RuntimeException("unable to get IPython hash");
+    return hash;
   }
 
   private void generateIPythonConfig(String pluginId, int port, String password, String command)

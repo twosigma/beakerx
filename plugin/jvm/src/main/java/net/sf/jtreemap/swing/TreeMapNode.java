ae88b534dc7cfdd996fc47b0e095d8503d3e3aa6
Scott Draves <spot@draves.org>
2015-12-17 14:57:59 -0500

    replace unicode in comment with ascii

--- plugin/jvm/src/main/java/net/sf/jtreemap/swing/TreeMapNode.java
+++ plugin/jvm/src/main/java/net/sf/jtreemap/swing/TreeMapNode.java
@@ -101,7 +101,7 @@ public class TreeMapNode extends DefaultMutableTreeNode {
      * @param weight
      *            weight of the leaf (if negative, we take the absolute value).
      * @param value
-     *            Value associée à la feuille
+     *            Value associee a la feuille
      */
     public TreeMapNode(final String label, final double weight, final Value value) {
         super(label);

76685b9912242d3c0e9bbd3634daadd1af7bcc74
Marina Navasiolava <m.navasiolava@gmail.com>
2015-12-29 10:09:38 -0200

    plot colors should depend on theme #3173. generate default colors on frontend

--- core/src/main/web/outputdisplay/bko-plot/plotutils.js
+++ core/src/main/web/outputdisplay/bko-plot/plotutils.js
@@ -795,8 +795,47 @@
         rgbaToHex(60, 30, 50),   // dark
       ],
 
+      createNiceColor: function(){
+        var hue = Math.random();
+        var saturation = 0.75;
+        var luminance = 0.5;
+        var rgb = this.hslToRgb(hue, saturation, luminance);
+        var niceColor = rgbaToHex(rgb[0], rgb[1], rgb[2]);
+        while (this.defaultColors.indexOf(niceColor) !== -1) {
+          niceColor = this.createNiceColor();
+        }
+        return niceColor;
+      },
+
+      //http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+      hslToRgb: function(h, s, l){
+        var r, g, b;
+
+        if(s == 0){
+          r = g = b = l; // achromatic
+        }else{
+          var hue2rgb = function(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+          }
+
+          var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+          var p = 2 * l - q;
+          r = hue2rgb(p, q, h + 1/3);
+          g = hue2rgb(p, q, h);
+          b = hue2rgb(p, q, h - 1/3);
+        }
+
+        return [r * 255, g * 255, b * 255];
+      },
+
+
       getDefaultColor: function(i) {
-        return this.defaultColors[i];
+        return i < this.defaultColors.length ? this.defaultColors[i] : this.createNiceColor();
       }
     };
   };

0b25a59b7aae2ba7d308a802aaf8f7ff069bce39
Tomislav Pesut <tomca32@gmail.com>
2016-01-22 09:28:03 -0500

    Added text-cell.js test file

--- test/protractorConf.js
+++ test/protractorConf.js
@@ -38,6 +38,7 @@ var config = {
           'tests/notebook.js',
           'tests/autotranslation.js',
           'tests/code-cell.js',
+          'tests/text-cell.js',
           'tests/cell-menu.js',
           'tests/language-manager.js',
           'tests/bugs.js'

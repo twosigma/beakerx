a8beb1b0f82ef2712abc2fd515cf11d228165e8f
Alice Trofimova <atrofimova@qulix.com>
2016-01-29 11:26:43 +0300

    #2836 dataTables header menu - small fix

--- core/src/main/web/outputdisplay/bko-tabledisplay/bko-tabledisplay.js
+++ core/src/main/web/outputdisplay/bko-tabledisplay/bko-tabledisplay.js
@@ -329,15 +329,6 @@
           return $scope.getCellDispOpts[i];
         };
 
-        //@todo remove
-        $scope.displayAll = function() {
-          var i;
-          for (i = 0; i < $scope.getCellSho.length; i++) {
-            $scope.getCellSho[i] = true;
-          }
-        };
-        //
-
         $scope.toggleColumnsVisibility = function(visible) {
           if (!$scope.table) {
             return;
@@ -754,7 +745,6 @@
               }
             }
           });
-
         };
         scope.init = function(model) {
           scope.doDestroy(true);

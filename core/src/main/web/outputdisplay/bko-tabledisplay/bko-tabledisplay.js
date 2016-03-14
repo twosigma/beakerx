bb63b34f61fee98fdace36aa0965470e4bfa4263
Marina Navasiolava <m.navasiolava@gmail.com>
2016-03-14 14:15:33 +0300

    Table width is broken after redraw #3663

--- core/src/main/web/outputdisplay/bko-tabledisplay/bko-tabledisplay.js
+++ core/src/main/web/outputdisplay/bko-tabledisplay/bko-tabledisplay.js
@@ -1042,6 +1042,8 @@
           var pp = me.parent();
           if (pp.width() > me.width()) {
             pp.width(me.width());
+          }
+          if (pp.width() >= me.width()) {
             me.attr('width', 'auto');
           } else {
             me.removeAttr('width');

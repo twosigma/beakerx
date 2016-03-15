8e66e30f55e7a1e23bbea14135e45b0acc9fe77b
Marina Navasiolava <m.navasiolava@gmail.com>
2016-03-15 17:11:21 +0300

    remove unused code

--- core/src/main/web/outputdisplay/bko-plot/plotutils.js
+++ core/src/main/web/outputdisplay/bko-plot/plotutils.js
@@ -638,7 +638,6 @@
       base64Fonts: {},
 
       getFontToInject: function(font) {
-        var defer = bkUtils.newDeferred();
         var src = '';
         for (var url in font.urlformats) {
           if (font.urlformats.hasOwnProperty(url)) {
@@ -656,7 +655,6 @@
           "font-weight: " + font.fontWeight + ";" +
           "font-style: " + font.fontStyle + ";" +
           " }\n";
-        return defer.promise;
       },
 
       //http://stackoverflow.com/questions/7370943/retrieving-binary-file-content-using-javascript-base64-encode-it-and-reverse-de

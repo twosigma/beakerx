e6914076824a6855be50fbb56353206ebf3df511
cit <paharelau@gmail.com>
2016-03-16 12:43:52 +0300

    Wrong chart size in Firefox browser #3683
    Safari not Safary

--- core/src/main/web/app/helpers/helper.js
+++ core/src/main/web/app/helpers/helper.js
@@ -77,7 +77,7 @@
       // Firefox 1.0+
       isFirefox: typeof InstallTrigger !== 'undefined',
       // At least Safari 3+: "[object HTMLElementConstructor]"
-      isSafary: Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0,
+      isSafari: Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0,
       // Chrome 1+
       isChrome: !!window.chrome && !!window.chrome.webstore,
 

728aaa2b478a07aca3c8f1c15399d33f9d8faf16
vefimov <vefimov@qulix.com>
2016-04-01 16:12:35 +0300

    create test for R tutorial

---
+++ test/tests/tutorials/language_demos/R-tutorial.js
@@ -0,0 +1,42 @@
+/*
+ *  Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+var BeakerPageObject = require('../../beaker.po.js');
+var path = require('path');
+var beakerPO;
+
+describe('R Tutorial', function () {
+
+    beforeEach(function (done) {
+        beakerPO = new BeakerPageObject();
+        browser.get(beakerPO.baseURL + "beaker/#/open?uri=file:config%2Ftutorials%2Fr-examples.bkr&readOnly=true").then(done);
+
+        beakerPO.waitUntilLoadingCellOutput();
+    });
+
+    it('R Examples', function () {
+        var idCell = "code2uVtKX";
+        beakerPO.scrollToCodeCellOutputByIdCell(idCell);
+        var svgElem = beakerPO.getCodeCellOutputByIdCell(idCell).all(by.css('svg > g'));
+        expect(svgElem.count()).toBe(1);
+        expect(svgElem.get(0).all(by.css('g')).count()).toBe(78);
+        expect(svgElem.get(0).all(by.css('path')).count()).toBe(108);
+
+    });
+
+
+});

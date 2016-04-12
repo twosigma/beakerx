4af287a307357cdadcbc5991336f8d428adda232
Scott Draves <spot@draves.org>
2016-04-11 23:07:03 -0400

    disable python test

--- test/protractorConf.js
+++ test/protractorConf.js
@@ -48,8 +48,7 @@ var config = {
     'tests/vim-mode.js',
     'tests/tutorials/language_demos/sql-tutorial.js',
     'tests/tutorials/language_demos/java-tutorial.js',
-    'tests/tutorials/language_demos/clojure-tutorial.js',
-    'tests/tutorials/language_demos/python-tutorial.js'
+    'tests/tutorials/language_demos/clojure-tutorial.js'
   ]
 };
 

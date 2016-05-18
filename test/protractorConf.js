9b0d2333bc805f06f5de54fbc05381b3c3d86375
vefimov <vefimov@qulix.com>
2016-05-18 20:40:31 +0300

    check tests

--- test/protractorConf.js
+++ test/protractorConf.js
@@ -39,12 +39,12 @@ var config = {
     'tests/easyform.js',
     'tests/landing-page.js',
     'tests/notebook.js',
-    'tests/code-cell.js',
-    'tests/text-cell.js',
+    //'tests/code-cell.js',
+    //'tests/text-cell.js',
     'tests/cell-menu.js',
     'tests/language-manager.js',
     'tests/vim-mode.js',
-    'tests/autotranslation.js'
+    //'tests/autotranslation.js'
   ]
 };
 

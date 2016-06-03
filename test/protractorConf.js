c8515a5afee57f51cbdbd2f24cdc20571918bd74
vefimov <vefimov@qulix.com>
2016-06-03 11:53:01 +0300

    enable all tests

--- test/protractorConf.js
+++ test/protractorConf.js
@@ -36,14 +36,14 @@ var config = {
     }));
   },
   specs: [
-    //'tests/easyform.js',
-    //'tests/landing-page.js',
-    //'tests/notebook.js',
-    //'tests/code-cell.js',
-    //'tests/text-cell.js',
-    //'tests/cell-menu.js',
-    //'tests/language-manager.js',
-    //'tests/vim-mode.js',
+    'tests/easyform.js',
+    'tests/landing-page.js',
+    'tests/notebook.js',
+    'tests/code-cell.js',
+    'tests/text-cell.js',
+    'tests/cell-menu.js',
+    'tests/language-manager.js',
+    'tests/vim-mode.js',
     'tests/autotranslation.js'
   ]
 };

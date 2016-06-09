12c2bccb30d64dcf1f4866a0d1b3514dbbf01614
Sergey Shkalikov <sshkalikov@gmail.com>
2016-06-09 16:20:19 +0300

    fix node starting

--- plugin/node/src/dist/app/app.js
+++ plugin/node/src/dist/app/app.js
@@ -25,6 +25,8 @@ var host = process.argv[3];
 
 var shells = {};
 
+console.log('Server Starting');
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: false})); // to support URL-encoded bodies
 

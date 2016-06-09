23bcd2d17c2a3ed3fe0811a1cec07684716fdc96
cit <paharelau@gmail.com>
2016-06-09 10:24:22 +0300

    cmd-shift-A inserts 2 cells #4235
    remove duplicate call

--- core/src/main/web/app/mainapp/components/notebook/codecell-directive.js
+++ core/src/main/web/app/mainapp/components/notebook/codecell-directive.js
@@ -383,9 +383,6 @@
           'Shift-Ctrl-A': function(cm) {
             scope.appendCodeCell();
           },
-          'Shift-Cmd-A': function(cm) {
-            scope.appendCodeCell();
-          },
           'Shift-Ctrl-E': function(cm) {
             scope.popupMenu();
             element.find('.inputcellmenu').find('li').find('a')[0].focus();

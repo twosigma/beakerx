569b88edcaa29dd10de88f8f28d0eae86dc2371d
vefimov <vefimov@qulix.com>
2016-06-23 14:05:49 +0300

    add test for "The Beaker Object" tutorial

--- test/tests/tutorials/feature_overview/beakerObject-tutorial.js
+++ test/tests/tutorials/feature_overview/beakerObject-tutorial.js
@@ -18,7 +18,7 @@
 var BeakerPageObject = require('../../beaker.po.js');
 var beakerPO;
 
-describe('Working with d3.js', function (done) {
+describe('The Beaker Object Tutorial', function (done) {
 
     beforeAll(function(done){
         beakerPO = new BeakerPageObject();
@@ -52,6 +52,83 @@ describe('Working with d3.js', function (done) {
             beakerPO.checkCellOutputTextByIdCell(idCell, "12");
             beakerPO.checkEvaluatorByIdCell(idCell, "Groovy");
         });
+    });
+
+    it('Storage and Sharing', function () {
+        var idCell = "codeaINahj";
+        beakerPO.scrollToBkCellByIdCell(idCell);
+        beakerPO.clickCodeCellInputButtonByIdCell(idCell, 'Text');
+        beakerPO.checkCellOutputTextByIdCell(idCell, "[1,1,2,3,5,8]");
+    });
+
+    describe('Viewing and Removing', function(){
+        it('Should display x, y values ', function () {
+            var idCell = "coderACA1q";
+            beakerPO.scrollToBkCellByIdCell(idCell);
+            beakerPO.clickCodeCellInputButtonByIdCell(idCell, 'OutputContainer');
+            var liVar = beakerPO.getCodeCellOutputByIdCell(idCell).all(by.css('li.outputcontainer-li'));
+            expect(liVar.count()).toBe(2);
+            expect(liVar.get(0).element(by.css('b')).getText()).toBe("y");
+            expect(liVar.get(0).element(by.css('pre')).getText()).toBe("[1,1,2,3,5,8]");
+            expect(liVar.get(1).element(by.css('b')).getText()).toBe("x");
+            expect(liVar.get(1).element(by.css('pre')).getText()).toBe("11");
+        });
+        it('Should delete x (by run cell)', function () {
+            var idCell = "codeAgGQzI";
+            beakerPO.scrollToBkCellByIdCell(idCell);
+            beakerPO.clickCodeCellInputButtonByIdCell(idCell, 'Text');
+            beakerPO.checkCellOutputTextByIdCell(idCell, "true");
+
+            idCell = "coderACA1q";
+            beakerPO.scrollToBkCellByIdCell(idCell);
+            beakerPO.clickCodeCellInputButtonByIdCell(idCell, 'OutputContainer');
+            var liVar = beakerPO.getCodeCellOutputByIdCell(idCell).all(by.css('li.outputcontainer-li'));
+            expect(liVar.count()).toBe(1);
+            expect(liVar.get(0).element(by.css('b')).getText()).toBe("y");
+        });
+        it('Should rename y to z (by menu)', function () {
+            idCell = "coderACA1q";
+            beakerPO.scrollToBkCellByIdCell(idCell);
+            beakerPO.clickCodeCellInputButtonByIdCell(idCell, 'OutputContainer');
+            var liVar = beakerPO.getCodeCellOutputByIdCell(idCell).all(by.css('li.outputcontainer-li'));
+            expect(liVar.get(0).element(by.css('b')).getText()).toBe("y");
+
+            liVar.get(0).element(by.css('a.dropdown-toggle')).click();
+            var renameElem =  liVar.get(0).element(by.cssContainingText('a', 'Rename'));
+            browser.actions().mouseMove(renameElem).perform();
+            renameElem.click();
+            browser.wait(beakerPO.EC.visibilityOf(element(by.css('div.modal-body'))), 10000).then(function(){
+                element(by.css('input#property-name')).sendKeys('z');
+                element(by.css('div.modal-footer button[ng-click="save()"]')).click();
+            });
+            expect(beakerPO.getCodeCellOutputByIdCell(idCell).all(by.css('li.outputcontainer-li')).get(0).element(by.css('b')).getText()).toBe("z");
+        });
+        it('Should delete z (by menu)', function () {
+            idCell = "coderACA1q";
+            beakerPO.scrollToBkCellByIdCell(idCell);
+            beakerPO.clickCodeCellInputButtonByIdCell(idCell, 'OutputContainer');
+            var liVar = beakerPO.getCodeCellOutputByIdCell(idCell).all(by.css('li.outputcontainer-li'));
+            expect(liVar.get(0).element(by.css('b')).getText()).toBe("z");
+
+            liVar.get(0).element(by.css('a.dropdown-toggle')).click();
+            var deleteElem =  liVar.get(0).all(by.cssContainingText('a', 'Delete')).get(0);
+            browser.actions().mouseMove(deleteElem).perform();
+            deleteElem.click().then(function(){
+                beakerPO.clickCodeCellInputButtonByIdCell(idCell, 'OutputContainer');
+                expect(beakerPO.getCodeCellOutputByIdCell(idCell).element(by.css('li.outputcontainer-li')).isPresent()).toBe(false);
+            });
+        });
+    });
+
+    it('Beaker API Calls', function () {
+        var idCell = "codeLRsgl6";
+        beakerPO.scrollToBkCellByIdCell(idCell);
+        beakerPO.clickCodeCellInputButtonByIdCell(idCell, 'Results');
+        var printElem = beakerPO.getCodeCellOutputByIdCell(idCell).all(by.css('li > pre'));
+        beakerPO.checkSubString(printElem.get(0), 'stdout for JavaScript:');
+        beakerPO.checkSubString(printElem.get(1), 'useful for debugging,');
+        beakerPO.checkSubString(printElem.get(2), 'different from');
+        beakerPO.checkSubString(beakerPO.getCodeCellOutputByIdCell(idCell).element(by.css('div.output-padding > pre')), 'a returned value');
     });
 
 });

0a56d393555bc44805d79cf372a801bc921b6004
Marina Navasiolava <m.navasiolava@gmail.com>
2016-06-29 15:20:55 +0300

    gradle bowerInstall #4346

--- core/src/main/web/vendor/bower_components/angular-touch/angular-touch.js
+++ core/src/main/web/vendor/bower_components/angular-touch/angular-touch.js
@@ -1,9 +1,9 @@
 /**
- * @license AngularJS v1.5.0
+ * @license AngularJS v1.5.5
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
-(function(window, angular, undefined) {'use strict';
+(function(window, angular) {'use strict';
 
 /* global ngTouchClickDirectiveFactory: false,
  */

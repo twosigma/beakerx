b0b977e9395e261a82dbdc55a4a5752489bcfa37
Scott Draves <spot@draves.org>
2016-07-06 11:45:25 -0400

    Revert "Revert "#1766. Can open same notebook multiple times""

--- core/src/main/web/app/mainapp/services/sessionmanager.js
+++ core/src/main/web/app/mainapp/services/sessionmanager.js
@@ -22,6 +22,7 @@
     'bk.utils',
     'bk.session',
     'bk.notebookManager',
+    'bk.core',
     'bk.notebookCellModelManager',
     'bk.notebookNamespaceModelManager',
     'bk.recentMenu',
@@ -37,6 +38,7 @@
       bkNotebookCellModelManager,
       bkNotebookCellModelManagerFactory,
       bkNotebookNamespaceModelManager,
+      bkCoreManager,
       bkEvaluatorManager,
       bkRecentMenu,
       bkElectron,
@@ -444,6 +446,8 @@
     var _sessionId = null;
     var _edited = false;
     var _needsBackup = false;
+    var _forceDisconnected = false;
+    var _lastModifiedTime = 0;
     var _saveDir = bkUtils.getHomeDirectory();
 
     var BeakerObject = function(nbmodel) {
@@ -860,6 +864,14 @@
           delete _subscriptions[sessionId];
         }
       };
+    
+      var _checkLastModifiedTime = function () {
+        if (_notebookUri.get()) {
+          bkUtils.newPromise(bkCoreManager.getFileLoader(_uriType).getLastModifiedTime(_notebookUri.get())).then(function (value) {
+            _lastModifiedTime = value;
+          });
+        }
+      };
 
     return {
       reset: function(notebookUri, uriType, readOnly, format, notebookModel, edited, sessionId) {
@@ -893,6 +905,7 @@
         bkSession.backup(_sessionId, generateBackupData());
         bkNotebookManager.init(this);
       },
+      checkLastModifiedTime: _checkLastModifiedTime,
       setSessionId: function(sessionId) {
         if (!sessionId) {
           sessionId = bkUtils.generateId(6);
@@ -923,6 +936,7 @@
         _notebookModel.set(notebookModel);
         bkHelper.initBeakerPrefs();
         _sessionId = sessionId;
+        _checkLastModifiedTime();
 
         _needsBackup = _edited;
         bkNotebookNamespaceModelManager.init(sessionId, notebookModel);
@@ -930,6 +944,7 @@
         connectcontrol(sessionId);
         bkSession.backup(_sessionId, generateBackupData());
         recordRecentNotebook();
+        _forceDisconnected = false;
       },
       clear: function() {
         disconnectcontrol(_sessionId);
@@ -1001,6 +1016,13 @@
       isSavable: function() {
         return _notebookUri && !_readOnly;
       },
+      checkFileModifiedSinceOpened: function () {
+        var saveData = this.getSaveData();
+        var fileLoader = bkCoreManager.getFileLoader(saveData.uriType);
+        return bkUtils.newPromise(fileLoader.getLastModifiedTime(saveData.notebookUri)).then(function (lastModifiedTime) {
+          return lastModifiedTime > _lastModifiedTime;
+        });
+      },
       /*
        * This function triggers all display implementations to save the current output status.
        * This save is asynchronous and happens in the current digest loop.
@@ -1060,6 +1082,12 @@
           }
           this.setNotebookModelEdited(true);
         }
+      },
+      forceDisconnect: function () {
+        _forceDisconnected = true;
+      },
+      isForceDisconnected: function () {
+        return _forceDisconnected;
       },
       evaluatorUnused: function(plugin) {
         var n = _.find(_notebookModel.get().cells, function (c) {

01e33256bd79acb492ed042e31dc9a1be201c070
aviate <eric@eric-heiden.de>
2016-07-13 20:28:01 +0000

    Removed unused imports

--- plugin/scala/src/main/scala/com/twosigma/beaker/scala/rest/ScalaShellRest.java
+++ plugin/scala/src/main/scala/com/twosigma/beaker/scala/rest/ScalaShellRest.java
@@ -38,8 +38,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Path("scalash")
 @Produces(MediaType.APPLICATION_JSON)
 @Singleton

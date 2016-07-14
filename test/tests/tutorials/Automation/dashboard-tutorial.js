de5d239bce669b28ab322fb86acc77cd74d54e79
vefimov <vefimov@qulix.com>
2016-07-14 14:30:38 +0300

    add test for 'Dashboard API' tutorial

---
+++ test/tests/tutorials/Automation/dashboard-tutorial.js
@@ -0,0 +1,40 @@
+/*
+ *  Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+var BeakerPageObject = require('../../beaker.po.js');
+var beakerPO;
+
+describe('Dashboard API', function (done) {
+
+    beforeAll(function(done){
+        beakerPO = new BeakerPageObject();
+        browser.get(beakerPO.baseURL + "beaker/#/open?uri=file:config%2Ftutorials%2Fdashboards.bkr&readOnly=true").then(done);
+        beakerPO.waitUntilLoadingCellOutput();
+    });
+
+    afterAll(function(done){
+        beakerPO.createScreenshot('dashboardsTutorial');
+        done();
+    });
+
+    it('Generate data', function () {
+        var idCell = "codeWZqKpe";
+        beakerPO.scrollToBkCellByIdCell(idCell);
+        beakerPO.clickCodeCellInputButtonByIdCell(idCell, 'Text');
+    });
+
+});

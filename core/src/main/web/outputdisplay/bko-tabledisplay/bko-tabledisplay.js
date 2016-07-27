fd4c0221367076f7cd5c101201a9dc728dfae387
Dmytro Gordiienko <dmitry.gordienko@codete.com>
2016-07-27 19:39:15 +0200

    Issue #4095 - The table displayed by pandas::DataFrame::sort_value() method is not sorted by the specified column - fix init options for table

--- core/src/main/web/outputdisplay/bko-tabledisplay/bko-tabledisplay.js
+++ core/src/main/web/outputdisplay/bko-tabledisplay/bko-tabledisplay.js
@@ -2221,6 +2221,7 @@
             'stateSave': true,
             'processing': true,
             'autoWidth': true,
+            'ordering': !!scope.tableOrder,
             'order': scope.tableOrder ? _.cloneDeep(scope.tableOrder) : [[0, 'asc']],
             'scrollX': '10%',
             'searching': true,

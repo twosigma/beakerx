c6958ae3a6dd89085e2593956fb20da15d78e8e5
Scott Draves <spot@draves.org>
2016-07-27 19:25:59 -0400

    disable node test

--- test/protractorWithoutRestartBrowserConf.js
+++ test/protractorWithoutRestartBrowserConf.js
@@ -54,7 +54,7 @@ var config = {
     'tests/tutorials/language_demos/python-tutorial.js',
     'tests/tutorials/language_demos/jscript-tutorial.js',
     'tests/tutorials/language_demos/R-tutorial.js',
-    'tests/tutorials/language_demos/nodejs-tutorial.js',
+    // 'tests/tutorials/language_demos/nodejs-tutorial.js',
     'tests/tutorials/standard_visual_api/d3js-tutorial.js',
     'tests/tutorials/standard_visual_api/p5js-tutorial.js',
    // 'tests/tutorials/automate/progress-reporting-tutorial.js',

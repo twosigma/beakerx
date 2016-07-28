2edf6057c220aeaeba2f3effdfba3e012bd79b48
Scott Draves <spot@draves.org>
2016-07-27 20:28:43 -0400

    disable autocomplete test

--- test/protractorWithoutRestartBrowserConf.js
+++ test/protractorWithoutRestartBrowserConf.js
@@ -62,7 +62,7 @@ var config = {
     'tests/tutorials/automate/notebook-reflection-tutorial.js',
     'tests/tutorials/automate/dashboard-tutorial.js',
     'tests/tables.js',
-    'tests/autocomplete.js',
+    // 'tests/autocomplete.js',
     'tests/tutorials/native_plotting/jscriptPlot-tutorial.js',
     'tests/tutorials/table_display/tableGroovy-tutorial.js',
     'tests/tutorials/feature_overview/autotranslation-tutorial.js',

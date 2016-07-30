cee10641bc6f58abd01fc47c170b2ef6264cf10b
Scott Draves <spot@draves.org>
2016-07-30 13:25:33 -0400

    Revert "Revert "issue #3312 (SQL login) implemented on new branch""

--- plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbExplorerFactory.java
+++ plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/autocomplete/db/DbExplorerFactory.java
@@ -17,12 +17,14 @@
 package com.twosigma.beaker.sqlsh.autocomplete.db;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Map;
 
 import javax.sql.DataSource;
 
 import com.twosigma.beaker.NamespaceClient;
 import com.twosigma.beaker.sqlsh.utils.BeakerParser;
+import com.twosigma.beaker.sqlsh.utils.ConnectionStringHolder;
 import com.twosigma.beaker.sqlsh.utils.DBConnectionException;
 import com.twosigma.beaker.sqlsh.utils.JDBCClient;
 
@@ -32,17 +34,14 @@ public class DbExplorerFactory {
   // private static final String VENDOR_JDBC_ORACLE = "jdbc:oracle:";
   // private static final String VENDOR_JDBC_MSSQL = "jdbc:sqlserver:";
 
-  public static DbInfo getDbInfo(String txt, JDBCClient jdbcClient, String sessionId,
-                                 String defaultConnectionString,
-                                 Map<String, String> namedConnectionString) {
+  public static DbInfo getDbInfo(String txt, JDBCClient jdbcClient, String sessionId, ConnectionStringHolder defaultConnectionString, Map<String, ConnectionStringHolder> namedConnectionString) {
 
     final NamespaceClient namespaceClient = NamespaceClient.getBeaker(sessionId);
     final BeakerParser beakerParser;
     try {
-      beakerParser = new BeakerParser(txt, namespaceClient,
-                                      defaultConnectionString, namedConnectionString);
+      beakerParser = new BeakerParser(txt, namespaceClient, defaultConnectionString, namedConnectionString, jdbcClient);
 
-      final String uri = beakerParser.getDbURI();
+      final String uri = beakerParser.getDbURI().getActualConnectionString();
 
       if (uri != null) {
         final DataSource ds = jdbcClient.getDataSource(uri);
@@ -55,6 +54,8 @@ public class DbExplorerFactory {
     } catch (IOException e) {
       e.printStackTrace();
     } catch (DBConnectionException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
       e.printStackTrace();
     }
 

d40fa43a8deeb5f5b73c4ae457148ddde8def1b5
Scott Draves <spot@draves.org>
2016-08-09 11:17:11 -0400

    missing import

--- plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/utils/QueryExecutor.java
+++ plugin/sqlsh/src/main/java/com/twosigma/beaker/sqlsh/utils/QueryExecutor.java
@@ -22,6 +22,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;

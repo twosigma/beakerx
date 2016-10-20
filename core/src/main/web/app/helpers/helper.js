fa9429c3e411824bc591e63205ad35a2780180b8
Konstantin Tarletski <konstantin.tarletski@ignite.ee>
2016-10-20 18:12:18 +0300

    issue #4772 "emacs mode keybindings on mac" -- fixed

--- core/src/main/web/app/helpers/helper.js
+++ core/src/main/web/app/helpers/helper.js
@@ -100,8 +100,12 @@
       var bkHelper = {
 
       isNewNotebookShortcut: function (e){
-        if (this.isMacOS || this.getInputCellKeyMapMode() === "emacs"){
-          return false; //issue #4722 e.ctrlKey && (e.which === 78);// Ctrl + n
+        if (this.isMacOS){
+          if(this.getInputCellKeyMapMode() === "emacs"){
+            return false; //issue #4722 
+          }else{
+            return returne.ctrlKey && (e.which === 78);// Ctrl + n
+          }
         }
         return e.altKey && (e.which === 78);// Alt + n
       },
@@ -174,8 +178,12 @@
           ((e.which>=49) && (e.which<=50));// alt + Shift + 1...2
       },
       isSearchReplace: function (e){
-        if (this.isMacOS || this.getInputCellKeyMapMode() === "emacs"){
-          return e.ctrlKey && (e.which === 83);// Ctrl + s
+        if (this.isMacOS){
+          if(this.getInputCellKeyMapMode() === "emacs"){
+            return e.ctrlKey && (e.which === 83);// Ctrl + s
+          }else{
+            return e.ctrlKey && (e.which === 70);// Ctrl + f
+          }
         }
         return e.altKey && (e.which === 70);// Alt + f
       },

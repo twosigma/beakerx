dab6a7b89d883330478f9a48dd92d5ac4c3f853c
jarek <jaroslaw.malek@codete.com>
2016-11-09 10:36:44 +0100

    #4748: revert namespace.js

--- plugin/ipythonPlugins/src/dist/vendor/ipython3/namespace.js
+++ plugin/ipythonPlugins/src/dist/vendor/ipython3/namespace.js
@@ -1,10 +1,10 @@
 // Copyright (c) IPython Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-var IPython3 = IPython3 || {};
+var IPython = IPython || {};
 define('ipython3_namespace', [], function(){
     "use strict";
-    IPython3.version = "3.0.0";
-    IPython3._target = '_blank';
-    return IPython3;
+    IPython.version = "3.0.0";
+    IPython._target = '_blank';
+    return IPython;
 });

b9820663893837449cef5953da27de713deb0c0e
Konstantin Tarletski <konstantin.tarletski@ignite.ee>
2016-12-01 15:41:06 +0200

    issue #4915 "fix ipywidgets"

--- plugin/ipythonPlugins/src/dist/vendor/ipython3/widget_float.js
+++ plugin/ipythonPlugins/src/dist/vendor/ipython3/widget_float.js
@@ -5,30 +5,30 @@ define('ipython3_widget_float', [
   'ipython3_widget',
   'ipython3_widget_int'
 ], function (widget, int_widgets) {
-    var IntSliderView = int_widgets.IntSliderView;
-    var IntTextView = int_widgets.IntTextView;
+  var IntSliderView = int_widgets.IntSliderView;
+  var IntTextView = int_widgets.IntTextView;
 
-    var FloatSliderView = IntSliderView.extend({
-        _parse_value: parseFloat,
+  var FloatSliderView = IntSliderView.extend({
+      _parse_value: parseFloat,
 
-        // matches: whitespace?, float, whitespace?, [-:], whitespace?, float
-        _range_regex: /^\s*([+-]?(?:\d*\.?\d+|\d+\.)(?:[eE][+-]?\d+)?)\s*[-:]\s*([+-]?(?:\d*\.?\d+|\d+\.)(?:[eE][+-]?\d+)?)/,
+      // matches: whitespace?, float, whitespace?, [-:], whitespace?, float
+      _range_regex: /^\s*([+-]?(?:\d*\.?\d+|\d+\.)(?:[eE][+-]?\d+)?)\s*[-:]\s*([+-]?(?:\d*\.?\d+|\d+\.)(?:[eE][+-]?\d+)?)/,
 
-        _validate_slide_value: function(x) {
-            /**
-             * Validate the value of the slider before sending it to the back-end
-             * and applying it to the other views on the page.
-             */
-            return x;
-        }
-    });
+      _validate_slide_value: function(x) {
+          /**
+           * Validate the value of the slider before sending it to the back-end
+           * and applying it to the other views on the page.
+           */
+          return x;
+      },
+  });
 
-    var FloatTextView = IntTextView.extend({
-        _parse_value: parseFloat
-    });
+  var FloatTextView = IntTextView.extend({
+      _parse_value: parseFloat
+  });
 
-    return {
-        'FloatSliderView': FloatSliderView,
-        'FloatTextView': FloatTextView
-    };
+  return {
+      'FloatSliderView': FloatSliderView,
+      'FloatTextView': FloatTextView,
+  };
 });

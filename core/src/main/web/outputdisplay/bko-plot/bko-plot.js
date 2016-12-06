24cc26dea764ce78e6311b5c43b94cb8db5066b9
sluhtoja <sten.luhtoja@ignite.ee>
2016-12-06 08:50:24 +0200

    #4890 plotting nans categories

--- core/src/main/web/outputdisplay/bko-plot/bko-plot.js
+++ core/src/main/web/outputdisplay/bko-plot/bko-plot.js
@@ -1851,16 +1851,20 @@
 
         scope.standardizeData = function() {
           var model = scope.model.getCellModel();
-          for(var i = 0; i<model.graphics_list.length; i++){
-            for(var j = 0; j<model.graphics_list[i].x.length; j++){
-              if (!$.isNumeric(model.graphics_list[i].x[j])
-                || !$.isNumeric(model.graphics_list[i].y[j])){
-                scope.invalidData.push({
-                  x: model.graphics_list[i].x[j], y: model.graphics_list[i].y[j]
-                });
-                model.graphics_list[i].x.splice(j, 1);
-                model.graphics_list[i].y.splice(j, 1);
-                j--;
+          if (model.graphics_list){
+            for(var i = 0; i<model.graphics_list.length; i++){
+              if (model.graphics_list[i].x && model.graphics_list[i].y){
+                for(var j = 0; j<model.graphics_list[i].x.length; j++){
+                  if (!$.isNumeric(model.graphics_list[i].x[j])
+                    || !$.isNumeric(model.graphics_list[i].y[j])){
+                    scope.invalidData.push({
+                      x: model.graphics_list[i].x[j], y: model.graphics_list[i].y[j]
+                    });
+                    model.graphics_list[i].x.splice(j, 1);
+                    model.graphics_list[i].y.splice(j, 1);
+                    j--;
+                  }
+                }
               }
             }
           }

4819540490999e03d65bd4c02762f94b10ef6f68
Vladimir <mailevv@gmail.com>
2017-03-02 20:57:14 +0300

    add tests for MessageCreator class (#135)
    * add tests for MessageCreator class
    
    * add tests for MessageCreator class
    
    * add tests for MessageCreator test

--- jupyter/Groovy/src/test/java/com/twosigma/beaker/jupyter/msg/MessageCreatorTest.java
+++ jupyter/Groovy/src/test/java/com/twosigma/beaker/jupyter/msg/MessageCreatorTest.java
@@ -15,6 +15,7 @@
  */
 package com.twosigma.beaker.jupyter.msg;
 
+import com.twosigma.beaker.jupyter.SocketEnum;
 import com.twosigma.beaker.jvm.object.SimpleEvaluationObject;
 import com.twosigma.beaker.widgets.GroovyKernelTest;
 import com.twosigma.beaker.widgets.TestWidgetUtils;
@@ -31,18 +32,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MessageCreatorTest {
 
   private MessageCreator messageCreator;
+  private SimpleEvaluationObject seo;
 
   @Before
   public void setUp() throws Exception {
-    GroovyKernelTest kernel = new GroovyKernelTest("id1");
-    messageCreator = new MessageCreator(kernel);
+    messageCreator = new MessageCreator(new GroovyKernelTest("id1"));
+    seo = new SimpleEvaluationObject("code");
+    seo.setJupyterMessage(new Message());
   }
 
   @Test
-  public void shouldReturnNullStringForNull() throws Exception {
+  public void createMessageWithNullResult_shouldReturnNullStringForNull() throws Exception {
     //given
-    SimpleEvaluationObject seo = new SimpleEvaluationObject(null);
-    seo.setJupyterMessage(new Message());
     seo.finished(null);
     //when
     List<MessageHolder> message = messageCreator.createMessage(seo);
@@ -52,10 +53,8 @@ public class MessageCreatorTest {
   }
 
   @Test
-  public void shouldReturnResult() throws Exception {
+  public void createMessageWithNotNullResult_shouldReturnResult() throws Exception {
     //given
-    SimpleEvaluationObject seo = new SimpleEvaluationObject("1/1");
-    seo.setJupyterMessage(new Message());
     seo.finished("1");
     //when
     List<MessageHolder> message = messageCreator.createMessage(seo);
@@ -64,4 +63,131 @@ public class MessageCreatorTest {
     assertThat(data.get(MessageCreator.TEXT_PLAIN)).isEqualTo("1");
   }
 
+  @Test
+  public void createMessageWithNotNullResult_createThreeMessages() throws Exception {
+    //given
+    seo.finished("result");
+    //when
+    List<MessageHolder> messages = messageCreator.createMessage(seo);
+    //then
+    assertThat(messages).isNotEmpty();
+    assertThat(messages.size()).isEqualTo(3);
+  }
+
+  @Test
+  public void createMessageWithNotNullResult_firstIOPubMessageHasTypeIsExecuteResult() throws Exception {
+    //given
+    seo.finished("result");
+    //when
+    List<MessageHolder> messages = messageCreator.createMessage(seo);
+    //then
+    assertThat(messages).isNotEmpty();
+    assertThat(messages.get(0).getSocketType()).isEqualTo(SocketEnum.IOPUB_SOCKET);
+    assertThat(messages.get(0).getMessage().type()).isEqualTo(JupyterMessages.EXECUTE_RESULT);
+  }
+
+  @Test
+  public void createMessageWithNotNullResult_secondIOPubMessageHasTypeIsStatus() throws Exception {
+    //given
+    seo.finished("result");
+    //when
+    List<MessageHolder> messages = messageCreator.createMessage(seo);
+    //then
+    assertThat(messages).isNotEmpty();
+    assertThat(messages.get(1).getSocketType()).isEqualTo(SocketEnum.IOPUB_SOCKET);
+    assertThat(messages.get(1).getMessage().type()).isEqualTo(JupyterMessages.STATUS);
+  }
+
+  @Test
+  public void createMessageWithNotNullResult_thirdShellMessageHasTypeIsExecuteReply() throws Exception {
+    //given
+    seo.finished("result");
+    //when
+    List<MessageHolder> messages = messageCreator.createMessage(seo);
+    //then
+    assertThat(messages).isNotEmpty();
+    assertThat(messages.get(2).getSocketType()).isEqualTo(SocketEnum.SHELL_SOCKET);
+    assertThat(messages.get(2).getMessage().type()).isEqualTo(JupyterMessages.EXECUTE_REPLY);
+  }
+
+  @Test
+  public void createIdleMessage_messageHasTypeIsStatus(){
+    //when
+    Message message = messageCreator.createIdleMessage(new Message());
+    //then
+    assertThat(message.type()).isEqualTo(JupyterMessages.STATUS);
+  }
+
+  @Test
+  public void createIdleMessage_messageHasExecutionStateIsIdle(){
+    //when
+    Message message = messageCreator.createIdleMessage(new Message());
+    //then
+    Map data = message.getContent();
+    assertThat(data.get(MessageCreator.EXECUTION_STATE)).isEqualTo(MessageCreator.IDLE);
+  }
+
+  @Test
+  public void createBusyMessage_messageHasTypeIsStatus(){
+    //when
+    Message message = messageCreator.createBusyMessage(new Message());
+    //then
+    assertThat(message.type()).isEqualTo(JupyterMessages.STATUS);
+  }
+
+  @Test
+  public void createBusyMessage_messageHasExecutionStateIsBusy(){
+    //when
+    Message message = messageCreator.createBusyMessage(new Message());
+    //then
+    Map data = message.getContent();
+    assertThat(data.get(MessageCreator.EXECUTION_STATE)).isEqualTo(MessageCreator.BUSY);
+  }
+
+  @Test
+  public void createMessageWithError_createThreeMessages() throws Exception {
+    //given
+    seo.error("some error");
+    //when
+    List<MessageHolder> messages = messageCreator.createMessage(seo);
+    //then
+    assertThat(messages).isNotEmpty();
+    assertThat(messages.size()).isEqualTo(3);
+  }
+
+  @Test
+  public void createMessageWithError_firstIOPubMessageHasTypeIsStream() throws Exception {
+    //given
+    seo.error("some error");
+    //when
+    List<MessageHolder> messages = messageCreator.createMessage(seo);
+    //then
+    assertThat(messages).isNotEmpty();
+    assertThat(messages.get(0).getSocketType()).isEqualTo(SocketEnum.IOPUB_SOCKET);
+    assertThat(messages.get(0).getMessage().type()).isEqualTo(JupyterMessages.STREAM);
+  }
+
+  @Test
+  public void createMessageWithError_secondIOPubMessageHasTypeIsStatus() throws Exception {
+    //given
+    seo.error("some error");
+    //when
+    List<MessageHolder> messages = messageCreator.createMessage(seo);
+    //then
+    assertThat(messages).isNotEmpty();
+    assertThat(messages.get(1).getSocketType()).isEqualTo(SocketEnum.IOPUB_SOCKET);
+    assertThat(messages.get(1).getMessage().type()).isEqualTo(JupyterMessages.STATUS);
+  }
+
+  @Test
+  public void createMessageWithError_thirdShellMessageHasTypeIsExecuteReply() throws Exception {
+    //given
+    seo.error("some error");
+    //when
+    List<MessageHolder> messages = messageCreator.createMessage(seo);
+    //then
+    assertThat(messages).isNotEmpty();
+    assertThat(messages.get(2).getSocketType()).isEqualTo(SocketEnum.SHELL_SOCKET);
+    assertThat(messages.get(2).getMessage().type()).isEqualTo(JupyterMessages.EXECUTE_REPLY);
+  }
 }

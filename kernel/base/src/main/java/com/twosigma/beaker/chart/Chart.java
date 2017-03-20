d50254a868dbb61200abf4f12adaaf753a23ab4d
Konstantin <konstantin.tarletski@ignite.ee>
2017-03-20 16:14:46 +0200

    PR for issue  #161 "every file must have copyright/license banner if possible" (#211)
    * #161 "every file must have copyright/license banner if possible"
    
    * #161 "every file must have copyright/license banner if possible"

--- kernel/base/src/main/java/com/twosigma/beaker/chart/Chart.java
+++ kernel/base/src/main/java/com/twosigma/beaker/chart/Chart.java
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package com.twosigma.beaker.chart;
 
 import java.util.ArrayList;
--- kernel/base/src/main/java/com/twosigma/beaker/chart/serializer/CategoryLinesSerializer.java
+++ kernel/base/src/main/java/com/twosigma/beaker/chart/serializer/CategoryLinesSerializer.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2014 TWO SIGMA INVESTMENTS, LLC
+ *  Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package com.twosigma.beaker.chart.serializer;
 
 import com.twosigma.beaker.chart.categoryplot.plotitem.CategoryLines;

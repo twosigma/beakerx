70dfb7aa1ddf3e6842088ca5e3e5612cf3317642
vefimov <vefimov@qulix.com>
2017-03-30 13:56:08 +0300

    add tests for ScalaEvaluator class

--- kernel/base/src/test/java/com/twosigma/beaker/evaluator/EvaluatorResultTestWatcher.java
+++ kernel/base/src/test/java/com/twosigma/beaker/evaluator/EvaluatorResultTestWatcher.java
@@ -23,7 +23,7 @@ import static com.twosigma.beaker.jvm.object.SimpleEvaluationObject.EvaluationSt
 
 public class EvaluatorResultTestWatcher {
   public static final int ATTEMPT = 20;
-  public static final int SLEEP_IN_MILLIS = 100;
+  public static final int SLEEP_IN_MILLIS = 500;
 
   public static void waitForResult(SimpleEvaluationObject seo) throws InterruptedException {
     int count = 0;

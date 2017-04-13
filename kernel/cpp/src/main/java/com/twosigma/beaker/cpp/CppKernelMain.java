3fc49b78b8041f54669267456a422365a9d65d09
jarek <jaroslaw.malek@codete.com>
2017-04-13 16:44:53 +0200

    #5062: init

---
+++ kernel/cpp/src/main/java/com/twosigma/beaker/cpp/CppKernelMain.java
@@ -0,0 +1,56 @@
+/*
+ *  Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.twosigma.beaker.cpp;
+
+import com.twosigma.beaker.evaluator.Evaluator;
+import com.twosigma.beaker.jupyter.handler.CommOpenHandler;
+import com.twosigma.jupyter.Kernel;
+import com.twosigma.jupyter.KernelConfigurationFile;
+import com.twosigma.jupyter.KernelRunner;
+import com.twosigma.jupyter.KernelSocketsFactory;
+import com.twosigma.jupyter.KernelSocketsFactoryImpl;
+import com.twosigma.jupyter.handler.KernelHandler;
+import com.twosigma.jupyter.message.Message;
+
+import java.io.IOException;
+
+import static com.twosigma.beaker.jupyter.Utils.uuid;
+
+public class CppKernelMain extends Kernel {
+
+  public CppKernelMain(final String id, final Evaluator evaluator, KernelSocketsFactory kernelSocketsFactory) {
+    super(id, evaluator, kernelSocketsFactory);
+  }
+
+  @Override
+  public CommOpenHandler getCommOpenHandler(Kernel kernel) {
+    return null;
+  }
+
+  @Override
+  public KernelHandler<Message> getKernelInfoHandler(Kernel kernel) {
+    return null;
+  }
+
+  public static void main(final String[] args) throws InterruptedException, IOException {
+    KernelRunner.run(() -> {
+      String id = uuid();
+      KernelSocketsFactoryImpl kernelSocketsFactory = new KernelSocketsFactoryImpl(new KernelConfigurationFile(args));
+      return new CppKernelMain(id, new CppEvaluator(id, id), kernelSocketsFactory);
+    });
+  }
+
+}

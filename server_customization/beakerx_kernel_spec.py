8a8b67009487359b065faaead9ab7fd2fbffd8d5
Scott Draves <spot@draves.org>
2017-04-19 19:03:06 -0400

    first cut, adds a fixed parameter to any jvm kernel

---
+++ server_customization/beakerx_kernel_spec.py
@@ -0,0 +1,23 @@
+# Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from jupyter_client.kernelspec import KernelSpec
+
+class BeakerXKernelSpec(KernelSpec):
+    def __init__(self, **kw):
+        super(BeakerXKernelSpec, self).__init__(**kw)
+        if self.argv[0] == 'java':
+            self.argv[1:1] = ['-Xmx6g']
+            print("XXX hacking kernel spec")
+            print(self.to_dict())
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup_args = dict(
     platforms               = "Linux, Mac OS X, Windows",
     keywords                = ['ipython', 'jupyter', 'extension', 'widgets', 'beaker'],
     include_package_data    = True,
-    packages                = ['beaker'],
+    packages                = ['beaker', 'server_customization'],
     classifiers             = [
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',

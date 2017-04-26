15633942290d3e178f1e213989c79548a6c446c5
Konstantin <konstantin.tarletski@ignite.ee>
2017-04-26 17:14:06 +0300

    failed to install on Windows #5075 (#5256)

--- beakerx/setup.py
+++ beakerx/setup.py
@@ -78,7 +78,7 @@ class NPM(Command):
 
     def has_npm(self):
         try:
-            check_call(['npm', '--version'])
+            check_call(['npm', '--version'], shell = True)
             return True
         except:
             return False

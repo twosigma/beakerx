722427b2ce8c9d76465ff41ae834fd98d7ef815c
jarek <jaroslaw.malek@codete.com>
2017-04-28 16:02:51 +0200

    #5192: wait for idle and result message

--- kernel/base/src/test/java/com/twosigma/beaker/evaluator/EvaluatorResultTestWatcher.java
+++ kernel/base/src/test/java/com/twosigma/beaker/evaluator/EvaluatorResultTestWatcher.java
@@ -54,18 +54,20 @@ public class EvaluatorResultTestWatcher {
 
   public static Optional<Message> waitForIdleMessage(KernelSocketsTest socketsTest) throws InterruptedException {
     int count = 0;
-    Optional<Message> result = getIdleMessage(socketsTest);
-    while (!result.isPresent() && count < ATTEMPT) {
+    Optional<Message> idleMessage = getIdleMessage(socketsTest);
+    boolean idleAndResultPresent = idleMessage.isPresent() && getResult(socketsTest).isPresent();
+    while (!idleAndResultPresent && count < ATTEMPT) {
       Thread.sleep(SLEEP_IN_MILLIS);
-      result = getIdleMessage(socketsTest);
+      idleMessage = getIdleMessage(socketsTest);
+      idleAndResultPresent = idleMessage.isPresent() && getResult(socketsTest).isPresent();
       count++;
     }
-    return result;
+    return idleMessage;
   }
 
   private static Optional<Message> getIdleMessage(KernelSocketsTest socketsTest) {
     return socketsTest.getPublishedMessages().stream().
-            filter(x -> (x.type().equals(JupyterMessages.STATUS)) &&(x.getContent().get("execution_state").equals("idle"))).findFirst();
+            filter(x -> (x.type().equals(JupyterMessages.STATUS)) && (x.getContent().get("execution_state").equals("idle"))).findFirst();
   }
 
   private static Optional<Message> getResult(KernelSocketsTest socketsTest) {

314bbdb5763b62723ef3245d58bb8709a3b75341
Konstantin Tarletski <konstantin.tarletski@ignite.ee>
2017-05-19 14:11:30 +0300

    #5041 "make java side of ipywidgets implementation complete" --- HTMLMath

---
+++ kernel/base/src/main/java/com/twosigma/beaker/widgets/strings/HTMLMath.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.twosigma.beaker.widgets.strings;
+
+/**
+ * Renders the string `value` as HTML, and render mathematics.
+ * 
+ * @author konst
+ *
+ */
+public class HTMLMath extends StringWidget {
+
+  public static final String VIEW_NAME_VALUE = "HTMLMathView";
+  public static final String MODEL_NAME_VALUE = "HTMLMathModel";
+
+  public HTMLMath() {
+    super();
+    openComm();
+  }
+
+  @Override
+  public String getModelNameValue() {
+    return MODEL_NAME_VALUE;
+  }
+
+  @Override
+  public String getViewNameValue() {
+    return VIEW_NAME_VALUE;
+  }
+}

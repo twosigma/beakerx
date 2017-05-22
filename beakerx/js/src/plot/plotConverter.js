83e3b4fd9bab0e5e060e65d86e4790aeda43b904
Jakub Szałek <jakub.szalek@codete.com>
2017-05-22 17:47:26 +0200

    #5321 set correct default color code for corssHair

--- beakerx/js/src/plot/plotConverter.js
+++ beakerx/js/src/plot/plotConverter.js
@@ -353,7 +353,7 @@ define([
       }
 
       if (model.crosshair != null) {
-        var color = model.crosshair.color || '#000000';
+        var color = model.crosshair.color || '#FF000000';
         newmodel.xCursor = {};
         var cursor = newmodel.xCursor;
 

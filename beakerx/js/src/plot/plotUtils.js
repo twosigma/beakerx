52d3305fe50e3033036f3c572419ccbb49a29ce4
Konstantin <konstantin.tarletski@ignite.ee>
2017-05-30 23:11:16 +0300

    PR for issue #5161 "plot interaction API" (#5404)
    * #5161 "plot interaction API"
    
    * #5161 "plot interaction API"
    
    * issue #5161 "plot interaction API" -- Actual change is for "table integration API" - moving functions from "TableDispaly.js" to "tableScope.js"
    
    * #5161 "plot interaction API"  --- temporary fix for pas tests.

--- beakerx/js/src/plot/plotUtils.js
+++ beakerx/js/src/plot/plotUtils.js
@@ -777,6 +777,27 @@ define([
 
       return [r * 255, g * 255, b * 255];
     },
+    getActionObject: function (plotType, e, subplotIndex) {
+      var actionObject = {};
+      if (plotType === "CategoryPlot") {
+        if(e.ele != null){
+          actionObject.category = e.ele.category;
+          actionObject.series = e.ele.series;
+          actionObject.type = "CategoryGraphicsActionObject";
+        }
+      } else {
+        if(plotType === "CombinedPlot") {
+          actionObject.subplotIndex = subplotIndex;
+          actionObject.type =  "CombinedPlotActionObject";
+        } else {
+          actionObject.type = "XYGraphicsActionObject";
+        }
+        if(e.ele != null){
+          actionObject.index = e.ele.index;
+        }
+      }
+      return actionObject;
+    },
     getActualCss: function(jqelement, jqFunction, jqFunctionParams) {
       //to get actual size/position/etc values of hidden elements
       var value;

52e662bf7bf07ebb69d87fd81a2bdb3f4bfa8948
Michał <michalgce@gmail.com>
2017-06-20 10:13:42 +0200

    #5421 Optimize DockerFile's, changed to OpenJDK.

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd /beakerx
+cd /home/beakerx/beakerx
 
 conda create -y -n beakerx python=3.5 jupyter pandas
 source activate beakerx
-../gradlew --no-daemon build
-../gradlew --no-daemon kernelInstall
-../gradlew --no-daemon :beakerx:install
+./gradlew --no-daemon build
+./gradlew --no-daemon kernelInstall
+./gradlew --no-daemon :beakerx:install
 (cd beakerx; pip install -e .)
 python -m beakerx.install --enable --prefix="${CONDA_PREFIX}"

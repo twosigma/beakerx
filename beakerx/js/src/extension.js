9def2faba7e69e5ed00ca91f476a10286bea7e4b
Jakub Szałek <jakub.szalek@codete.com>
2017-06-21 14:43:02 +0200

    #5488 fix Roboto font loader

--- beakerx/js/src/extension.js
+++ beakerx/js/src/extension.js
@@ -29,6 +29,7 @@ if (window.require) {
         }
     });
 }
+__webpack_public_path__ = document.querySelector('body').getAttribute('data-base-url') + 'nbextensions/beakerx/';
 
 require('./../src/shared/style/beakerx.scss');
 require('./../src/plot/bko-combinedplot.css');

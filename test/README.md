0219b0b3b31d10de5fce4e59f192361840e106bd
Vladimir <mailevv@gmail.com>
2017-06-22 17:22:03 +0300

    add test module to use e2e tests (#5440)
    * add test module for e2e tests
    
    * Create first test. Add readme file.
    
    * use webdriver.io and yarn in test module
    
    * test runner script
    
    * check dtcontainer
    
    * revert demo file

---
+++ test/README.md
@@ -0,0 +1,26 @@
+##Dependencies
+Running the e2e tests from its GitHub source code requires Chrome browser version 58+.
+
+##Setting Up a Test Environment
+
+Create a [notebook configuration file](http://jupyter-notebook.readthedocs.io/en/latest/public_server.html#prerequisite-a-notebook-configuration-file):
+
+```
+$ jupyter notebook --generate-config
+```
+
+[Prepare a hashed password](http://jupyter-notebook.readthedocs.io/en/latest/public_server.html#preparing-a-hashed-password) 
+Set up password as 'beakerx':
+
+``` 
+$ jupyter notebook password
+Enter password: beakerx 
+Verify password: beakerx
+[NotebookPasswordApp] Wrote hashed password to /Users/you/.jupyter/jupyter_notebook_config.json
+```
+
+## Run the tests
+
+```
+python ./runner.py
+```
---
+++ test/tests/beakerx.po.js
@@ -0,0 +1,48 @@
+/*
+ *  Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+var BeakerXPageObject = function () {
+
+  this.baseURL = 'http://127.0.0.1:8888/tree/demoFiles';
+  this.kernelIdleIcon = $('i.kernel_idle_icon');
+
+  this.loginJupyter = function () {
+    browser.setValue('#password_input', 'beakerx');
+    browser.click('#login_submit');
+    browser.waitForEnabled('=plotDemo.ipynb');
+  }
+
+  this.clickRunCell = function () {
+    var cssRunCell = 'button[data-jupyter-action="jupyter-notebook:run-cell-and-select-next"]';
+    browser.waitForEnabled(cssRunCell);
+    browser.click(cssRunCell);
+    this.kernelIdleIcon.waitForEnabled();
+  }
+
+  this.getCodeCellByIndex = function (index) {
+    return $$('div.code_cell')[index];
+  }
+
+  this.runCodeCellByIndex = function (index) {
+    var codeCell = this.getCodeCellByIndex(index);
+    codeCell.scroll()
+    codeCell.click();
+    this.clickRunCell();
+    return codeCell;
+  }
+
+};
+module.exports = BeakerXPageObject;

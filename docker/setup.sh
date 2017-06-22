3bcb3e41fd53dd7d80bcffc28f37a9ce2428de11
Michał <michalgce@gmail.com>
2017-06-22 10:04:01 +0200

    #5421 Changed to installing OpenJDK via conda.

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,8 +4,8 @@ cd /home/beakerx/beakerx
 
 conda create -y -n beakerx python=3.5 jupyter pandas
 source activate beakerx
-./gradlew --no-daemon build
-./gradlew --no-daemon kernelInstall
-./gradlew --no-daemon :beakerx:install
+./gradlew build
+./gradlew kernelInstall
+./gradlew :beakerx:install
 (cd beakerx; pip install -e .)
 python -m beakerx.install --enable --prefix="${CONDA_PREFIX}"

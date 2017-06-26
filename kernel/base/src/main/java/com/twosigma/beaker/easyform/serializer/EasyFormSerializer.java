9aeed2903e8afa96429340a26cd72758872a0120
Robert Bidas <rb9@o2.pl>
2017-06-26 10:14:31 +0200

    #5387 change headers

--- kernel/base/src/main/java/com/twosigma/beaker/easyform/serializer/EasyFormSerializer.java
+++ kernel/base/src/main/java/com/twosigma/beaker/easyform/serializer/EasyFormSerializer.java
@@ -1,12 +1,17 @@
 /*
- * Copyright (c) 2017 Two Sigma Investments, LP
- * All Rights Reserved
+ *  Copyright 2014 TWO SIGMA OPEN SOURCE, LLC
  *
- * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
- * Two Sigma Investments, LLC.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * The copyright notice above does not evidence any
- * actual or intended publication of such source code.
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.twosigma.beaker.easyform.serializer;
 

9f8c06452a1f022e8ca87de76bb1a922a54d52af
jaroslawmalekcodete <jaroslaw.malek@codete.com>
2017-07-04 21:10:36 +0200

    jarek: [tests refactoring] wait for idle, result and sent messages (#5587)
    * jarek: wait for idle, result and sent messages
    
    * jarek: reduce SLEEP_IN_MILLIS to 1 millis
    
    * jarek: increase SLEEP_IN_MILLIS to 5 millis
    
    * jarek: increase SLEEP_IN_MILLIS to 10 millis

--- kernel/java/src/test/java/com/twosigma/beakerx/javash/kernel/JavaKernelTest.java
+++ kernel/java/src/test/java/com/twosigma/beakerx/javash/kernel/JavaKernelTest.java
@@ -30,7 +30,9 @@ import java.util.Optional;
 
 import static com.twosigma.MessageAssertions.verifyExecuteReplyMessage;
 import static com.twosigma.beakerx.MessageFactoryTest.getExecuteRequestMessage;
-import static com.twosigma.beakerx.evaluator.EvaluatorResultTestWatcher.waitForResultAndReturnIdleMessage;
+import static com.twosigma.beakerx.evaluator.EvaluatorResultTestWatcher.waitForIdleMessage;
+import static com.twosigma.beakerx.evaluator.EvaluatorResultTestWatcher.waitForResult;
+import static com.twosigma.beakerx.evaluator.EvaluatorResultTestWatcher.waitForSentMessage;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JavaKernelTest {
@@ -60,13 +62,14 @@ public class JavaKernelTest {
     Message message = getExecuteRequestMessage(code);
     //when
     kernelSocketsService.handleMsg(message);
-    kernelSocketsService.handleMsg(message);
-    Optional<Message> idleMessage = waitForResultAndReturnIdleMessage(kernelSocketsService.getKernelSockets());
     //then
+    Optional<Message> idleMessage = waitForIdleMessage(kernelSocketsService.getKernelSockets());
     assertThat(idleMessage).isPresent();
+    waitForResult(kernelSocketsService.getKernelSockets());
     verifyPublishedMsgs(kernelSocketsService);
-    verifySentMsgs(kernelSocketsService);
     verifyResult(kernelSocketsService.getExecuteResultMessage().get());
+    waitForSentMessage(kernelSocketsService.getKernelSockets());
+    verifySentMsgs(kernelSocketsService);
   }
 
   private void verifyPublishedMsgs(KernelSocketsServiceTest service) {

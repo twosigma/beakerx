226d631294664230634ad613c1497daac0af28aa
Konstantin Tarletski <konstantin.tarletski@ignite.ee>
2017-07-09 15:32:27 +0300

    PR for isue #5578 "implement CyclingOutputContainerLayoutManager" -- JS impl #5613 --- FIX

--- kernel/base/src/main/java/com/twosigma/beakerx/jvm/object/CyclingOutputContainerLayoutManager.java
+++ kernel/base/src/main/java/com/twosigma/beakerx/jvm/object/CyclingOutputContainerLayoutManager.java
@@ -15,20 +15,13 @@
  */
 package com.twosigma.beakerx.jvm.object;
 
-import com.twosigma.beakerx.evaluator.InternalVariable;
-import com.twosigma.beakerx.widgets.Widget;
 import com.twosigma.beakerx.widgets.box.CyclingDisplayBox;
-import com.twosigma.beakerx.widgets.box.VBox;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class CyclingOutputContainerLayoutManager extends OutputContainerLayoutManager {
   
   public static final String PERIOD = "period";
   
   private volatile long  period = 5000L;
-  private TimerThread timer = null;
   
   public long getPeriod() {
     return period;
@@ -43,86 +36,6 @@ public class CyclingOutputContainerLayoutManager extends OutputContainerLayoutMa
     CyclingDisplayBox gridView = new CyclingDisplayBox(getWidgets(container));
     gridView.sendUpdate(PERIOD, period);
     gridView.display();
-  }
-  
-  
-  public void display_old(OutputContainer container) {
-    if(timer != null){
-      try {
-        timer.interrupt();
-        timer.join(period);
-      } catch (InterruptedException e) {
-        //nothing to do
-      }
-    }
-    
-    List<WidgetHolder> tabList = new ArrayList<>();
-    for (Widget w : getWidgets(container)) {
-      List<Widget> list = new ArrayList<>();
-      list.add(w);
-      WidgetHolder item = new WidgetHolder();
-      item.box = new VBox(list);
-      item.dispalyed = Boolean.FALSE;
-      tabList.add(item);
-    }
-    
-    timer = new TimerThread(InternalVariable.getSimpleEvaluationObject(), tabList );
-    timer.start();
-  }
-  
-  /**
-   * Needed separate {@code Thread} because in another way Jupyter can not complete {@code display} method.
-   * 
-   * @author konst
-   *
-   */
-  private class TimerThread extends Thread{
-
-    private SimpleEvaluationObject seo;
-    private List<WidgetHolder> tabList;
-    
-    /**
-     * 
-     * @param seo need to be passed to get right "parent message" because it based on {@code ThreadLocal}
-     * @param tabList
-     */
-    private TimerThread(SimpleEvaluationObject seo, List<WidgetHolder> tabList){
-      this.seo = seo;
-      this.tabList = tabList;
-    }
-    
-    @Override
-    public void run() {
-      InternalVariable.setValue(seo);
-      WidgetHolder currentBox = null;
-      boolean display = true;
-      while(display){
-        for (WidgetHolder box : tabList) {
-          if(currentBox != null){
-            currentBox.box.getLayout().setDisplay("none");
-          }
-          currentBox = box;
-          if(!currentBox.dispalyed){
-            currentBox.box.display();
-            currentBox.dispalyed = Boolean.TRUE;
-          }else{
-            currentBox.box.getLayout().setDisplay("");
-          }
-          try {
-            Thread.sleep(period);
-          } catch (InterruptedException e) {
-            display = false;
-            break;
-          }
-        }
-      }
-    }
-    
-  }
-  
-  private class WidgetHolder {
-    private VBox box;
-    private boolean dispalyed = false;
   }
   
 }
--- kernel/base/src/main/java/com/twosigma/beakerx/widgets/Layout.java
+++ kernel/base/src/main/java/com/twosigma/beakerx/widgets/Layout.java
@@ -70,6 +70,7 @@ public class Layout extends Widget {
     content.put("top", "");
     content.put("visibility", "");
     content.put(WIDTH, getWidth());
+
     return content;
   }
 

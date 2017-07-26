2dc70cd4c81905c00f431eefa5e586ee008cef45
Michał Witkowski <michalgce@gmail.com>
2017-07-26 14:46:40 +0200

    #5725 Added adjusting import for Kotlin kernel. (#5743)

---
+++ kernel/kotlin/src/test/java/com/twosigma/beakerx/kotlin/evaluator/object/ImportTestHelper.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.twosigma.beakerx.kotlin.evaluator.object;
+
+/**
+ * This class exists solely for testing the Kotlin import functionality.
+ * The package name (containing "object") is significant.
+ *
+ * @see com.twosigma.beakerx.kotlin.evaluator.KotlinEvaluatorTest
+ */
+
+public class ImportTestHelper {
+  public static void staticMethod() {
+  }
+}

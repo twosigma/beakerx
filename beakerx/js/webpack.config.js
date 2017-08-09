117b35a5a11249bebba539e9ed95e37c39c922d0
Mariusz Jurowicz <mariusz.jurowicz@xsolve.pl>
2017-08-09 12:10:00 +0200

    update embedable package build

--- beakerx/js/webpack.config.js
+++ beakerx/js/webpack.config.js
@@ -15,7 +15,7 @@
  */
 
 var webpack = require('webpack');
-var version = require('./package.json').version;
+var package = require('./package.json');
 var path = require('path');
 
 // Custom webpack loaders are generally the same for all webpack bundles, hence
@@ -132,7 +132,7 @@ module.exports = [
       filename: 'index.js',
       path: path.resolve(__dirname, './dist/'),
       libraryTarget: 'amd',
-      publicPath: 'https://unpkg.com/beakerx@' + version + '/dist/'
+      publicPath: 'https://unpkg.com/' + package.name + '@' + package.version + '/dist/'
     },
     devtool: 'source-map',
     module: {

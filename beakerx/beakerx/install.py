4981dd13be47220202a99fd8cd40af73bc6bf69a
Scott Draves <spot@draves.org>
2017-08-16 18:54:05 -0400

    actually install them, use a different root for the expanded dirs

--- beakerx/beakerx/install.py
+++ beakerx/beakerx/install.py
@@ -16,30 +16,31 @@ import argparse
 import beakerx
 import os
 import shutil
+import subprocess
 import sys
 
 def install_kernels(package_dir):
-    print("install_kernels, package_dir=" + package_dir)
     kernels_dir = os.path.join(package_dir, "static", "kernel")
+    expanded_kernels_dir = os.path.join(package_dir, "static", "expanded_kernel")
     def install_kernel(kernel_dir):
         kernel_name = os.path.basename(kernel_dir)
-        print("install_kernel, kernel_dir=" + kernel_dir)
-        print("kernel_name="+kernel_name)
         sep = ';' if sys.platform == 'win32' else ':'
         classpath = (os.path.abspath(os.path.join(kernels_dir, 'base', 'lib', '*')) + sep +
                      os.path.abspath(os.path.join(kernel_dir, 'lib', '*')))
         classpath = classpath.replace('\\', '/')
-        print(classpath)
         spec_file_name = os.path.join(kernel_dir, 'kernel.json')
         with open(spec_file_name, 'r') as spec_file:
             spec_content = spec_file.read()
         spec_content = spec_content.replace('__PATH__', classpath)
-        expanded_dir = os.path.join(kernel_dir, "classpath_expanded")
+        expanded_dir = os.path.join(expanded_kernels_dir, kernel_name)
         shutil.rmtree(expanded_dir, ignore_errors=True)
         os.makedirs(expanded_dir)
         expanded_spec_file_name = os.path.join(expanded_dir, 'kernel.json')
         with open(expanded_spec_file_name, "w") as expanded_spec_file:
             expanded_spec_file.write(spec_content)
+        install_cmd = ['jupyter', 'kernelspec', 'install', '--sys-prefix',
+                       '--replace', '--name', kernel_name, expanded_dir]
+        subprocess.run(install_cmd, check=True)
     for dir, subdirs, files in os.walk(kernels_dir):
         if 'kernel.json' in files:
             install_kernel(dir)
@@ -53,8 +54,6 @@ def main():
     parser.add_argument("--prefix",
                         help="location of the environment to install into")
     args = parser.parse_args()
-    if args.prefix is not None:
-        print("prefix="+args.prefix)
     install_kernels(os.path.dirname(beakerx.__file__))
 
 if __name__ == "__main__":

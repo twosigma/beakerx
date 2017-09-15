044590401770add49eaadf6e720b4e380bc57068
vefimov <vefimov@qulix.com>
2017-09-15 14:26:46 +0300

    add e2e tests for IntSlider, IntProgress

--- test/tests/javaWidgetsTest.js
+++ test/tests/javaWidgetsTest.js
@@ -33,6 +33,8 @@ describe('Java widgets notebook', function () {
     it('Widget value = 60', function () {
       beakerxPO.kernelIdleIcon.waitForEnabled();
       beakerxPO.runCallAndCheckOutputText(1, '60');
+      var codeCell = beakerxPO.getCodeCellByIndex(0);
+      expect(codeCell.$('div.widget-readout').getText()).toBe('60')
     });
 
     it('Widget value = 76', function () {
@@ -52,6 +54,7 @@ describe('Java widgets notebook', function () {
     it('Cell output contains "false"', function () {
       beakerxPO.kernelIdleIcon.waitForEnabled();
       beakerxPO.runCallAndCheckOutputText(4, 'false');
+
     });
 
     it('Cell output contains "50"', function () {
@@ -62,11 +65,14 @@ describe('Java widgets notebook', function () {
     it('Cell output contains "horizontal"', function () {
       beakerxPO.kernelIdleIcon.waitForEnabled();
       beakerxPO.runCallAndCheckOutputText(6, 'horizontal');
+      var codeCell = beakerxPO.getCodeCellByIndex(0);
+      expect(codeCell.$('div.ui-slider-horizontal').isExisting()).toBeTruthy();
     });
 
     it('Cell output contains "#087636"', function () {
       beakerxPO.kernelIdleIcon.waitForEnabled();
       beakerxPO.runCallAndCheckOutputText(7, '#087636');
+      //TODO setting of color hasn't worked yet
     });
 
     it('Cell output contains "20"', function () {
@@ -77,6 +83,7 @@ describe('Java widgets notebook', function () {
     it('Cell output contains "true"', function () {
       beakerxPO.kernelIdleIcon.waitForEnabled();
       beakerxPO.runCallAndCheckOutputText(9, 'true');
+      //TODO visible hasn't worked yet
     });
   });
 

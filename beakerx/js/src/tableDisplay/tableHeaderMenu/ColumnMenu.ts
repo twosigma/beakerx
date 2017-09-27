bfa6ed3337870dd3412ef0ebbc6763c616357860
mjurowicz-xsolve <mjurowicz-xsolve@users.noreply.github.com>
2017-09-27 17:24:32 +0200

    #6030 handle esc keypress to close menu (#6038)

--- beakerx/js/src/tableDisplay/tableHeaderMenu/ColumnMenu.ts
+++ beakerx/js/src/tableDisplay/tableHeaderMenu/ColumnMenu.ts
@@ -17,23 +17,13 @@
 import $ from 'jquery';
 import HeaderMenu from './HeaderMenu';
 
-interface Dom {
-  container: any,
-  menu: any
-}
-
 export default class ColumnMenu extends HeaderMenu {
-  private dom: Dom;
   private column: any;
 
   constructor(scope, column, cellSettings) {
     super(scope);
     this.column = column;
     this.cell = cellSettings.cell;
-    this.dom = {
-      container:  $(this.dtApi.table().container()),
-      menu:       null
-    };
 
     this.buildMenu();
   }
@@ -46,11 +36,10 @@ export default class ColumnMenu extends HeaderMenu {
     if (!this.cell || !menu || !$.isArray(menu.items)) {
       return;
     }
+
     $(this.cell).append($trigger);
 
-    this.dom.menu = $trigger;
-    this.columnIndex = self.getColumnIndex();
-
+    this.columnIndex = self.getColumnIndex($trigger);
     this.menu.addClass('bko-header-menu');
     this.menu.addClass('dropdown');
     $(this.menu.contentNode).addClass('dropdown-menu');
@@ -58,22 +47,15 @@ export default class ColumnMenu extends HeaderMenu {
     this.createItems(menu.items, this.menu);
   }
 
-  private getColumnIndex(): number {
+  private getColumnIndex($trigger: any): number {
     let columnIndex = this.dtApi.column($(this.cell).index() + ':visible').index();
     const fixedCols = this.dtApi.settings()[0]._oFixedColumns;
     const rightHeader = fixedCols ? fixedCols.dom.clone.right.header : null;
 
-    if (rightHeader && $(rightHeader).has(this.dom.menu).length) {
+    if (rightHeader && $(rightHeader).has($trigger).length) {
       columnIndex = this.dtApi.columns(':visible')[0].length - fixedCols.s.rightColumns + columnIndex;
     }
 
     return columnIndex;
-  }
-
-  destroy(): void {
-    $(document.body).off('click.table-headermenu');
-    $(this.menu.node).off('keyup.keyTable, change');
-    this.dom.container.off('click.headermenu');
-    this.dom.container = null;
   }
 }

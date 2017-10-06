eb9afdc87211c9d2812b906ad88067f7f3c3471c
jarek <jaroslaw.malek@codete.com>
2017-10-06 11:41:12 +0200

    #5965: adapt generic test for kotlin kernel

--- kernel/kotlin/src/test/java/com/twosigma/beakerx/kotlin/kernel/KotlinKernelTest.java
+++ kernel/kotlin/src/test/java/com/twosigma/beakerx/kotlin/kernel/KotlinKernelTest.java
@@ -15,83 +15,21 @@
  */
 package com.twosigma.beakerx.kotlin.kernel;
 
-import com.twosigma.beakerx.KernelCloseKernelAction;
-import com.twosigma.beakerx.KernelSocketsServiceTest;
-import com.twosigma.beakerx.kernel.KernelRunner;
-import com.twosigma.beakerx.kernel.comm.Comm;
+import com.twosigma.beakerx.KernelExecutionTest;
+import com.twosigma.beakerx.kernel.CloseKernelAction;
+import com.twosigma.beakerx.kernel.Kernel;
+import com.twosigma.beakerx.kernel.KernelSocketsFactory;
 import com.twosigma.beakerx.kotlin.evaluator.KotlinEvaluator;
-import com.twosigma.beakerx.message.Message;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
-import java.util.Map;
-import java.util.Optional;
-
-import static com.twosigma.MessageAssertions.verifyExecuteReplyMessage;
-import static com.twosigma.beakerx.MessageFactoryTest.getExecuteRequestMessage;
-import static com.twosigma.beakerx.evaluator.EvaluatorResultTestWatcher.waitForIdleMessage;
-import static com.twosigma.beakerx.evaluator.EvaluatorResultTestWatcher.waitForResult;
-import static com.twosigma.beakerx.evaluator.EvaluatorResultTestWatcher.waitForSentMessage;
 import static com.twosigma.beakerx.evaluator.EvaluatorTest.getTestTempFolderFactory;
 import static com.twosigma.beakerx.evaluator.TestBeakerCellExecutor.cellExecutor;
-import static org.assertj.core.api.Assertions.assertThat;
 
-public class KotlinKernelTest {
+public class KotlinKernelTest extends KernelExecutionTest {
 
-  private Kotlin kernel;
-  private KernelSocketsServiceTest kernelSocketsService;
-  private Thread kernelThread;
-
-  @Before
-  public void setUp() throws Exception {
-    String sessionId = "sessionId2";
-    KotlinEvaluator evaluator =
-            new KotlinEvaluator(sessionId, sessionId, cellExecutor(), getTestTempFolderFactory());
-    kernelSocketsService = new KernelSocketsServiceTest();
-    kernel = new Kotlin(sessionId, evaluator, kernelSocketsService, KernelCloseKernelAction.NO_ACTION);
-    kernelThread = new Thread(() -> KernelRunner.run(() -> kernel));
-    kernelThread.start();
-    kernelSocketsService.waitForSockets();
+  @Override
+  protected Kernel createKernel(String sessionId, KernelSocketsFactory kernelSocketsFactory, CloseKernelAction closeKernelAction) {
+    KotlinEvaluator evaluator = new KotlinEvaluator(sessionId, sessionId, cellExecutor(), getTestTempFolderFactory());
+    return new Kotlin(sessionId, evaluator, kernelSocketsFactory, closeKernelAction);
   }
 
-  @After
-  public void tearDown() throws Exception {
-    kernelSocketsService.shutdown();
-    kernelThread.join();
-  }
-
-  @Test
-  public void evaluate() throws Exception {
-    //given
-    String code = "10/2";
-    Message message = getExecuteRequestMessage(code);
-    //when
-    kernelSocketsService.handleMsg(message);
-    //then
-    Optional<Message> idleMessage = waitForIdleMessage(kernelSocketsService.getKernelSockets());
-    assertThat(idleMessage).isPresent();
-    waitForResult(kernelSocketsService.getKernelSockets());
-    verifyPublishedMsgs(kernelSocketsService);
-    verifyResult(kernelSocketsService.getExecuteResultMessage().get());
-    waitForSentMessage(kernelSocketsService.getKernelSockets());
-    verifySentMsgs(kernelSocketsService);
-  }
-
-  private void verifyPublishedMsgs(KernelSocketsServiceTest service) {
-    assertThat(service.getBusyMessage()).isPresent();
-    assertThat(service.getExecuteInputMessage()).isPresent();
-    assertThat(service.getExecuteResultMessage()).isPresent();
-    assertThat(service.getIdleMessage()).isPresent();
-  }
-
-  private void verifySentMsgs(KernelSocketsServiceTest service) {
-    verifyExecuteReplyMessage(service.getReplyMessage());
-  }
-
-  private void verifyResult(Message result) {
-    Map actual = ((Map) result.getContent().get(Comm.DATA));
-    String value = (String) actual.get("text/plain");
-    assertThat(value).isEqualTo("5");
-  }
 }

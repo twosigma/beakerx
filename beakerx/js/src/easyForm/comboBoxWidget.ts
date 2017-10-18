1901d0ae8648eb0f42bee1e0b80344166523db41
Mariusz Jurowicz <mariusz.jurowicz@xsolve.pl>
2017-10-18 14:34:21 +0200

    #5994 fix comboBox setting value

---
+++ beakerx/js/src/easyForm/comboBoxWidget.ts
@@ -0,0 +1,81 @@
+/*
+ *  Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+declare function require(moduleName: string): any;
+const widgets = require('jupyter-js-widgets');
+const comboBox = require('../comboBox/jQueryComboBox');
+
+import $ from 'jquery';
+
+class ComboBoxModel extends widgets.SelectModel {
+  defaults() {
+    return {
+      ...super.defaults(),
+      _view_name: "ComboBoxView",
+      _model_name: "ComboBoxModel",
+      _model_module: 'beakerx',
+      _view_module: 'beakerx'
+    };
+  }
+}
+
+class ComboBoxView extends widgets.SelectView {
+  render(): void {
+    super.render();
+
+    this.el.classList.add('widget-combobox');
+    this.listbox.setAttribute('easyform-editable', this.model.get('editable'));
+    this.listbox.setAttribute('size', this.model.get('size'));
+
+    setTimeout(() => {
+      $(this.listbox).combobox({
+        change: this.setValueToModel.bind(this),
+      });
+
+      this.update();
+    });
+  }
+
+  setValueToModel(value) {
+    this.model.set('value', value, { updated_view: this });
+    this.touch();
+  }
+
+  update() {
+    super.update();
+
+    let value = this.model.get('value');
+    let editable = this.model.get('editable');
+
+    if (editable) {
+      this.listbox.value = value;
+      this.$el.find('.easyform-combobox-input').val(value);
+      return;
+    }
+
+    for (let option of this.listbox.children) {
+      if (option.value === value) {
+        this.model.set('index', option.index);
+        break;
+      }
+    }
+  }
+}
+
+export default {
+  ComboBoxModel,
+  ComboBoxView
+};

0b0b280c234b2fc1d4ea1baa261859a8d376d35e
Michał Witkowski <michalgce@gmail.com>
2017-10-19 15:49:55 +0200

    #5980 #5982 #5995 #5996 #5997 Added support for INDEX. (#6161)
    * #5980 Added support for INDEX.
    
    * #5980 Fixed test.
    
    * Simplify if statemant.
    
    * #5980 Added backward compatibility for customizing styles.
    
    * #5980 Setting up default value.
    
    * #5980 Rollback backward compatibility. Fixed tests.
    
    * update doc to match code

--- kernel/base/src/test/java/com/twosigma/beakerx/widgets/selections/SelectMultipleTest.java
+++ kernel/base/src/test/java/com/twosigma/beakerx/widgets/selections/SelectMultipleTest.java
@@ -68,9 +68,9 @@ public class SelectMultipleTest {
     //given
     SelectMultiple widget = selectMultiple();
     //when
-    widget.setOptions(new String[]{"2", "3"});
+    widget.setOptions(new String[]{"1", "2"});
     //then
-    verifyMsgForProperty(kernel, SelectMultiple.OPTIONS_LABELS, new String[]{"2", "3"});
+    verifyMsgForProperty(kernel, SelectMultiple.OPTIONS_LABELS, new String[]{"1", "2"});
   }
 
   private SelectMultiple selectMultiple() throws NoSuchAlgorithmException {

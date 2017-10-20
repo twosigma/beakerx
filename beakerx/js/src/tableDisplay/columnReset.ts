eda4cc3f539866e232e58d2d945407c083109e10
Mariusz Jurowicz <mariusz.jurowicz@xsolve.pl>
2017-10-20 12:47:51 +0200

    #5821 reset colorByUnique

--- beakerx/js/src/tableDisplay/columnReset.ts
+++ beakerx/js/src/tableDisplay/columnReset.ts
@@ -14,23 +14,27 @@
  *  limitations under the License.
  */
 
-var MenuHelper = require('./tableHeaderMenu/MenuHelper');
+import MenuHelper from './tableHeaderMenu/MenuHelper';
 
-module.exports = function(TableScope) {
+export default function extendTableScopeWithColumnReset(TableScope: any) {
+  TableScope.prototype.resetColumnHeatmap = function(columnIndex: number, cellHighlighters: any) {
+    const highlighter = this.cellHighlighters[columnIndex];
 
-  TableScope.prototype.resetColumnHeatmap = function(columnIndex, cellHighlighters) {
-    var highlighter = this.cellHighlighters[columnIndex];
     if (highlighter && highlighter instanceof cellHighlighters.HeatmapHighlighter) {
       this.showHideHeatmap(columnIndex);
     }
   };
 
-  TableScope.prototype.resetColumnDataBars = function(columnIndex) {
+  TableScope.prototype.resetColumnDataBars = function(columnIndex: number) {
     this.barsOnColumn.hasOwnProperty(columnIndex) && this.showHideBars(columnIndex);
   };
 
-  TableScope.prototype.resetColumnContainerFixed = function(columnIndex) {
-    var menuHelper = new MenuHelper(this);
+  TableScope.prototype.resetColumnColorByUnique = function(columnIndex: number) {
+    this.haColumnUniqueEntriesHighlighted(columnIndex) && this.showHideUniqueEntries(this.colorder[columnIndex]);
+  };
+
+  TableScope.prototype.resetColumnContainerFixed = function(columnIndex: number) {
+    const menuHelper = new MenuHelper(this);
 
     if (menuHelper.isFixedRight(columnIndex)) {
       this.pagination.fixRight = 0;
@@ -42,35 +46,35 @@ module.exports = function(TableScope) {
     this.updateFixedColumnsSeparator();
   };
 
-  TableScope.prototype.resetColumnTypesAndAlignments = function(columnIndex) {
-    var menuHelper = new MenuHelper(this);
-    var typesAndAllignments = this.getColumnTypeAndAlignment(columnIndex);
+  TableScope.prototype.resetColumnTypesAndAlignments = function(columnIndex: number) {
+    const typesAndAllignments = this.getColumnTypeAndAlignment(columnIndex);
+    const menuHelper = new MenuHelper(this);
 
     this.actualtype[columnIndex - 1] = typesAndAllignments.actualtype;
     menuHelper.doAlignment(columnIndex, typesAndAllignments.actualalign);
   };
 
-  TableScope.prototype.resetColumnFilters = function(columnIndex) {
-    var column = this.table.column(columnIndex);
+  TableScope.prototype.resetColumnFilters = function(columnIndex: number) {
+    const column = this.table.column(columnIndex);
 
     this.clearFilter(column, this.getColumnFilter(column));
     this.checkFilter();
   };
 
-  TableScope.prototype.resetColumnSort = function(columnIndex) {
-    var order = this.table.order();
+  TableScope.prototype.resetColumnSort = function(columnIndex: number) {
+    const order = this.table.order();
 
     order.length && order[0][0] === columnIndex && this.table.order([]).draw();
   };
 
-  TableScope.prototype.resetColumnSearch = function(columnIndex) {
-    var column = this.table.column(columnIndex);
+  TableScope.prototype.resetColumnSearch = function(columnIndex: number) {
+    const column = this.table.column(columnIndex);
 
     column.search('');
     this.checkFilter();
   };
 
-  TableScope.prototype.resetColumnWidth = function(columnIndex) {
+  TableScope.prototype.resetColumnWidth = function(columnIndex: number) {
     this.columnWidth = this.columnWidth.splice(columnIndex, 1);
   };
 };

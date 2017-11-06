fa31be8c4c11ecdc81399844ffb459dd74d32034
Robert Bidas <rb9@o2.pl>
2017-11-06 13:52:09 +0100

    #6234 #6239 JVM options improvements

--- beakerx/beakerx/environment.py
+++ beakerx/beakerx/environment.py
@@ -14,20 +14,31 @@
 
 from os import environ, path
 from jupyter_core import paths
+import json
+
+sadsd = "" \
+        ""
+default_config = "{" \
+                 "\"beakerx\": {" \
+                 "\"jvm_options\": {" \
+                 "\"heap_GB\": \"\"," \
+                 "\"other\": []," \
+                 "\"properties\": {}" \
+                 "}" \
+                 "}" \
+                 "}"
 
 
 class EnvironmentSettings:
-    suffix_other = '_other_'
-    suffix_java = '_java_'
-    _base_var_name = 'beakerx_java_arg'
-    other_var_name = _base_var_name + suffix_other
-    java_var_name = _base_var_name + suffix_java
+    def __init__(self):
+        pass
+
     config_path = path.join(paths.jupyter_config_dir(), 'beakerx.json')
 
     @staticmethod
     def save_setting_to_file(content):
         file = open(EnvironmentSettings.config_path, 'w+')
-        file.write(content)
+        file.write(json.dumps(json.loads(content), indent=4, sort_keys=True))
         file.close()
 
     @staticmethod
@@ -36,61 +47,34 @@ class EnvironmentSettings:
             file = open(EnvironmentSettings.config_path, 'r')
             content = file.read()
         except IOError:
-            content = "{\"payload\": {\"Xmx\": \"\", \"other\": [], \"jvm\": {}}}"
+            content = default_config
+            EnvironmentSettings.save_setting_to_file(default_config)
         else:
             file.close()
 
         return content
 
     @staticmethod
-    def read_beakerx_env_map_settings(suffix=""):
-        args = {}
-
-        for x in environ:
-            if x.startswith(EnvironmentSettings._base_var_name + suffix):
-                args[x] = environ[x]
-
-        return args
-
-    @staticmethod
-    def read_beakerx_env_settings(suffix=""):
+    def read_beakerx_env_settings():
         args = []
 
-        for x in environ:
-            if x.startswith(EnvironmentSettings._base_var_name + suffix):
-                args.append(environ[x])
+        settings = json.loads(EnvironmentSettings.read_setting_from_file())
+        beakerx_settings = settings['beakerx']
+        if 'jvm_options' in beakerx_settings:
+            jvm_settings = beakerx_settings['jvm_options']
+            for x in jvm_settings['other']:
+                args.append(x)
+
+            for x in jvm_settings['properties']:
+                value = '-D' + x + '=' + jvm_settings['properties'][x]
+                args.append(value)
+
+            if 'heap_GB' in jvm_settings and jvm_settings['heap_GB']:
+                val = float(jvm_settings['heap_GB'])
+                if val.is_integer():
+                    value = '-Xmx' + jvm_settings['heap_GB'] + 'g'
+                else:
+                    value = '-Xmx' + str(int(val * 1024)) + 'm'
+                args.append(value)
 
         return args
-
-    @staticmethod
-    def set_beakerx_env_settings(jvm_settings):
-        EnvironmentSettings.clear_beakerx_env_setting()
-        other_var_name = EnvironmentSettings.other_var_name
-
-        n = 1
-        var_name = other_var_name
-        for x in jvm_settings['other']:
-            environ[var_name] = x
-            var_name = other_var_name + str(n)
-            n += 1
-
-        for x in jvm_settings['jvm']:
-            var_name = EnvironmentSettings.java_var_name + str(x)
-            value = '-D' + x + '=' + jvm_settings['jvm'][x]
-            environ[var_name] = value
-
-        if '-Xmx' in jvm_settings and jvm_settings['-Xmx']:
-            var_name = EnvironmentSettings.java_var_name + str(jvm_settings['-Xmx'])
-            val = float(jvm_settings['-Xmx'])
-            if val.is_integer():
-                value = '-Xmx' + jvm_settings['-Xmx'] + 'g'
-            else:
-                value = '-Xmx' + str(int(val * 1024)) + 'm'
-            environ[var_name] = value
-
-
-    @staticmethod
-    def clear_beakerx_env_setting():
-        for x in environ:
-            if x.startswith(EnvironmentSettings._base_var_name):
-                del environ[x]

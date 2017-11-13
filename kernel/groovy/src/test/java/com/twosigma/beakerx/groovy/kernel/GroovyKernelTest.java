7ba1be5f8e631c4bcb627d385787e57a8ea5f2a3
jaroslawmalekcodete <jaroslaw.malek@codete.com>
2017-11-13 16:05:33 +0100

    #6293: fix displaying new boolean[3] by replacing default jvmrepr ToStringDisplayer to our BeakerxToStringDisplayer (#6302)

--- kernel/groovy/src/test/java/com/twosigma/beakerx/groovy/kernel/GroovyKernelTest.java
+++ kernel/groovy/src/test/java/com/twosigma/beakerx/groovy/kernel/GroovyKernelTest.java
@@ -19,14 +19,44 @@ import com.twosigma.beakerx.KernelExecutionTest;
 import com.twosigma.beakerx.kernel.CloseKernelAction;
 import com.twosigma.beakerx.kernel.Kernel;
 import com.twosigma.beakerx.kernel.KernelSocketsFactory;
+import com.twosigma.beakerx.kernel.comm.Comm;
+import com.twosigma.beakerx.message.Message;
+import org.junit.Test;
 
+import java.util.Map;
+import java.util.Optional;
+
+import static com.twosigma.beakerx.MessageFactoryTest.getExecuteRequestMessage;
+import static com.twosigma.beakerx.evaluator.EvaluatorResultTestWatcher.waitForIdleMessage;
+import static com.twosigma.beakerx.evaluator.EvaluatorResultTestWatcher.waitForResult;
 import static com.twosigma.beakerx.groovy.TestGroovyEvaluator.groovyEvaluator;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GroovyKernelTest extends KernelExecutionTest {
 
   @Override
   protected Kernel createKernel(String sessionId, KernelSocketsFactory kernelSocketsFactory, CloseKernelAction closeKernelAction) {
     return new Groovy(sessionId, groovyEvaluator(), kernelSocketsFactory, closeKernelAction);
+  }
+
+  @Test
+  public void evaluateBooleanArray() throws Exception {
+    //given
+    String code = "new boolean[3];";
+    Message message = getExecuteRequestMessage(code);
+    //when
+    kernelSocketsService.handleMsg(message);
+    //then
+    Optional<Message> idleMessage = waitForIdleMessage(kernelSocketsService.getKernelSockets());
+    assertThat(idleMessage).isPresent();
+    waitForResult(kernelSocketsService.getKernelSockets());
+    verifyResult(kernelSocketsService.getExecuteResultMessage().get());
+  }
+
+  private void verifyResult(Message result) {
+    Map actual = ((Map) result.getContent().get(Comm.DATA));
+    String value = (String) actual.get("text/plain");
+    assertThat(value).isEqualTo("[false, false, false]");
   }
 
 }

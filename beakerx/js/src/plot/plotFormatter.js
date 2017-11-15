b4ab38d016832cc6ca6debf4c4e9f11a09a91e77
Mariusz Jurowicz <mjurowicz-xsolve@users.noreply.github.com>
2017-11-15 15:17:05 +0100

    #6291 fix YAxis with bounds crash (#6311)

--- beakerx/js/src/plot/plotFormatter.js
+++ beakerx/js/src/plot/plotFormatter.js
@@ -261,6 +261,7 @@ define([
             }
             return result;
           };
+
           newmodel.vrange = getModelRange(range, newmodel.xAxis.type === "log", newmodel.yAxis.type === "log");
           if(newmodel.yAxisR){
             newmodel.vrangeR = getModelRange(rangeR, newmodel.xAxis.type === "log", newmodel.yAxisR.type === "log");
@@ -278,8 +279,8 @@ define([
           if (focus.xr != null) { vrange.xr = Math.max(focus.xr, vrange.xr); }
           if (focus.yl != null) { vrange.yl = Math.min(focus.yl, vrange.yl); }
           if (focus.yr != null) { vrange.yr = Math.max(focus.yr, vrange.yr); }
-          if (focus.yl_r != null) { vrangeR.yl = Math.min(focus.yl_r, vrangeR.yl); }
-          if (focus.yr_r != null) { vrangeR.yr = Math.max(focus.yr_r, vrangeR.yr); }
+          if (vrangeR && focus.yl_r != null) { vrangeR.yl = Math.min(focus.yl_r, vrangeR.yl); }
+          if (vrangeR && focus.yr_r != null) { vrangeR.yr = Math.max(focus.yr_r, vrangeR.yr); }
 
           var updateRangeSpan = function(r) {
             if (r) {

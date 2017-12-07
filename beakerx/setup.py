e2bbce4120d8141228fa6fc7a8ebbf869e5cae09
Jms Dnns <jdennis@gmail.com>
2017-12-07 10:52:12 -0500

    #6474 removed python 2 and added python 3.6 to setup.py

--- beakerx/setup.py
+++ beakerx/setup.py
@@ -68,12 +68,11 @@ setup_args = dict(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Multimedia :: Graphics',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     entry_points={
         'console_scripts': [

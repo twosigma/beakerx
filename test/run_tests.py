79b6379df8a9c1409b5245fc410b0833df56b614
Scott Draves <spot@draves.org>
2017-12-14 17:34:08 -0500

    clean up test doc, add banner

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,4 +1,6 @@
-﻿# Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+#!/usr/bin/env python
+#
+# Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

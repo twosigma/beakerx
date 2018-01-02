2544f496bcadb5e95e3971d713461c74046f180a
Joe Pallas <pallas@cs.stanford.edu>
2018-01-01 22:05:39 -0800

    Add TreeMap properties

--- kernel/scala/src/test/scala/com/twosigma/beakerx/scala/chart/treemap/TreeMapPropertiesTest.scala
+++ kernel/scala/src/test/scala/com/twosigma/beakerx/scala/chart/treemap/TreeMapPropertiesTest.scala
@@ -20,55 +20,87 @@ import com.twosigma.beakerx.chart.Color
 import com.twosigma.beakerx.chart.treemap.util.{ColorProvider, IToolTipBuilder}
 import com.twosigma.beakerx.chart.treemap.{Mode, ValueAccessor}
 import com.twosigma.beakerx.kernel.KernelManager
-import net.sf.jtreemap.swing.TreeMapNode
-import org.junit.Test
+import net.sf.jtreemap.swing.{DefaultValue, TreeMapNode}
+import org.junit.{Before, Test}
 import org.scalatest.Matchers._
 
 class TreeMapPropertiesTest {
-  @Test
-  def settersAndGetters(): Unit = {
-    val kernel = new KernelTest()
+  var kernel: KernelTest = _
+  var treeMap: TreeMap = _
+
+  @Before
+  def setUp(): Unit = {
+    kernel = new KernelTest()
     KernelManager.register(kernel)
-    val treeMap = new TreeMap()
+    treeMap = new TreeMap()
+  }
 
-    treeMap.colorProvider shouldBe treeMap.getColorProvider
-    val testCP = new ColorProvider {
-      override def getColor(value: TreeMapNode): Color = Color.BLACK
-    }
-    treeMap.colorProvider = testCP
-    treeMap.colorProvider shouldBe testCP
+  @Test
+  def valueAccessor(): Unit = {
+    assume(treeMap.getValueAccessor != ValueAccessor.WEIGHT)
+    treeMap.valueAccessor should contain(treeMap.getValueAccessor)
+    treeMap.valueAccessor = ValueAccessor.WEIGHT
+    treeMap.valueAccessor should contain(ValueAccessor.WEIGHT)
+  }
 
-    treeMap.mode shouldBe empty
-    treeMap.mode = Mode.SLICE
-    treeMap.mode should contain(Mode.SLICE)
-
-    treeMap.ratio shouldBe empty
-    treeMap.ratio = 1.0 / 3.0
-    treeMap.ratio should contain(1.0 / 3.0)
-
-    treeMap.root shouldBe empty
-    val testRoot = new TreeMapNode("Test")
+  @Test
+  def toolTipBuilder(): Unit = {
+    val testRoot = new TreeMapNode("root")
+    testRoot.setValue(new DefaultValue()) // value required for tooltip serialization
     treeMap.root = testRoot
-    treeMap.root should contain(testRoot)
 
-    treeMap.round shouldBe empty
-    treeMap.round = false
-    treeMap.round should contain(false)
-
-    treeMap.sticky shouldBe empty
-    treeMap.sticky = false
-    treeMap.sticky should contain(false)
-
+    require(treeMap.root.isDefined)
     treeMap.toolTipBuilder shouldBe empty
     val testBuilder = new IToolTipBuilder {
       override def getToolTip(node: TreeMapNode): String = "tooltip"
     }
     treeMap.toolTipBuilder = testBuilder
     treeMap.toolTipBuilder should contain(testBuilder)
+  }
 
-    assume(treeMap.getValueAccessor != ValueAccessor.WEIGHT)
-    treeMap.valueAccessor should contain(treeMap.getValueAccessor)
-    treeMap.valueAccessor = ValueAccessor.WEIGHT
-    treeMap.valueAccessor should contain(ValueAccessor.WEIGHT)
+  @Test
+  def sticky(): Unit = {
+    treeMap.sticky shouldBe empty
+    treeMap.sticky = false
+    treeMap.sticky should contain(false)
+  }
+
+  @Test
+  def round(): Unit = {
+    treeMap.round shouldBe empty
+    treeMap.round = false
+    treeMap.round should contain(false)
+  }
+
+  @Test
+  def root(): Unit = {
+    treeMap.root shouldBe empty
+    val testRoot = new TreeMapNode("Test")
+    treeMap.root = testRoot
+    treeMap.root should contain(testRoot)
+  }
+
+  @Test
+  def ratio(): Unit = {
+    treeMap.ratio shouldBe empty
+    treeMap.ratio = 1.0 / 3.0
+    treeMap.ratio should contain(1.0 / 3.0)
+  }
+
+  @Test
+  def mode(): Unit = {
+    treeMap.mode shouldBe empty
+    treeMap.mode = Mode.SLICE
+    treeMap.mode should contain(Mode.SLICE)
+  }
+
+  @Test
+  def colorProvider(): Unit = {
+    treeMap.colorProvider shouldBe treeMap.getColorProvider
+    val testCP = new ColorProvider {
+      override def getColor(value: TreeMapNode): Color = Color.BLACK
+    }
+    treeMap.colorProvider = testCP
+    treeMap.colorProvider shouldBe testCP
   }
 }

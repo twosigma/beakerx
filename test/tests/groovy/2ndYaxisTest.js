ba68b67610fceb2b6078ac40c64d1591c519718f
vefimov <vefimov@qulix.com>
2018-01-10 14:21:03 +0300

    add tests

--- test/tests/groovy/2ndYaxisTest.js
+++ test/tests/groovy/2ndYaxisTest.js
@@ -28,8 +28,26 @@ describe('2nd Y Axis tests', function () {
     beakerxPO.closeAndHaltNotebook();
   });
 
-  function getYPosition(dtContainer, index){
-    return dtContainer.$('g#i' + index + ' > circle#i' + index + '_2').getAttribute('cy');
+  function getAttrFor2ndElement(dtContainer, slctr, index, attr){
+    return dtContainer.$('g#i' + index + ' ' +  slctr + '#i' + index + '_1').getAttribute(attr);
+  }
+
+  function expectToEqualsElements(dtContainer, slctr, attr, i0, i1){
+    var g0_elem2 = getAttrFor2ndElement(dtContainer, slctr, (!i0)?0:i0, attr);
+    var g1_elem2 = getAttrFor2ndElement(dtContainer, slctr, (!i1)?1:i1, attr);
+    expect(g1_elem2).toEqual(g0_elem2);
+  }
+
+  function expect2ndYElementIsHigher(dtContainer, slctr, attr, i0, i1){
+    var g0_elem2 = getAttrFor2ndElement(dtContainer, slctr, (!i0)?0:i0, attr);
+    var g1_elem2 = getAttrFor2ndElement(dtContainer, slctr, (!i1)?1:i1, attr);
+    expect(g1_elem2).toBeLessThan(g0_elem2);
+  }
+
+  function expect2ndYElementIsLower(dtContainer, slctr, attr, i0, i1){
+    var g0_elem2 = getAttrFor2ndElement(dtContainer, slctr, (!i0)?0:i0, attr);
+    var g1_elem2 = getAttrFor2ndElement(dtContainer, slctr, (!i1)?1:i1, attr);
+    expect(g1_elem2).toBeGreaterThan(g0_elem2);
   }
 
   describe('Default scale (lines)', function(){
@@ -38,10 +56,8 @@ describe('2nd Y Axis tests', function () {
       dtContainer = beakerxPO.runCellToGetDtContainer(0);
       expect(dtContainer.$$('g#maing > g').length).toEqual(2);
     });
-    it('Line (2nd Y axis) is under line (1st Y axis)', function(){
-      var line1_point3 = getYPosition(dtContainer, 0);
-      var line2_point3 = getYPosition(dtContainer, 1);
-      expect(line2_point3).toBeGreaterThan(line1_point3);
+    it('Line for 2nd Y axis is under Line for 1st Y axis', function(){
+      expect2ndYElementIsLower(dtContainer, 'circle', 'cy');
     });
   });
 
@@ -51,10 +67,8 @@ describe('2nd Y Axis tests', function () {
       dtContainer = beakerxPO.runCellToGetDtContainer(1);
       expect(dtContainer.$$('g#maing > g').length).toEqual(2);
     });
-    it('Line (2nd Y axis) and line (1st Y axis) have the same 4th point', function(){
-      var line1_point3 = getYPosition(dtContainer, 0);
-      var line2_point3 = getYPosition(dtContainer, 1);
-      expect(line2_point3).toEqual(line1_point3);
+    it('Line for 2nd and 1st Y axises have the same 2nd point', function(){
+      expectToEqualsElements(dtContainer, 'circle', 'cy');
     });
   });
 
@@ -64,30 +78,46 @@ describe('2nd Y Axis tests', function () {
       dtContainer = beakerxPO.runCellToGetDtContainer(2);
       expect(dtContainer.$$('g#maing > g').length).toEqual(2);
     });
-    it('Line (2nd Y axis) is over line (1st Y axis)', function(){
-      var line1_point3 = getYPosition(dtContainer, 0);
-      var line2_point3 = getYPosition(dtContainer, 1);
-      expect(line2_point3).toBeLessThan(line1_point3);
+    it('Line for 2nd Y axis is over Line for 1st Y axis', function(){
+      expect2ndYElementIsHigher(dtContainer, 'circle', 'cy');
     });
   });
 
-  //TODO when will be fixed 2nd Y for categoryBars
+  describe('Default scale (category bars)', function(){
+    var dtContainer;
+    it('Plot has 16 bars', function(){
+      dtContainer = beakerxPO.runCellToGetDtContainer(3);
+      expect(dtContainer.$$('g#maing rect').length).toEqual(16);
+    });
+    it('Bars for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'rect', 'y', 0, 2);
+      expectToEqualsElements(dtContainer, 'rect', 'y', 1, 3);
+    });
+  });
 
-  function getAttrFor2ndElement(dtContainer, slctr, index, attr){
-    return dtContainer.$('g#i' + index + ' ' +  slctr + '#i' + index + '_1').getAttribute(attr);
-  }
+  describe('The same scale (category bars)', function(){
+    var dtContainer;
+    it('Plot has 16 bars', function(){
+      dtContainer = beakerxPO.runCellToGetDtContainer(4);
+      expect(dtContainer.$$('g#maing rect').length).toEqual(16);
+    });
+    it('Bars for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'rect', 'y', 0, 2);
+      expectToEqualsElements(dtContainer, 'rect', 'y', 1, 3);
+    });
+  });
 
-  function expectToEqualsElements(dtContainer, slctr, attr){
-    var g1_elem2 = getAttrFor2ndElement(dtContainer, slctr, 0, attr);
-    var g2_elem2 = getAttrFor2ndElement(dtContainer, slctr, 1, attr);
-    expect(g2_elem2).toEqual(g1_elem2);
-  }
-
-  function expect2ndYElementIsHigher(dtContainer, slctr, attr){
-    var g1_elem2 = getAttrFor2ndElement(dtContainer, slctr, 0, attr);
-    var g2_elem2 = getAttrFor2ndElement(dtContainer, slctr, 1, attr);
-    expect(g2_elem2).toBeLessThan(g1_elem2);
-  }
+  describe('2nd YAxis is zoomed (category bars)', function(){
+    var dtContainer;
+    it('Plot has 16 bars', function(){
+      dtContainer = beakerxPO.runCellToGetDtContainer(5);
+      expect(dtContainer.$$('g#maing rect').length).toEqual(16);
+    });
+    it('Bar for 2nd Y axis is higher than Bar for 1st Y axis', function(){
+      expect2ndYElementIsHigher(dtContainer, 'rect', 'y', 0, 2);
+      expect2ndYElementIsHigher(dtContainer, 'rect', 'y', 1, 3);
+    });
+  });
 
   describe('Default scale (stems)', function(){
     var dtContainer;
@@ -185,6 +215,137 @@ describe('2nd Y Axis tests', function () {
     });
     it('Point for 2nd Y axis is higher than Point for 1st Y axis', function(){
       expect2ndYElementIsHigher(dtContainer, 'circle', 'cy');
+    });
+  });
+
+  describe('Default scale (areas)', function(){
+    var dtContainer;
+    it('Plot has 2 areas', function(){
+      dtContainer = beakerxPO.runCellToGetDtContainer(15);
+      expect(dtContainer.$$('g#maing polygon').length).toEqual(2);
+    });
+    it('Bars for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'rect', 'y');
+    });
+  });
+
+  describe('The same scale (areas)', function(){
+    var dtContainer;
+    it('Plot has 2 areas', function(){
+      dtContainer = beakerxPO.runCellToGetDtContainer(16);
+      expect(dtContainer.$$('g#maing polygon').length).toEqual(2);
+    });
+    it('Bars for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'rect', 'y');
+    });
+  });
+
+  describe('2nd YAxis is zoomed (areas)', function(){
+    var dtContainer;
+    it('Plot has 2 areas', function(){
+      dtContainer = beakerxPO.runCellToGetDtContainer(17);
+      expect(dtContainer.$$('g#maing polygon').length).toEqual(2);
+    });
+    it('Bar for 2nd Y axis is higher than Bar for 1st Y axis', function(){
+      expect2ndYElementIsHigher(dtContainer, 'rect', 'y');
+    });
+  });
+
+  function expectToEqualsAllElements(dtContainer, i0, i1, i2, i3){
+    var g0_elem2 = getAttrFor2ndElement(dtContainer, 'circle', i0, 'cy');
+    var g1_elem2 = getAttrFor2ndElement(dtContainer, 'circle', i1, 'cy');
+    var g2_elem2 = getAttrFor2ndElement(dtContainer, 'rect', i2, 'y');
+    var g3_elem2 = getAttrFor2ndElement(dtContainer, 'line', i3, 'y2');
+    expect(g0_elem2).toEqual(g1_elem2);
+    expect(g0_elem2).toEqual(g2_elem2);
+    expect(g0_elem2).toEqual(g3_elem2);
+  }
+
+  describe('Default scale (lines, points, bars and stems)', function(){
+    var dtContainer;
+    it('Plot has lines, points, bars and stems', function(){
+      dtContainer = beakerxPO.runCellToGetDtContainer(18);
+      expect(dtContainer.$$('g#maing path.plot-line').length).toBeGreaterThan(1);
+      expect(dtContainer.$$('g#maing g.plot-point').length).toBeGreaterThan(1);
+      expect(dtContainer.$$('g#maing g.plot-bar').length).toBeGreaterThan(1);
+      expect(dtContainer.$$('g#maing g.plot-stem').length).toBeGreaterThan(1);
+    });
+    it('Line, Point, Bar and Stem for 1st Y axis have the same y position', function(){
+      expectToEqualsAllElements(dtContainer, 0, 1, 2, 3);
+    });
+    it('Line, Point, Bar and Stem for 2nd Y axis have the same y position', function(){
+      expectToEqualsAllElements(dtContainer, 4, 5, 6, 7);
+    });
+    it('Line for 2nd and 1st Y axises have the same 2nd point', function(){
+      expectToEqualsElements(dtContainer, 'circle', 'cy', 0, 4);
+    });
+    it('Points for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'circle', 'cy', 1, 5);
+    });
+    it('Bars for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'rect', 'y', 2, 6);
+    });
+    it('Stems for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'line', 'y2', 3, 7);
+    });
+
+  });
+
+  describe('The same scale (lines, points, bars and stems)', function(){
+    var dtContainer;
+    it('Plot has lines, points, bars and stems', function(){
+      dtContainer = beakerxPO.runCellToGetDtContainer(19);
+      expect(dtContainer.$$('g#maing path.plot-line').length).toBeGreaterThan(1);
+      expect(dtContainer.$$('g#maing g.plot-point').length).toBeGreaterThan(1);
+      expect(dtContainer.$$('g#maing g.plot-bar').length).toBeGreaterThan(1);
+      expect(dtContainer.$$('g#maing g.plot-stem').length).toBeGreaterThan(1);
+    });
+    it('Line, Point, Bar and Stem for 1st Y axis have the same y position', function(){
+      expectToEqualsAllElements(dtContainer, 0, 1, 2, 3);
+    });
+    it('Line, Point, Bar and Stem for 2nd Y axis have the same y position', function(){
+      expectToEqualsAllElements(dtContainer, 4, 5, 6, 7);
+    });
+    it('Line for 2nd and 1st Y axises have the same 2nd point', function(){
+      expectToEqualsElements(dtContainer, 'circle', 'cy', 0, 4);
+    });
+    it('Points for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'circle', 'cy', 1, 5);
+    });
+    it('Bars for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'rect', 'y', 2, 6);
+    });
+    it('Stems for 2nd and 1st Y axises have the same height', function(){
+      expectToEqualsElements(dtContainer, 'line', 'y2', 3, 7);
+    });
+  });
+
+  describe('2nd YAxis is zoomed (lines, points, bars and stems)', function(){
+    var dtContainer;
+    it('Plot has lines, points, bars and stems', function(){
+      dtContainer = beakerxPO.runCellToGetDtContainer(20);
+      expect(dtContainer.$$('g#maing path.plot-line').length).toBeGreaterThan(1);
+      expect(dtContainer.$$('g#maing g.plot-point').length).toBeGreaterThan(1);
+      expect(dtContainer.$$('g#maing g.plot-bar').length).toBeGreaterThan(1);
+      expect(dtContainer.$$('g#maing g.plot-stem').length).toBeGreaterThan(1);
+    });
+    it('Line, Point, Bar and Stem for 1st Y axis have the same y position', function(){
+      expectToEqualsAllElements(dtContainer, 0, 1, 2, 3);
+    });
+    it('Line, Point, Bar and Stem for 2nd Y axis have the same y position', function(){
+      expectToEqualsAllElements(dtContainer, 4, 5, 6, 7);
+    });
+    it('Line for 2nd Y axis is over Line for 1st Y axis', function(){
+      expect2ndYElementIsHigher(dtContainer, 'circle', 'cy', 0, 4);
+    });
+    it('Point for 2nd Y axis is higher than Point for 1st Y axis', function(){
+      expect2ndYElementIsHigher(dtContainer, 'circle', 'cy', 1, 5);
+    });
+    it('Bar for 2nd Y axis is higher than Bar for 1st Y axis', function(){
+      expect2ndYElementIsHigher(dtContainer, 'rect', 'y', 2, 6);
+    });
+    it('Stem for 2nd Y axis is higher than Stem for 1st Y axis', function(){
+      expect2ndYElementIsHigher(dtContainer, 'line', 'y2', 3, 7);
     });
   });
 

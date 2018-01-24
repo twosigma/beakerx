2f0558033a7bb0439ed0f2df149c9c8a3d6e61d8
Łukasz Pior <piorek82@gmail.com>
2018-01-24 15:29:01 +0100

    #6423 BeakerxApi and Urls for BeakerxTree

---
+++ js/lab/src/tree/BeakerxApi.ts
@@ -0,0 +1,37 @@
+/*
+ *  Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import * as $ from "jquery";
+import Urls from "./Urls";
+
+export default class BeakerxApi {
+
+  public static getVersion(): Promise<string> {
+
+    return new Promise((resolve, reject) => {
+      $.ajax(`${Urls.API_URL}version`, {
+        success: (data, status) => {
+          resolve(data.version);
+        },
+        error: (jqXHR, status, err) => {
+          reject();
+        }
+      });
+
+    });
+  }
+
+}

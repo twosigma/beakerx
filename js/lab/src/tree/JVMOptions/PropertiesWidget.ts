e93860b91653c62b91bd32870744a25106710641
Łukasz Pior <piorek82@gmail.com>
2018-01-29 14:52:13 +0100

    #6423 PropertiesWidget, PropertiesModel

--- js/lab/src/tree/JVMOptions/PropertiesWidget.ts
+++ js/lab/src/tree/JVMOptions/PropertiesWidget.ts
@@ -15,12 +15,25 @@
  */
 
 import * as $ from "jquery";
+import * as _ from "underscore";
+
 import { Widget } from "@phosphor/widgets";
+import { MessageLoop, Message } from "@phosphor/messaging";
+import { Messages } from "./Messages";
+import PropertiesOptionsChangedMessage = Messages.PropertiesOptionsChangedMessage;
+import { IPropertiesJVMOptions } from "../BeakerxApi";
 
-export default class PropertiesWidget extends Widget {
+export class PropertiesWidget extends Widget {
 
+  public readonly ADD_BUTTON_SELECTOR: string = '#add_property_jvm_sett';
+  public readonly PROPERTIES_PANEL_SELECTOR: string = '#properties_property';
 
   public readonly HTML_ELEMENT_TEMPLATE = `
+<style>
+#properties_property .form-control {
+    margin-right: 10px;
+}
+</style>
 <fieldset>
   <div class="panel panel-default">
     <div class="panel-heading">
@@ -41,13 +54,184 @@ export default class PropertiesWidget extends Widget {
 </fieldset>
 `;
 
-  constructor() {
-    super();
-    this.createContent();
+  private _elements: JQuery<HTMLElement>[] = [];
+
+  public get $node(): JQuery<HTMLElement> {
+    return $(this.node);
   }
 
-  private createContent() {
-    $(this.HTML_ELEMENT_TEMPLATE)
-      .appendTo(this.node);
+  constructor() {
+    super();
+
+    $(this.HTML_ELEMENT_TEMPLATE).appendTo(this.node);
+
+    this.$node
+      .find(this.ADD_BUTTON_SELECTOR)
+      .on('click', this.addPropertyButtonClickedHandler.bind(this));
   }
+
+  public onLoad(properties: IPropertiesJVMOptions) {
+    this.clear();
+    for (let property of properties) {
+      this.addPropertyElement(property.name, property.value);
+    }
+  }
+
+  public processMessage(msg: Message): void {
+    switch (msg.type) {
+      case Private.TYPE_ELEMENT_ADDED:
+        this.onElementAdded(msg as Private.ElementAddedMessage);
+        break;
+      case Private.TYPE_ELEMENT_REMOVED:
+        this.onElementRemoved(msg as Private.ElementRemovedMessage);
+        break;
+      default:
+        super.processMessage(msg);
+    }
+  }
+
+  private clear() {
+    this._elements = [];
+    this.$node.find(this.PROPERTIES_PANEL_SELECTOR).empty();
+  }
+
+  private addPropertyButtonClickedHandler(evt) {
+    this.addPropertyElement();
+  }
+
+  private addPropertyElement(name: string = '', value: string = '') {
+    let element = this.createFormRowElement()
+      .append(this.createInputElement('name', name))
+      .append(this.createInputElement('value', value))
+      .append(this.createRemoveButtonElement());
+
+    this._elements.push(element);
+
+    element.appendTo(this.$node.find(this.PROPERTIES_PANEL_SELECTOR));
+
+    MessageLoop.sendMessage(this, new Private.ElementAddedMessage(element));
+  }
+
+  private createFormRowElement(): JQuery<HTMLElement> {
+    return $('<div>', {
+      class: 'form-group form-inline bko-spacing'
+    });
+  }
+
+  private createInputElement(placeholder: string, val: string = ''): JQuery<HTMLElement> {
+    return $('<input>', {
+      class: 'form-control',
+      type: 'text',
+      placeholder: placeholder,
+    }).val(val)
+      .data('val', val);
+  }
+
+  private createRemoveButtonElement(): JQuery<HTMLElement> {
+    return $('<button>', {
+      'type': 'button',
+      'class': 'btn btn-default'
+    }).append(
+      $('<i>', { class: 'fa fa-times'})
+    );
+  }
+
+  private onElementAdded(msg: Private.ElementAddedMessage): void {
+    let addedElement = msg.element;
+    addedElement.find('button')
+      .on('click', {
+        el: addedElement
+      }, this.removePropertyButtonClickedHandler.bind(this));
+
+    this.propertiesChanged();
+
+    addedElement.find('input')
+      .on('keyup', _.debounce(
+        this.inputChangedHandler.bind(this),
+        1000
+      ));
+  }
+
+  private onElementRemoved(msg: Private.ElementRemovedMessage): void {
+    this.propertiesChanged();
+  }
+
+  private inputChangedHandler(evt): void {
+    this.propertiesChanged();
+  }
+
+  private removePropertyButtonClickedHandler(evt) {
+    let elementToRemove = evt.data.el;
+    elementToRemove.remove();
+
+    this._elements = this._elements.filter((el) => {
+      return el !== elementToRemove;
+    });
+
+    MessageLoop.sendMessage(this, new Private.ElementRemovedMessage(elementToRemove));
+  }
+
+  private propertiesChanged() {
+    let msg = new PropertiesOptionsChangedMessage(this.collectProperties());
+    MessageLoop.sendMessage(this.parent, msg);
+  }
+
+  private collectProperties(): IPropertiesJVMOptions {
+    let properties = [];
+    for (const row of this._elements) {
+      let inputs = row.find('input[placeholder]');
+      let name = inputs.eq(0).val();
+      let value = inputs.eq(1).val();
+      if ('' === name) {
+        continue;
+      }
+      properties.push({ name: name, value: value });
+    }
+    return properties;
+  }
+}
+
+export class PropertiesModel {
+  constructor(private widget: PropertiesWidget) {
+
+  }
+
+  update(properties: IPropertiesJVMOptions) {
+    this.widget.onLoad(properties);
+  }
+}
+
+
+namespace Private {
+
+  export const TYPE_ELEMENT_ADDED = 'element-added';
+
+  export class ElementAddedMessage extends Message {
+    constructor(element: JQuery<HTMLElement>) {
+      super(TYPE_ELEMENT_ADDED);
+      this._element = element;
+    }
+
+    public get element(): JQuery<HTMLElement> {
+      return this._element;
+    }
+
+    private _element: JQuery<HTMLElement>;
+  }
+
+  export const TYPE_ELEMENT_REMOVED = 'element-removed';
+
+  export class ElementRemovedMessage extends Message {
+    constructor(element: JQuery<HTMLElement>) {
+      super(TYPE_ELEMENT_REMOVED);
+      this._element = element;
+    }
+
+    public get element(): JQuery<HTMLElement> {
+      return this._element;
+    }
+
+    private _element: JQuery<HTMLElement>;
+  }
+
 }

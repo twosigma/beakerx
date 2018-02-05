b34cfbcc9e49f601e3562688f880757134ce5ac3
vefimov <vefimov@qulix.com>
2018-02-05 12:28:15 +0300

    refactor test for regression

--- test/tests/python/tableAPIPythonTest.js
+++ test/tests/python/tableAPIPythonTest.js
@@ -172,100 +172,13 @@ describe('Testing of table (python)', function () {
     });
   });
 
-  function checkColumnValues(tBody, colIndex, value1, value2, value3){
-    tableHelper.getTableBodyCell(tBody, 0, colIndex).click();
-    expect(tableHelper.getTableBodyCell(tBody, 0, colIndex).getText()).toMatch(value1);
-    expect(tableHelper.getTableBodyCell(tBody, 1, colIndex).getText()).toMatch(value2);
-    expect(tableHelper.getTableBodyCell(tBody, 2, colIndex).getText()).toMatch(value3);
-    browser.keys("Left");
-    browser.keys('\uE000');
-  }
-
-  describe("Pandas data types for TableDisplay", function() {
-
-    it('Create pandas DataFrame with all types', function () {
-      cellIndex += 1;
-      var dtypes = beakerxPO.runCellToGetOutputTextElement(cellIndex).getText();
-      expect(dtypes).toMatch(/A\s*bool/);
-      expect(dtypes).toMatch(/B\s*int64/);
-      expect(dtypes).toMatch(/C\s*uint64/);
-      expect(dtypes).toMatch(/D\s*float64/);
-      expect(dtypes).toMatch(/E\s*complex64/);
-      expect(dtypes).toMatch(/F\s*datetime64.ns./);
-      expect(dtypes).toMatch(/G\s*datetime64.ns, US.Eastern./);
-      expect(dtypes).toMatch(/H\s*timedelta64.ns./);
-      expect(dtypes).toMatch(/I\s*object/);
-      expect(dtypes).toMatch(/J\s*category/);
-    });
-
-    var tBody;
-    var tHead;
+  describe("TableDisplay(pandas DataFrame)", function() {
     it('TableDisplay should display table from pandas dataFrame', function () {
       cellIndex += 1;
       var dtContainer = beakerxPO.runCellToGetDtContainer(cellIndex);
-      tBody = tableHelper.getDataTableBody(dtContainer);
-      tHead = tableHelper.getDataTablesScrollHead(dtContainer);
-      checkTableRows(tBody, 3);
-    });
-
-    it('TableDisplay should display values with "bool" type', function () {
-      var colIndex = 1;
-      checkColumnValues(tBody, colIndex, /True/, /False/, /True/);
-      expect(tHead.getText()).toMatch('A');
-    });
-
-    it('TableDisplay should display values with "int64" type', function () {
-      var colIndex = 2;
-      checkColumnValues(tBody, colIndex, /-10/, /0/, /10/);
-      expect(tHead.getText()).toMatch('B');
-    });
-
-    it('TableDisplay should display values with "uint64" type', function () {
-      var colIndex = 3;
-      checkColumnValues(tBody, colIndex, /100/, /0/, /10/);
-      expect(tHead.getText()).toMatch('C');
-    });
-
-    it('TableDisplay should display values with "float64" type', function () {
-      var colIndex = 4;
-      checkColumnValues(tBody, colIndex, /-10.0/, /0.0/, /10.0/);
-      expect(tHead.getText()).toMatch('D');
-    });
-
-    it('TableDisplay should display  values with "complex64" type', function () {
-      var colIndex = 5;
-      checkColumnValues(tBody, colIndex, /-10.1\+5j/, /(0.2\+0j)/, /(10.3-3j)/);
-      expect(tHead.getText()).toMatch('E');
-    });
-
-    it('TableDisplay should display values with "datetime64[ns]" type', function () {
-      var colIndex = 6;
-      checkColumnValues(tBody, colIndex, /20130101 \d\d:30:00.000/, /20130102 \d\d:30:00.000/, /20130103 \d\d:30:00.000/);
-      expect(tHead.getText()).toMatch('F');
-    });
-
-    it('TableDisplay should display values with "datetime64[ns, timeZone]" type', function () {
-      var colIndex = 7;
-      checkColumnValues(tBody, colIndex, /20130101 \d\d:30:00.000/, /20130102 \d\d:30:00.000/, /20130103 \d\d:30:00.000/);
-      expect(tHead.getText()).toMatch('G');
-    });
-
-    it('TableDisplay should display values with "timedelta64[ns]" type', function () {
-      var colIndex = 8;
-      checkColumnValues(tBody, colIndex, /\d+ nanoseconds/, /\d+ nanoseconds/, /\d+ nanoseconds/);
-      expect(tHead.getText()).toMatch('H');
-    });
-
-    it('TableDisplay should display values with "object" type', function () {
-      var colIndex = 9;
-      checkColumnValues(tBody, colIndex, /tst/, /123/, /0.123/);
-      expect(tHead.getText()).toMatch('I');
-    });
-
-    it('TableDisplay should display values with "category" type', function () {
-      var colIndex = 10;
-      checkColumnValues(tBody, colIndex, /A/, /B/, /C/);
-      expect(tHead.getText()).toMatch('J');
+      var tBody = tableHelper.getDataTableBody(dtContainer);
+      expect(tableHelper.getTableBodyCell(tBody, 0, 1).getText()).toMatch('24');
+      expect(tableHelper.getTableBodyCell(tBody, 1, 1).getText()).toMatch('36L');
     });
   });
 

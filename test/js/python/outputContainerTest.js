86fe3545c44e02a57692833cacc4bf285f05b111
vefimov <vefimov@qulix.com>
2018-03-15 14:36:56 +0300

    add tests for OutputContainers (Python)

---
+++ test/js/python/outputContainerTest.js
@@ -0,0 +1,101 @@
+/*
+ *  Copyright 2018 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+var BeakerXPageObject = require('../beakerx.po.js');
+var beakerxPO;
+
+describe('(Groovy) Output Containers ', function () {
+
+  beforeAll(function () {
+    beakerxPO = new BeakerXPageObject();
+    beakerxPO.runNotebookByUrl('/test/ipynb/python/OutputContainersTest.ipynb');
+  });
+
+  afterAll(function () {
+    beakerxPO.closeAndHaltNotebook();
+  });
+
+  var cellIndex;
+
+  function widgetPlotIsVisible(widget){
+    return widget.isVisible('div#plotLegendContainer');
+  }
+
+  function widgetTableIsVisible(widget){
+    return widget.isVisible('div.bko-table');
+  }
+
+  describe('(Python) Without output. ', function() {
+    it("Cell doesn't have output. ", function () {
+      cellIndex = 0;
+      var codeCell = beakerxPO.runCodeCellByIndex(cellIndex);
+      expect(beakerxPO.getAllOutputAreaChildren(codeCell).length).toBe(0);
+    });
+  });
+
+  describe('(Python) Stacked Output Containers ', function() {
+    it('Cell output contains 4 output containers ', function () {
+      cellIndex += 1;
+      var codeCell = beakerxPO.runCodeCellByIndex(cellIndex);
+      var outputs = beakerxPO.getAllOutputAreaChildren(codeCell);
+      expect(outputs.length).toBe(4);
+      expect(outputs[0].getText()).toMatch(/simplest example/);
+      expect(outputs[1].getText()).toMatch(/2, 3, 5, 7/);
+      expect(outputs[2].$('h1').getText()).toMatch(/title/);
+      expect(outputs[3].getText()).toMatch(/None/);
+    });
+  });
+
+  function clickOnTabByName(output, name){
+    output.click('div.p-TabBar-tabLabel=' + name);
+  }
+
+  function getTabLabelText(output, tabIndex){
+    return output.$$('div.p-TabBar-tabLabel')[tabIndex].getText();
+  }
+
+  describe('(Python) Tabbed Output Containers ', function() {
+    var output;
+    var widgets;
+
+    it('Cell contains Tabbed Output with 4 tabs ', function () {
+      cellIndex += 1;
+      var codeCell = beakerxPO.runCodeCellByIndex(cellIndex);
+      output = beakerxPO.getAllOutputsWidget(codeCell)[0];
+      widgets =  output.$$('div.widget-tab-contents > div.p-Widget > div');
+      expect(widgets.length).toBe(4);
+      expect(getTabLabelText(output, 0)).toMatch(/Scatter with History/);
+      expect(getTabLabelText(output, 1)).toMatch(/Short Term/);
+      expect(getTabLabelText(output, 2)).toMatch(/Long Term/);
+      expect(getTabLabelText(output, 3)).toMatch(/1990\/01/);
+    });
+
+    it('Tabbed Output contains 3 plots and 1 table ', function () {
+      clickOnTabByName(output, 'Scatter with History');
+      expect(widgetPlotIsVisible(widgets[0])).toBeTruthy();
+
+      clickOnTabByName(output, 'Short Term');
+      expect(widgetPlotIsVisible(widgets[1])).toBeTruthy();
+
+      clickOnTabByName(output, 'Long Term');
+      expect(widgetPlotIsVisible(widgets[2])).toBeTruthy();
+
+      clickOnTabByName(output, '1990/01');
+      expect(widgetTableIsVisible(widgets[3])).toBeTruthy();
+    });
+  });
+
+});

53a196c96042b4cd02a1c19a409c7d799dbfe1ab
Łukasz Pior <piorek82@gmail.com>
2018-03-21 13:42:06 +0100

    #6513 fix SparkUIView labels width

--- js/notebook/src/SparkUI.ts
+++ js/notebook/src/SparkUI.ts
@@ -14,27 +14,64 @@
  *  limitations under the License.
  */
 
+import * as $ from "jquery";
+
 const widgets = require('./widgets');
 
 class SparkUIModel extends widgets.VBoxModel {
-    defaults() {
-        return {
-            ...super.defaults(),
-            _view_name: "SparkUIView",
-            _model_name: "SparkUIModel",
-            _model_module: 'beakerx',
-            _view_module: 'beakerx',
-            _model_module_version: BEAKERX_MODULE_VERSION,
-            _view_module_version: BEAKERX_MODULE_VERSION
-        };
-    }
+  defaults() {
+    return {
+      ...super.defaults(),
+      _view_name: "SparkUIView",
+      _model_name: "SparkUIModel",
+      _model_module: 'beakerx',
+      _view_module: 'beakerx',
+      _model_module_version: BEAKERX_MODULE_VERSION,
+      _view_module_version: BEAKERX_MODULE_VERSION
+    };
+  }
 }
 
 class SparkUIView extends widgets.VBoxView {
+  public render() {
+    super.render();
+    this.updateLabels();
+  }
+
+  public update() {
+    super.update();
+    this.updateLabels();
+  }
+
+  private updateLabels() {
+    setTimeout(() => {
+      let labels = this.$el.find('label');
+      let maxW = this.calculateLabelsWidth(labels);
+
+      for(let l of labels) {
+        $(l).outerWidth(maxW);
+      }
+    }, 100);
+  }
+
+  private calculateLabelsWidth(labels): number {
+    let maxW = 0;
+    for(let label of labels) {
+      maxW = Math.max(maxW, this.getLabelWidth(label));
+    }
+
+    return Math.ceil(maxW);
+
+  }
+
+  private getLabelWidth(labelEl): number {
+    return $(labelEl).css({
+      width: 'auto',
+    }).outerWidth();
+  }
 }
 
 export default {
-    SparkUIModel,
-    SparkUIView
+  SparkUIModel,
+  SparkUIView
 };
-

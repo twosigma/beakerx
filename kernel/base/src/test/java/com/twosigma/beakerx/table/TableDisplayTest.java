5f32991fae968ff2bc7f87e32cc8e79347a4ac66
jarek <jaroslaw.malek@codete.com>
2018-03-23 14:10:17 +0100

    downgrade jackson to 2.6.5 and fix test

--- kernel/base/src/test/java/com/twosigma/beakerx/table/TableDisplayTest.java
+++ kernel/base/src/test/java/com/twosigma/beakerx/table/TableDisplayTest.java
@@ -504,7 +504,7 @@ public class TableDisplayTest {
     assertThat(tableDisplay.getStringFormatForTimes()).isEqualTo(days);
     LinkedHashMap model = getModelUpdate();
     assertThat(model.size()).isEqualTo(1);
-    assertThat(model.get(STRING_FORMAT_FOR_TIMES)).isEqualTo(days);
+    assertThat(model.get(STRING_FORMAT_FOR_TIMES)).isEqualTo(days.toString());
   }
 
   @Test

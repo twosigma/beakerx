727ee91ac5768591c2d06fcaf1385de9b336576a
Scott Draves <spot@draves.org>
2018-04-10 10:48:00 -0400

    fixup copyright banners for code copied from spark, add attribution

--- kernel/scala/src/main/scala/com/twosigma/beakerx/scala/evaluator/ScalaInterpreter.scala
+++ kernel/scala/src/main/scala/com/twosigma/beakerx/scala/evaluator/ScalaInterpreter.scala
@@ -1,5 +1,10 @@
 /*
- * Copyright 2018 TWO SIGMA OPEN SOURCE, LLC
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * Spark (https://github.com/apache/spark/blob/master/NOTICE) for
+ * additional information regarding copyright ownership.
+ * 
+ * Modifications copyright (C) 2018 TWO SIGMA OPEN SOURCE, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

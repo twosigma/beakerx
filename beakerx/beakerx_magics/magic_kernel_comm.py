d396f3d697d3f5ca8cd169401eea8e84ce69de90
Lukasz Mitusinski <lukasz.mitusinski@xsolve.pl>
2018-04-19 16:56:03 +0200

    #7205 fixed broken comm in python

--- beakerx/beakerx_magics/magic_kernel_comm.py
+++ beakerx/beakerx_magics/magic_kernel_comm.py
@@ -1,3 +1,16 @@
+# Copyright 2017 TWO SIGMA OPEN SOURCE, LLC  #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from IPython import get_ipython
 import logging
 
@@ -5,7 +18,7 @@ def comm_msg(stream, ident, msg):
     content = msg['content']
     comm_id = content['comm_id']
     comm_manager = get_ipython().kernel.comm_manager
-    comm = comm_manager.comms.get('comm_id')
+    comm = comm_manager.comms.get(comm_id)
     if comm is None:
         magic_registry = comm_manager.kernel.shell.magics_manager.registry
         for magic in magic_registry.values():

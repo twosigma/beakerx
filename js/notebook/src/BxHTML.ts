e7dd37cfb7361c7742f113a0cb6b473c1692b3c1
Mariusz Jurowicz <mariusz.jurowicz@xsolve.pl>
2018-05-08 18:39:42 +0200

    #7274 foldout for classpath add mvn

--- js/notebook/src/BxHTML.ts
+++ js/notebook/src/BxHTML.ts
@@ -15,27 +15,48 @@
  */
 
 const widgets = require('./widgets');
+const accordion = require('jquery-ui/ui/widgets/accordion');
+const CONTENT_MAX_LENGTH = 250;
+const DEFAULT_LABEL_TEXT = 'Output';
 
 class BxHTMLModel extends widgets.HTMLModel {
-    defaults() {
-        return {
-            ...super.defaults(),
-            _view_name: "BxHTMLView",
-            _model_name: "BxHTMLModel",
-            _model_module: 'beakerx',
-            _view_module: 'beakerx',
-            _model_module_version: BEAKERX_MODULE_VERSION,
-            _view_module_version: BEAKERX_MODULE_VERSION
-        };
-    }
+  defaults() {
+    return {
+      ...super.defaults(),
+      _view_name: "BxHTMLView",
+      _model_name: "BxHTMLModel",
+      _model_module: 'beakerx',
+      _view_module: 'beakerx',
+      _model_module_version: BEAKERX_MODULE_VERSION,
+      _view_module_version: BEAKERX_MODULE_VERSION
+    };
+  }
 }
 
 class BxHTMLView extends widgets.HTMLView {
-    render() {
-        super.render();
-        this.content.style.lineHeight = "20px";
-        this.content.style.fontSize = "14px";
+  render() {
+    super.render();
+    this.el.classList.add('bx-html-widget');
+    this.content.style.lineHeight = "20px";
+    this.content.style.fontSize = "14px";
+
+    if (!this.label.innerText) {
+      this.label.innerText = DEFAULT_LABEL_TEXT;
+      this.label.style.display = 'block'
     }
+
+    const $widget: any = $(this.el);
+
+    $widget.accordion({
+      header: 'label',
+      collapsible: true,
+      heightStyle: "content",
+    });
+
+    if (this.content.textContent.length > CONTENT_MAX_LENGTH) {
+      $widget.accordion('option', 'active', false);
+    }
+  }
 }
 
 export default {

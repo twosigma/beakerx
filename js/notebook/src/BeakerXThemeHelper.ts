37d02130ffc5ba2ebbb1372387e7478778157d82
Łukasz Pior <piorek82@gmail.com>
2018-05-30 13:40:08 +0200

    #6688 BeakerXThemeHelper - WIP

---
+++ js/notebook/src/BeakerXThemeHelper.ts
@@ -0,0 +1,73 @@
+/*
+ *  Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { DataGrid } from "@phosphor/datagrid";
+import { DEFAULT_BORDER_COLOR } from "./tableDisplay/dataGrid/style/dataGridStyle";
+
+export default class BeakerXThemeHelper {
+  public static _isDark: boolean = false;
+
+  public static set isDark(isDark: boolean) {
+    this._isDark = isDark;
+  }
+
+  public static getStyle(): DataGrid.IStyle {
+      return this._isDark ?
+        this.getDarkStyle() :
+        this.getLightStyle();
+  }
+
+  public static get DEFAULT_DATA_FONT_COLOR(): string {
+    return this._isDark ? '#ffffff': '#000000';
+  }
+
+  public static get DEFAULT_HEADER_FONT_COLOR(): string {
+    return this._isDark ? '#ffffff': '#515A5A';
+  }
+
+  public static get DEFAULT_HIGHLIGHT_COLOR(): string {
+    return this._isDark ? '#dfdfdf': '#6ba2c7';
+  }
+
+  public static get DEFAULT_CELL_BACKGROUND(): string {
+    return '';
+  }
+
+  public static get FOCUSED_CELL_BACKGROUND(): string {
+    return this._isDark ? 'rgb(99, 99, 99)' : 'rgb(200, 200, 200)';
+  }
+
+  private static getDarkStyle(): DataGrid.IStyle {
+    return {
+    ...DataGrid.defaultStyle,
+      voidColor: '#636363',
+      backgroundColor: '#212121',
+      headerBackgroundColor: '#212121',
+      rowBackgroundColor: i => i % 2 === 0 ? '#424242' : '',
+      gridLineColor: '#111111'
+    };
+  }
+
+  private static getLightStyle(): DataGrid.IStyle {
+    return {
+      ...DataGrid.defaultStyle,
+      voidColor: '#ffffff',
+      headerBackgroundColor: '#E6E6E6',
+      rowBackgroundColor: i => i % 2 === 0 ? '#f9f9f9' : '',
+      gridLineColor: DEFAULT_BORDER_COLOR
+    };
+  }
+}

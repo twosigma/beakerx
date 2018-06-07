4db3dbdf9c3d4ea29c0dc5f2aa037f81da7066a3
Mariusz Jurowicz <mariusz.jurowicz@xsolve.pl>
2018-06-07 11:23:16 +0200

    #7493 fix TableDisplay missing in Lab

--- js/notebook/src/embed.js
+++ js/notebook/src/embed.js
@@ -15,11 +15,11 @@ require('./plot/bko-plot.css');
 
 var loadedModules = [
   require("./Plot"),
-  require("./TableDisplay"),
   require("./EasyForm"),
   require("./TabView"),
   require("./GridView"),
   require("./CyclingDisplayBox"),
+  require("./TableDisplay").default,
   require("./SparkUI").default,
   require("./Spinner").default,
   require("./SparkStateProgress").default,

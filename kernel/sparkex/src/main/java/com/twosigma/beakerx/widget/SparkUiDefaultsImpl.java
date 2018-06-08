b2ff61c262d258da318fc57272ff679690a3b1ab
jaroslawmalekcodete <jaroslaw.malek@codete.com>
2018-06-08 15:21:25 +0200

    #7471: store spark props (#7504)

--- kernel/sparkex/src/main/java/com/twosigma/beakerx/widget/SparkUiDefaultsImpl.java
+++ kernel/sparkex/src/main/java/com/twosigma/beakerx/widget/SparkUiDefaultsImpl.java
@@ -19,21 +19,29 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.twosigma.beakerx.widget.SparkUI.BEAKERX_ID;
 import static com.twosigma.beakerx.widget.SparkUI.SPARK_APP_NAME;
 import static com.twosigma.beakerx.widget.SparkUI.SPARK_EXTRA_LISTENERS;
+import static com.twosigma.beakerx.widget.SparkUIApi.SPARK_EXECUTOR_CORES;
+import static com.twosigma.beakerx.widget.SparkUIApi.SPARK_EXECUTOR_MEMORY;
+import static com.twosigma.beakerx.widget.SparkUIApi.SPARK_MASTER;
 
 public class SparkUiDefaultsImpl implements SparkUiDefaults {
 
+  public static final String NAME = "name";
+  public static final String VALUE = "value";
+  public static final String PROPERTIES = "properties";
   public static final String SPARK_OPTIONS = "spark_options";
   public static final String BEAKERX = "beakerx";
   private Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -44,10 +52,11 @@ public class SparkUiDefaultsImpl implements SparkUiDefaults {
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   public void saveSparkConf(SparkConf sparkConf) {
-    Map<String, String> newSparkConf = getNewSparkConf(sparkConf);
+    Map<String, Object> newSparkConf = toMap(sparkConf);
     try {
-      Map<String, Map> map = beakerxJson(path);
+      Map<String, Map> map = beakerxJsonAsMap(path);
       map.get(BEAKERX).put(SPARK_OPTIONS, newSparkConf);
       String content = gson.toJson(map);
       Files.write(path, content.getBytes(StandardCharsets.UTF_8));
@@ -59,36 +68,76 @@ public class SparkUiDefaultsImpl implements SparkUiDefaults {
   @Override
   public void loadDefaults(SparkSession.Builder builder) {
     SparkConf sparkConf = SparkEngineImpl.getSparkConfBasedOn(builder);
-    try {
-      Map<String, Map> beakerxJson = beakerxJson(path);
-      Map<String, String> map = (Map<String, String>) beakerxJson.get(BEAKERX).get(SPARK_OPTIONS);
-      if (map != null) {
-        map.entrySet().stream()
-                .filter(x -> !sparkConf.contains(x.getKey()))
-                .forEach(x -> builder.config(x.getKey(), x.getValue()));
-      }
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+    Map<String, Map> beakerxJson = beakerxJsonAsMap(path);
+    Map<String, Object> map = getOptions(beakerxJson);
+    if (map != null) {
+      map.entrySet().stream()
+              .filter(x -> !sparkConf.contains(x.getKey()))
+              .forEach(x -> addToBuilder(builder, x.getKey(), x.getValue()));
     }
   }
 
-  private Map<String, Map> beakerxJson(Path path) throws IOException {
-    String pathToFile = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
-    return toMap(pathToFile);
+  @SuppressWarnings("unchecked")
+  private void addToBuilder(SparkSession.Builder builder, String key, Object value) {
+    if (isOneOfMainProp(key)) {
+      builder.config(key, (String) value);
+    } else if (key.equals(PROPERTIES)) {
+      List<Map<String, String>> props = (List<Map<String, String>>) value;
+      props.forEach(x -> builder.config(x.get(NAME), x.get(VALUE)));
+    }
   }
 
-  private Map<String, Map> toMap(String pathToFile) {
-    return (Map<String, Map>) gson.fromJson(pathToFile, Map.class);
+  @SuppressWarnings("unchecked")
+  private Map<String, Object> getOptions(Map<String, Map> beakerxJson) {
+    return (Map<String, Object>) beakerxJson.get(BEAKERX).get(SPARK_OPTIONS);
   }
 
-  private Map<String, String> getNewSparkConf(SparkConf sparkConf) {
-    Map<String, String> result = new HashMap<>();
+  private boolean isOneOfMainProp(String key) {
+    return key.equals(SPARK_MASTER) || key.equals(SPARK_EXECUTOR_MEMORY) || key.equals(SPARK_EXECUTOR_CORES);
+  }
+
+  @SuppressWarnings("unchecked")
+  Map<String, Map> beakerxJsonAsMap(Path path) {
+    String jsonAsString = null;
+    try {
+      jsonAsString = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return (Map<String, Map>) gson.fromJson(jsonAsString, Map.class);
+  }
+
+  private Map<String, Object> toMap(SparkConf sparkConf) {
+    Map<String, Object> result = new HashMap<>();
     Arrays.stream(sparkConf.getAll())
             .filter(x -> !x._1.equals(SPARK_EXTRA_LISTENERS))
             .filter(x -> !x._1.equals(BEAKERX_ID))
             .filter(x -> !x._1.equals(SPARK_APP_NAME))
-            .forEach(x -> result.put(x._1, x._2));
+            .forEach(x -> addToMap(result, x));
     return result;
+  }
+
+  private void addToMap(Map<String, Object> result, Tuple2<String, String> x) {
+    if (isOneOfMainProp(x._1)) {
+      result.put(x._1, x._2);
+    } else {
+      List<Map<String, String>> props = getProps(result);
+      Map<String, String> e = new HashMap<>();
+      e.put(NAME, x._1);
+      e.put(VALUE, x._2);
+      props.add(e);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private List<Map<String, String>> getProps(Map<String, Object> result) {
+    Object propsAsObject = result.get(PROPERTIES);
+    if (propsAsObject == null) {
+      List<Map<String, String>> props = new ArrayList<>();
+      result.put(PROPERTIES, props);
+      return props;
+    }
+    return (List<Map<String, String>>) propsAsObject;
   }
 
 }

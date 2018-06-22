f0642973d993ba33a16b8f5d016d818e4195342a
Curtis Rueden <ctrueden@wisc.edu>
2018-06-22 17:17:38 -0500

    Display image outputs as images by default

--- kernel/base/src/main/java/com/twosigma/beakerx/BeakerxDefaultDisplayers.java
+++ kernel/base/src/main/java/com/twosigma/beakerx/BeakerxDefaultDisplayers.java
@@ -16,9 +16,15 @@
 package com.twosigma.beakerx;
 
 import com.twosigma.beakerx.mimetype.MIMEContainer;
+import com.twosigma.beakerx.util.Images;
 import jupyter.Displayer;
 import jupyter.Displayers;
 
+import java.awt.image.RenderedImage;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,6 +33,7 @@ public class BeakerxDefaultDisplayers {
 
   public static void registerDefaults() {
     registerCodeCellDisplayer();
+    registerImageDisplayer();
   }
 
   private static void registerCodeCellDisplayer() {
@@ -39,6 +46,26 @@ public class BeakerxDefaultDisplayers {
           sb.append("Metadata:").append((value).getMetadata()).append(System.getProperty("line.separator"));
           sb.append("Source:").append((value).getSource());
           put(MIMEContainer.MIME.TEXT_PLAIN, sb.toString());
+        }};
+      }
+    });
+  }
+
+  private static void registerImageDisplayer() {
+    Displayers.register(RenderedImage.class, new Displayer<RenderedImage>() {
+      @Override
+      public Map<String, String> display(RenderedImage value) {
+        return new HashMap<String, String>() {{
+          try {
+            byte[] data = Images.encode(value);
+            String base64 = Base64.getEncoder().encodeToString(data);
+            put(MIMEContainer.MIME.IMAGE_PNG, base64);
+          }
+          catch (IOException exc) {
+            StringWriter sw = new StringWriter();
+            exc.printStackTrace(new PrintWriter(sw));
+            put(MIMEContainer.MIME.TEXT_HTML, "<div><pre>" + sw.toString() + "</pre></div>");
+          }
         }};
       }
     });

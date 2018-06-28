49e64f2030839d328c13200af8ca07b8e03c2ad6
vefimov <vefimov@qulix.com>
2018-06-28 15:46:54 +0300

    add tests for SparkUI

--- test/js/scala/sparkUITest.js
+++ test/js/scala/sparkUITest.js
@@ -28,11 +28,36 @@ describe('Spark UI', function () {
     beakerxPO.closeAndHaltNotebook();
   });
 
-  var cellIndex;
+  function startSparkSession(codeCell){
+    var output = beakerxPO.getAllOutputsWidget(codeCell)[0];
+    output.click('button.p-Widget.bx-spark-connect');
+    browser.waitUntil(function(){
+      return output.$('div.p-Widget.bx-status-panel').isVisible();
+    });
+  }
+
+  function stopSparkSession(codeCell){
+    var output = beakerxPO.getAllOutputsWidget(codeCell)[0];
+    output.click('button.bx-button.icon-close');
+    browser.waitUntil(function(){
+      return !output.isExisting('div.p-Widget.bx-status-panel');
+    });
+  }
+
+  function runSparkCell(cellIndex){
+    var codeCell = beakerxPO.runCodeCellByIndex(cellIndex);
+    browser.waitUntil(function(){
+      return codeCell.$$('div.bx-spark-stagePanel').length > 0;
+    });
+    expect(codeCell.$('div.bx-spark-stagePanel').isVisible()).toBeTruthy();
+    return codeCell;
+  }
+
+  var imageDir = 'scala/sparkgui';
 
   describe('Add Spark jars ', function () {
     it('Should add Spark jars ', function () {
-      cellIndex = 0;
+      var cellIndex = 0;
       beakerxPO.runCodeCellByIndex(cellIndex);
       beakerxPO.kernelIdleIcon.waitForEnabled();
       beakerxPO.waitAndCheckOutputTextOfWidget(cellIndex, /spark-sql/, 1);
@@ -42,10 +67,11 @@ describe('Spark UI', function () {
 
   describe('Spark cell magic ', function () {
     var output;
+    var codeCell;
 
     it('Should display GUI dialog', function () {
-      cellIndex += 1;
-      var codeCell = beakerxPO.runCodeCellByIndex(cellIndex);
+      var cellIndex = 1;
+      codeCell = beakerxPO.runCodeCellByIndex(cellIndex);
       output = beakerxPO.getAllOutputsWidget(codeCell)[0];
       expect(output.$('button.p-Widget.bx-spark-connect').isEnabled()).toBeTruthy();
       expect(output.$('div.p-Widget.bx-spark-profile').isEnabled()).toBeTruthy();
@@ -55,19 +81,62 @@ describe('Spark UI', function () {
     });
 
     it('Should start spark session', function () {
-      output.click('button.p-Widget.bx-spark-connect');
-      browser.waitUntil(function(){
-        return output.$('div.p-Widget.bx-status-panel').isVisible();
-      });
+      startSparkSession(codeCell);
       expect(output.$('div.p-Widget.bx-status-panel').isEnabled()).toBeTruthy();
     });
 
     it('Should stop spark session', function () {
-      output.click('button.bx-button.icon-close');
-      browser.waitUntil(function(){
-        return !output.isExisting('div.p-Widget.bx-status-panel');
-      });
+      stopSparkSession(codeCell);
       expect(output.isExisting('div.p-Widget.bx-status-panel')).toBeFalsy();
+    });
+  });
+
+  describe('Spark session', function () {
+    it('Should calculate PI', function () {
+      var codeCellSpark1 = beakerxPO.getCodeCellByIndex(1);
+      startSparkSession(codeCellSpark1);
+
+      var cellIndex = 2;
+      runSparkCell(cellIndex);
+      beakerxPO.waitAndCheckOutputTextOfStdout(cellIndex, /Pi is roughly \d.\d*/);
+    });
+
+    it('Should display header of table', function () {
+      var cellIndex = 3;
+      var codeCell = runSparkCell(cellIndex);
+      browser.waitUntil(function(){
+        return codeCell.$$('canvas').length > 0;
+      });
+      var canvas = codeCell.$('canvas');
+      var imageData = beakerxPO.getCanvasImageData(canvas, 630, 46);
+      beakerxPO.checkImageData(imageData.value, imageDir, 'cell4_case1.png');
+    });
+
+    it('Should display table', function () {
+      var cellIndex = 4;
+      var codeCell = runSparkCell(cellIndex);
+      browser.waitUntil(function(){
+        return codeCell.$$('canvas').length > 0;
+      });
+      var canvas = codeCell.$('canvas');
+      var imageData = beakerxPO.getCanvasImageData(canvas, 630, 46);
+      beakerxPO.checkImageData(imageData.value, imageDir, 'cell5_case1.png');
+    });
+  });
+
+  describe('Auto Connect', function () {
+    it('Should auto connect to spark session', function () {
+      var codeCellSpark1 = beakerxPO.getCodeCellByIndex(1);
+      stopSparkSession(codeCellSpark1);
+
+      var cellIndex = 5;
+      var codeCell = beakerxPO.runCodeCellByIndex(cellIndex);
+      var output = beakerxPO.getAllOutputsWidget(codeCell)[0];
+      browser.waitUntil(function(){
+        return output.$('div.p-Widget.bx-status-panel').isVisible();
+      });
+      expect(output.$('div.p-Widget.bx-status-panel').isEnabled()).toBeTruthy();
+      stopSparkSession(codeCell);
     });
   });
 

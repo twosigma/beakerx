73b1f852fd3620efc11e0fc650a7412c87f0d187
Scott Draves <spot@draves.org>
2018-06-29 16:37:20 -0400

    only listen for local connections

--- beakerx/beakerx/beakerx_autotranslation_server.py
+++ beakerx/beakerx/beakerx_autotranslation_server.py
@@ -86,7 +86,7 @@ def make_app():
 
 def get_free_tcp_port():
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp.bind(('', 0))
+    tcp.bind(('localhost', 0))
     addr, port = tcp.getsockname()
     tcp.close()
     return port

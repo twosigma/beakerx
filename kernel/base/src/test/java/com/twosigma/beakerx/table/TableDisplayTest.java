56003b9f191e0111fba9e5658f4897b973fefdc2
jarek <jaroslaw.malek@codete.com>
2018-07-03 13:02:19 +0200

    #7628: fix flint demo crashes

--- kernel/base/src/test/java/com/twosigma/beakerx/table/TableDisplayTest.java
+++ kernel/base/src/test/java/com/twosigma/beakerx/table/TableDisplayTest.java
@@ -504,7 +504,7 @@ public class TableDisplayTest {
     assertThat(tableDisplay.getStringFormatForTimes()).isEqualTo(days);
     LinkedHashMap model = getModelUpdate();
     assertThat(model.size()).isEqualTo(1);
-    assertThat(model.get(STRING_FORMAT_FOR_TIMES)).isEqualTo(days.toString());
+    assertThat(model.get(STRING_FORMAT_FOR_TIMES)).isEqualTo(days);
   }
 
   @Test

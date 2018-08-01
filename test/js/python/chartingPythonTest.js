a3159de7633017543d04f781f7cad4ae6748ab77
vefimov <vefimov@qulix.com>
2018-08-01 14:19:22 +0300

    add tests for Python Heatmap

--- test/js/python/chartingPythonTest.js
+++ test/js/python/chartingPythonTest.js
@@ -14,10 +14,13 @@
  *  limitations under the License.
  */
 
+var chartingBaseObject = require('../chartingBase').prototype;
 var BeakerXPageObject = require('../beakerx.po.js');
 var beakerxPO;
 
-describe('Charting python test', function () {
+describe('Charting Python tests ', function () {
+
+  chartingBaseObject.constructor.apply(this, ['Python']);
 
   beforeAll(function () {
     beakerxPO = new BeakerXPageObject();
@@ -26,78 +29,6 @@ describe('Charting python test', function () {
 
   afterAll(function () {
     beakerxPO.closeAndHaltNotebook();
-  });
-
-  var cellIndex;
-
-  describe('Histogram', function () {
-    it('Widget area has dtcontainer', function () {
-      cellIndex = 0;
-      beakerxPO.runCodeCellByIndex(cellIndex);
-      cellIndex += 1;
-      var dtContainer = beakerxPO.runCellToGetDtContainer(cellIndex);
-      expect(dtContainer.$('#maing > g.plot-bar').isEnabled()).toBeTruthy();
-    });
-
-    it('Histogram has Title and Axes Labels', function () {
-      cellIndex += 1;
-      var dtContainer = beakerxPO.runCellToGetDtContainer(cellIndex);
-      expect(dtContainer.$('#plotTitle').getText()).toEqual('Wide Histogram with Manual Parameters');
-      expect(dtContainer.$('#xlabel').getText()).toEqual('Size');
-      expect(dtContainer.$('#ylabel').getText()).toEqual('Count');
-    });
-
-    it('Plot has two histograms (Overlap)', function () {
-      cellIndex += 1;
-      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
-      var g0 = svgElement.$('#maing > g#i0');
-      var g1 = svgElement.$('#maing > g#i1');
-      expect(g0.getCssProperty('fill').value).toEqual('rgb(0,154,166)');
-      expect(g0.getCssProperty('fill-opacity').value).toEqual(1);
-      expect(g1.getCssProperty('fill').value).toEqual('rgb(230,50,50)');
-      expect(g1.getCssProperty('fill-opacity').value).toBeLessThan(1);
-      expect(g0.$$('rect').length).toEqual(25);
-      expect(g1.$$('rect').length).toEqual(25);
-    });
-
-    it('Plot has two histograms (Stack)', function () {
-      cellIndex += 1;
-      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
-      var g0 = svgElement.$('#maing > g#i0');
-      var g1 = svgElement.$('#maing > g#i1');
-      expect(g0.$$('rect').length).toEqual(25);
-      expect(g1.$$('rect').length).toEqual(25);
-    });
-
-    it('Plot has two histograms (Side by Side)', function () {
-      cellIndex += 1;
-      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
-      var g0 = svgElement.$('#maing > g#i0');
-      var g1 = svgElement.$('#maing > g#i1');
-      expect(g0.$$('rect').length).toEqual(25);
-      expect(g1.$$('rect').length).toEqual(25);
-    });
-
-    it('Plot has Cumulative histogram', function () {
-      cellIndex += 1;
-      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
-      var g0 = svgElement.$('#maing > g#i0');
-      expect(g0.$$('rect').length).toEqual(55);
-    });
-
-    it('Plot has Normed histogram', function () {
-      cellIndex += 1;
-      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
-      var g0 = svgElement.$('#maing > g#i0');
-      expect(g0.$$('rect').length).toBeGreaterThan(1);
-    });
-
-    it('Plot has Log histogram', function () {
-      cellIndex += 1;
-      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
-      var g0 = svgElement.$('#maing > g#i0');
-      expect(g0.$$('rect').length).toBeGreaterThan(1);
-    });
   });
 
 });

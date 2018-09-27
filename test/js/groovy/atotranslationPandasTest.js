e4d2d3e3cd2f055bf9d1f15ba1d4a819e9aa87ac
vefimov <vefimov@qulix.com>
2018-09-27 13:13:02 +0300

    add tests for Autotranslation of Pandas

---
+++ test/js/groovy/atotranslationPandasTest.js
@@ -0,0 +1,82 @@
+/*
+ *  Copyright 2018 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+var BeakerXPageObject = require('../beakerx.po.js');
+var beakerxPO;
+
+describe('Autotranslation Pandas', function () {
+
+  beforeAll(function () {
+    beakerxPO = new BeakerXPageObject();
+    beakerxPO.runNotebookByUrl('/test/ipynb/groovy/AutotranslationPandasTest.ipynb');
+  });
+
+  afterAll(function () {
+    beakerxPO.closeAndHaltNotebook();
+  });
+
+  var cellIndex;
+  var imageDir = 'groovy/autoPandas';
+
+  describe('(Groovy kernel) Create DataFrame from file on python ', function () {
+    it("Display pandas DataFrame on python ", function () {
+      cellIndex = 0;
+      var codeCell = beakerxPO.runCodeCellByIndex(cellIndex);
+      expect(beakerxPO.getAllOutputAreaChildren(codeCell).length).toBe(0);
+      browser.pause(1000);
+      cellIndex += 1;
+      var width = 600, height = 90;
+      var canvas = beakerxPO.runCellToGetCanvas(cellIndex);
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      beakerxPO.checkImageData(imageData.value, imageDir, 'cell1_case1.png');
+      // beakerxPO.createTableImage(imageData.value, imageDir, 'cell1_case1.png');
+    });
+
+    it("Display pandas DataFrame on groovy ", function () {
+      cellIndex += 2;
+      var width = 600, height = 90;
+      var canvas = beakerxPO.runCellToGetCanvas(cellIndex);
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      beakerxPO.checkImageData(imageData.value, imageDir, 'cell1_case2.png');
+      // beakerxPO.createTableImage(imageData.value, imageDir, 'cell1_case2.png');
+    });
+  });
+
+  describe('(Groovy kernel) Append column to existing index on python ', function () {
+    it("Display pandas DataFrame on python ", function () {
+      cellIndex += 2;
+      var codeCell = beakerxPO.runCodeCellByIndex(cellIndex);
+      expect(beakerxPO.getAllOutputAreaChildren(codeCell).length).toBe(0);
+
+      cellIndex += 1;
+      var width = 220, height = 100;
+      var canvas = beakerxPO.runCellToGetCanvas(cellIndex);
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      beakerxPO.checkImageData(imageData.value, imageDir, 'cell3_case1.png');
+      // beakerxPO.createTableImage(imageData.value, imageDir, 'cell3_case1.png');
+    });
+
+    it("Display pandas DataFrame on groovy ", function () {
+      cellIndex += 2;
+      var width = 220, height = 100;
+      var canvas = beakerxPO.runCellToGetCanvas(cellIndex);
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      beakerxPO.checkImageData(imageData.value, imageDir, 'cell3_case2.png');
+      // beakerxPO.createTableImage(imageData.value, imageDir, 'cell3_case2.png');
+    });
+  });
+
+});

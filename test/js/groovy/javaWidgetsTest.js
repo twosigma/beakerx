becf319fcdd73f104c12e04f74b39f0792216208
vefimov <vefimov@qulix.com>
2018-10-05 15:07:07 +0300

    add tests for JavaWidgets

--- test/js/groovy/javaWidgetsTest.js
+++ test/js/groovy/javaWidgetsTest.js
@@ -31,94 +31,117 @@ describe('Java widgets notebook test ', function () {
   var cellIndex;
 
   describe('IntSlider widget ', function () {
-    var wdgIndex;
+    var widget;
     it('Cell has IntSlider widget ', function () {
       cellIndex = 0;
-      wdgIndex = cellIndex;
-      var widget = beakerxPO.runCellToGetWidgetElement(cellIndex);
+      widget = beakerxPO.runCellToGetWidgetElement(cellIndex);
       expect(widget.$('div.slider-container').isEnabled()).toBeTruthy();
     });
 
-    it('Widget value = 60 ', function () {
+    it('Get value by code ', function () {
       cellIndex += 1;
       beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /60/);
-      var codeCell = beakerxPO.getCodeCellByIndex(wdgIndex);
-      expect(codeCell.$('div.widget-readout').getText()).toBe('60')
+      expect(widget.$('div.widget-readout').getText()).toBe('60');
+      expect(widget.$('div.ui-slider.ui-widget-content > span').getAttribute('style')).toMatch(/left: 60%/);
     });
 
-    it('Widget value = 76 ', function () {
+    it('Set new value by code ', function () {
       cellIndex += 1;
       beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /76/);
-      var codeCell = beakerxPO.getCodeCellByIndex(wdgIndex);
-      expect(codeCell.$('div.widget-readout').getText()).toBe('76')
+      expect(widget.$('div.widget-readout').getText()).toBe('76');
+      expect(widget.$('div.ui-slider.ui-widget-content > span').getAttribute('style')).toMatch(/left: 76%/);
     });
 
-    it('Widget description = "desc1" ', function () {
+    it('Set description to "desc1" ', function () {
       cellIndex += 1;
       beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /desc1/);
-      var codeCell = beakerxPO.getCodeCellByIndex(wdgIndex);
-      expect(codeCell.$('label.widget-label').getText()).toBe('desc1')
+      expect(widget.$('label.widget-label').getText()).toBe('desc1');
     });
 
-    it('Execute result output contains "false" ', function () {
+    it('Disable widget ', function () {
       cellIndex += 1;
-      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /false/);
+      expect(widget.$('div.ui-slider').getAttribute('class')).not.toMatch(/ui-slider-disabled/);
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /true/);
+      expect(widget.$('div.ui-slider').getAttribute('class')).toMatch(/ui-slider-disabled/);
     });
 
-    it('Execute result output contains "50" ', function () {
+    it('Set max to "250" and min to "50" ', function () {
       cellIndex += 1;
       beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /50/);
+      expect(widget.$('div.widget-readout').getText()).toBe('150');
+      expect(widget.$('span').getAttribute('style')).toMatch(/left: 50%/);
     });
 
-    it('Execute result output contains "horizontal" ', function () {
+    it('Set to vertical orientation ', function () {
       cellIndex += 1;
-      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /horizontal/);
-      var codeCell = beakerxPO.getCodeCellByIndex(wdgIndex);
-      expect(codeCell.$('div.ui-slider-horizontal').isExisting()).toBeTruthy();
+      expect(widget.$('div.ui-slider').getAttribute('class')).toMatch(/ui-slider-horizontal/);
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /vertical/);
+      expect(widget.$('div.ui-slider').getAttribute('class')).toMatch(/ui-slider-vertical/);
+      expect(widget.$('span').getAttribute('style')).toMatch(/bottom: 50%/)
     });
 
-    it('Execute result output contains "20" ', function () {
+    it('Set step to "20" ', function () {
       cellIndex += 1;
-      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /20/);
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /76/);
+      expect(widget.$('span').getAttribute('style')).toMatch(/bottom: 10%/)
     });
 
-    it('Execute result output contains "true" ', function () {
+    it('Change widget color ', function () {
       cellIndex += 1;
-      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /true/);
-      //TODO visible hasn't worked yet
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /#F04080/);
+      expect(widget.$('span').getCssProperty('background-color').value).toEqual('rgba(240,64,128,1)');
     });
   });
 
   describe('IntProgress widget ', function () {
+    var widget;
     it('Cell has IntProgress widget ', function () {
       cellIndex += 1;
-      var widget = beakerxPO.runCellToGetWidgetElement(cellIndex);
+      widget = beakerxPO.runCellToGetWidgetElement(cellIndex);
       expect(widget.$('div.progress').isEnabled()).toBeTruthy();
     });
 
-    it('Widget value = 10 ', function () {
+    it('Get value by code ', function () {
       cellIndex += 1;
-      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /10/);
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /60/);
     });
 
-    it('Widget value = 110 ', function () {
+    it('Set value by code ', function () {
       cellIndex += 1;
-      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /110/);
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /76/);
+      expect(widget.$('div.progress-bar').getAttribute('style')).toMatch(/width: 76%/);
     });
 
-    it('Execute result output contains "50" ', function () {
+    it('Set description to "desc2" ', function () {
       cellIndex += 1;
-      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /50/);
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /desc2/);
+      expect(widget.$('label.widget-label').getText()).toBe('desc2');
     });
 
-    it('Execute result output contains "20" ', function () {
+    it('Set max to "250" and min to "50" ', function () {
       cellIndex += 1;
-      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /20/);
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /150/);
+      expect(widget.$('div.progress-bar').getAttribute('style')).toMatch(/width: 50%/);
     });
 
-    it('Execute result output contains "horizontal" ', function () {
+    it('Set to vertical orientation ', function () {
       cellIndex += 1;
-      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /horizontal/);
+      expect(widget.getAttribute('class')).toMatch(/widget-inline-hbox/);
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /vertical/);
+      expect(widget.getAttribute('class')).toMatch(/widget-inline-vbox/);
+    });
+
+    it('Set step to "20" ', function () {
+      cellIndex += 1;
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /76/);
+      expect(widget.$('div.progress-bar').getAttribute('style')).toMatch(/height: 13%/);
+    });
+
+    it('Set style to "SUCCESS" ', function () {
+      cellIndex += 1;
+      expect(widget.$('div.progress-bar').getAttribute('class')).not.toMatch(/progress-bar-success/);
+      beakerxPO.runAndCheckOutputTextOfExecuteResult(cellIndex, /SUCCESS/);
+      expect(widget.$('div.progress-bar').getAttribute('class')).toMatch(/progress-bar-success/);
     });
   });
 

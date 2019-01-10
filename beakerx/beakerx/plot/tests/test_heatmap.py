cadfe2ad3b30a8a9e1dd4baeb6999bfd4313f734
jaroslawmalekcodete <jaroslaw.malek@codete.com>
2019-01-10 22:10:11 +0100

    #7879: add python unit tests for plots + remove DeprecationWarning (#7915)

---
+++ beakerx/beakerx/plot/tests/test_heatmap.py
@@ -0,0 +1,83 @@
+# Copyright 2019 TWO SIGMA OPEN SOURCE, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest
+from random import randint
+
+from ..chart import HeatMap, XYChart
+
+
+class TestHeatMap(unittest.TestCase):
+
+    def test_empty_data(self):
+        # given
+        # when
+        widget = HeatMap(data=[])
+        # then
+        model = widget.model
+        self.assertFalse(model[XYChart.TOO_MANY_ROWS])
+        self.assertEqual(model[XYChart.TOTAL_NUMBER_OF_POINTS], 0)
+        self.assertEqual(model[XYChart.NUMBER_OF_POINTS_TO_DISPLAY], 0)
+        self.assertEqual(model[XYChart.ROWS_LIMIT_ITEMS], widget.rows_limit)
+
+    def test_xLowerMargin(self):
+        # given
+        # when
+        widget = HeatMap(data=[], xLowerMargin=1.0)
+        # then
+        model = widget.model
+        self.assertEqual(model["x_lower_margin"], 1.0)
+
+    def test_yLowerMargin(self):
+        # given
+        # when
+        widget = HeatMap(data=[], yLowerMargin=2.0)
+        # then
+        model = widget.model
+        self.assertEqual(model["y_lower_margin"], 2.0)
+
+    def test_yUpperMargin(self):
+        # given
+        # when
+        widget = HeatMap(data=[], yUpperMargin=3.0)
+        # then
+        model = widget.model
+        self.assertEqual(model["y_upper_margin"], 3.0)
+
+    def test_should_not_limit_data(self):
+        # given
+        maxdepth = 10
+        data = [[randint(1, 100) for x in range(maxdepth)] for y in range(maxdepth)]
+        # when
+        widget = HeatMap(data=data)
+        # then
+        model = widget.model
+        self.assertFalse(model[XYChart.TOO_MANY_ROWS])
+        self.assertEqual(model[XYChart.TOTAL_NUMBER_OF_POINTS], 100)
+        self.assertEqual(model[XYChart.NUMBER_OF_POINTS_TO_DISPLAY], 100)
+        self.assertEqual(model[XYChart.ROWS_LIMIT_ITEMS], widget.rows_limit)
+
+    def test_should_limit_data(self):
+        # given
+        maxdepth = 1001
+        data = [[randint(1, 100) for x in range(maxdepth)] for y in range(maxdepth)]
+        # when
+        widget = HeatMap(100, 10, data=data)
+        # then
+        model = widget.model
+        self.assertTrue(model[XYChart.TOO_MANY_ROWS])
+        self.assertEqual(model[XYChart.TOTAL_NUMBER_OF_POINTS], 1002001)
+        self.assertEqual(model[XYChart.NUMBER_OF_POINTS_TO_DISPLAY], 10201)
+        self.assertEqual(model[XYChart.ROWS_LIMIT_ITEMS], 100)
---
+++ beakerx/beakerx/plot/tests/test_time_plot.py
@@ -0,0 +1,45 @@
+# Copyright 2019 TWO SIGMA OPEN SOURCE, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest
+
+from ..chart import TimePlot, Line
+
+
+class TestTimePlot(unittest.TestCase):
+
+    def test_plot(self):
+        # given
+        # when
+        plot = TimePlot(timeZone="America/New_York")
+
+        # then
+        model = plot.model
+        self.assertEqual(model['timezone'], "America/New_York")
+
+        self.assertEqual(len(model['rangeAxes']), 1)
+        self.assertEqual(len(model['texts']), 0)
+        self.assertEqual(len(model['constant_lines']), 0)
+        self.assertEqual(len(model['constant_bands']), 0)
+        self.assertEqual(len(model['graphics_list']), 0)
+
+    def test_add_Line_to_plot(self):
+        # given
+        plot = TimePlot()
+        # when
+        plot.add(Line())
+        # then
+        model = plot.model
+        self.assertEqual(len(model['graphics_list']), 1)

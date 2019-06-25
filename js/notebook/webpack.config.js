a0626149a69f973f8ae6f6a5e3de48f8cb607494
Łukasz Pior <piorek82@gmail.com>
2019-06-25 11:56:54 +0200

    #8056 cleanup webpack config

--- js/notebook/webpack.config.js
+++ js/notebook/webpack.config.js
@@ -20,7 +20,6 @@ const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const tsConfigPath = path.resolve(__dirname, './src/tsconfig.json');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // Custom webpack loaders are generally the same for all webpack bundles, hence
 // stored in a separate local variable.
@@ -182,16 +181,7 @@ module.exports = [
       '@jupyter-widgets/jupyterlab-manager',
       '@jupyterlab'
     ]),
-    // plugins: plugins.concat([
-    //   new CopyWebpackPlugin([{
-    //     from: path.resolve(__dirname, './src/types'),
-    //     to: path.resolve(__dirname, '../lab/lib/types')
-    //   },
-    //   {
-    //     from: path.resolve(__dirname, './src/index.d.ts'),
-    //     to: path.resolve(__dirname, '../lab/lib/index.d.ts')
-    //   }])
-    // ])
+    plugins: plugins
   },
   {
     // tree - notebook

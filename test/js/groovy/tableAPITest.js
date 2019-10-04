8db90fe1f4ecee0347e9c90c67f0c290d49bf462
vefimov <vefimov@qulix.com>
2019-10-04 15:08:58 +0300

    add tests for table API

--- test/js/groovy/tableAPITest.js
+++ test/js/groovy/tableAPITest.js
@@ -220,4 +220,68 @@ describe('Testing of table (groovy) ', function () {
     });
   });
 
+  describe('ImageFormat for local files ', function () {
+    it('Should display formatted table ', function() {
+      cellIndex += 1;
+      var width = 740, height = 240;
+      var canvas = beakerxPO.runCellToGetCanvas(cellIndex);
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      beakerxPO.checkImageData(imageData, imageDir, 'cell16_case1.png');
+      // beakerxPO.createTableImage(imageData, imageDir, 'cell16_case1.png');
+    });
+  });
+/*
+  describe('ImageFormat for web files ', function () {
+    it('Should display formatted table ', function() {
+      cellIndex += 2;
+      var width = 470, height = 100;
+      var canvas = beakerxPO.runCellToGetCanvas(cellIndex);
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      // beakerxPO.checkImageData(imageData, imageDir, 'cell17_case1.png');
+      beakerxPO.createTableImage(imageData, imageDir, 'cell17_case1.png');
+    });
+  });
+
+  describe('ImageFormat for encoded data ', function () {
+    it('Should display formatted table ', function() {
+      cellIndex += 2;
+      var width = 150, height = 150;
+      var canvas = beakerxPO.runCellToGetCanvas(cellIndex);
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      // beakerxPO.checkImageData(imageData, imageDir, 'cell18_case1.png');
+      beakerxPO.createTableImage(imageData, imageDir, 'cell18_case1.png');
+    });
+  });
+
+  describe('sendModel() method ', function () {
+    it('Should display table before updating ', function() {
+      cellIndex += 2;
+      var width = 160, height = 90;
+      var canvas = beakerxPO.runCellToGetCanvas(cellIndex);
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      // beakerxPO.checkImageData(imageData, imageDir, 'cell19_case1.png');
+      beakerxPO.createTableImage(imageData, imageDir, 'cell19_case1.png');
+    });
+    it('Should display table after updating ', function() {
+      cellIndex += 1;
+      var width = 160, height = 90;
+      beakerxPO.runCodeCellByIndex(cellIndex);
+      var canvas = beakerxPO.getCodeCellByIndex(cellIndex - 1).$('canvas');
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      // beakerxPO.checkImageData(imageData, imageDir, 'cell19_case2.png');
+      beakerxPO.createTableImage(imageData, imageDir, 'cell19_case2.png');
+    });
+  });
+
+  describe('RowFilter ', function () {
+    it('Should display formatted table ', function() {
+      cellIndex += 3;
+      var width = 230, height = 70;
+      var canvas = beakerxPO.runCellToGetCanvas(cellIndex);
+      var imageData = beakerxPO.getCanvasImageData(canvas, width, height);
+      // beakerxPO.checkImageData(imageData, imageDir, 'cell20_case1.png');
+      beakerxPO.createTableImage(imageData, imageDir, 'cell20_case1.png');
+    });
+  }); */
+
 });

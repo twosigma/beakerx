0956057dc638e8195d85136f9306bb6419b8bbf5
vefimov <vefimov@qulix.com>
2019-10-29 17:12:02 +0300

    add tests for Plots (python)

--- test/js/python/plotPythonTest.js
+++ test/js/python/plotPythonTest.js
@@ -90,4 +90,176 @@ describe('Testing of Plot (python)', function () {
     });
   });
 
+  describe('Run cell with points', function(){
+    it('Plot has points', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      expect(plotHelper.getAllPointsByGIndexAndType(svgElement, 0, 'rect').length).toBeGreaterThan(0);
+    });
+
+    it('Should sets point colors using lists', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      expect(svgElement.$('rect#i0_0').getCSSProperty('fill').value).toEqual('rgb(0,0,0)');
+      expect(svgElement.$('rect#i0_1').getCSSProperty('fill').value).toEqual('rgb(255,0,0)');
+    });
+  });
+
+  describe('Run cell with areas', function(){
+    it('Plot has 2 areas', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      expect(plotHelper.getAllAreas(svgElement).length).toEqual(2);
+    });
+
+    it('Plot has area with base', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      expect(plotHelper.getAllAreas(svgElement).length).toEqual(1);
+    });
+  });
+
+  describe('Run cell with stacking', function(){
+    it('Plot has 2 areas', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      expect(plotHelper.getAllAreas(svgElement).length).toEqual(2);
+    });
+  });
+
+  describe('Run cell with constant lines', function(){
+    it('Plot has 4 constant lines', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      expect(plotHelper.getAllConstLines(svgElement).length).toEqual(4);
+    });
+  });
+
+  describe('Run cell with constant bands', function(){
+    it('Plot has constant band', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      expect(plotHelper.getAllConstBands(svgElement).length).toBeGreaterThan(0);
+    });
+
+    it('Should sets constant band color', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      expect(svgElement.$('#i1.plot-constband').getCSSProperty('fill').value).toEqual('rgb(128,128,128)');
+    });
+  });
+
+  describe('Run cell with text', function(){
+    it('Plot has 8 text elements', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      expect(plotHelper.getAllTexts(svgElement).length).toEqual(8);
+    });
+  });
+
+  describe('Run cell with crosshair', function(){
+    it('Plot has crosshair', function(){
+      cellIndex += 1;
+      var svgElement = beakerxPO.runCellToGetSvgElement(cellIndex);
+      beakerxPO.performMouseMove(svgElement.$('g#maing'), 100, 100);
+      var dtcontainer = beakerxPO.getDtContainerByIndex(cellIndex);
+      expect(dtcontainer.$('div#cursor_xlabel').isDisplayed()).toBeTruthy();
+      expect(dtcontainer.$('div#cursor_ylabel').isDisplayed()).toBeTruthy();
+    });
+  });
+
+  describe('Pandas library', function () {
+    it('Plot has Bars', function () {
+      var svgElement = beakerxPO.runCellToGetSvgElement(9);
+      expect(svgElement.$('g.plot-bar').isDisplayed()).toBeTruthy();
+      expect(svgElement.$$('rect.plot-resp').length).toBeGreaterThan(0);
+    });
+
+    it('Plot has Line', function () {
+      var svgElement = beakerxPO.runCellToGetSvgElement(10);
+      expect(svgElement.$$('circle.plot-resp').length).toEqual(7);
+      expect(svgElement.$('path.plot-line').isDisplayed()).toBeTruthy();
+    });
+  });
+
+  describe('Plot Stacking', function(){
+    it('Plot has 2 Areas', function(){
+      var svgElement = beakerxPO.runCellToGetSvgElement(11);
+      expect(svgElement.$$('g > polygon.plot-area').length).toEqual(2);
+      expect(svgElement.$('rect#i0_0').getLocation('y')).toBeGreaterThan(svgElement.$('rect#i1_0').getLocation('y'));
+      expect(svgElement.$('rect#i0_0').getLocation('x')).toBe(svgElement.$('rect#i1_0').getLocation('x'));
+    });
+  });
+
+  describe('Simple Time Plot', function(){
+    it('Time Plot has 2 lines and time axis', function(){
+      beakerxPO.kernelIdleIcon.waitForEnabled();
+      var svgElement = beakerxPO.runCellToGetSvgElement(12);
+      expect(svgElement.$$('g#i0 > circle').length).toBeGreaterThan(0);
+      expect(svgElement.$$('g#i1 > circle').length).toBeGreaterThan(0);
+      expect(svgElement.$('g#i0 > path.plot-line').isDisplayed()).toBeTruthy();
+      expect(svgElement.$('g#i1 > path.plot-line').isDisplayed()).toBeTruthy();
+      expect(svgElement.$('text#label_x_0').getText()).toEqual('1990');
+    });
+
+    it('Time Plot has millisecond resolution', function(){
+      beakerxPO.kernelIdleIcon.waitForEnabled();
+      var dtContainer = beakerxPO.runCellToGetDtContainer(13);
+      var svgElement = dtContainer.$('#svgg');
+      svgElement.$('rect#i0_0').click();
+      var tipElement = dtContainer.$('div#tip_i0_0');
+      expect(tipElement.getText()).toMatch('x: 2017 Oct 09 Mon, 05:26:41 .624');
+    });
+
+    it('Time Plot has nanosecond resolution', function(){
+      beakerxPO.kernelIdleIcon.waitForEnabled();
+      var dtContainer = beakerxPO.runCellToGetDtContainer(14);
+      var svgElement = dtContainer.$('#svgg');
+      svgElement.$('rect#i0_1').click();
+      var tipElement = dtContainer.$('div#tip_i0_1');
+      expect(tipElement.getText()).toMatch('x: 2017 Oct 09 Mon, 09:26:41.624000007');
+    });
+  });
+
+  describe('Second Y Axis', function(){
+    it('Plot has second Y Axis', function(){
+      var svgElement = beakerxPO.runCellToGetSvgElement(15);
+      expect(svgElement.$('text#yrlabel').getText()).toEqual('Test y axis');
+      expect(svgElement.$('text#label_yr_0').getText()).toEqual('10');
+      expect(svgElement.$('line#tick_yr_0').isEnabled()).toBeTruthy();
+    });
+  });
+
+  function getCircleAttr(elem, index, attribute){
+    return elem.$('circle#' + index).getAttribute(attribute)
+  }
+
+  function getMaingG(elem, index){
+    return elem.$$('div.dtcontainer')[index].$('g#i0');
+  }
+
+  describe('Logarithmic Scale', function(){
+    var combPlot;
+
+    it('Combined plot has 3 plots', function(){
+      combPlot = beakerxPO.runCodeCellByIndex(16).$('div.combplot-plotcontainer');
+      expect(combPlot.isDisplayed()).toBeTruthy();
+      expect(combPlot.$$('div.dtcontainer').length).toEqual(3);
+    });
+
+    it('Second plot has Log Y axis', function(){
+      var g0 = getMaingG(combPlot, 0);
+      var g1 = getMaingG(combPlot, 1);
+      expect(getCircleAttr(g0, 'i0_0', 'cy')).toBe(getCircleAttr(g1, 'i0_0', 'cy'));
+      expect(getCircleAttr(g0, 'i0_50', 'cy')).toBeGreaterThan(getCircleAttr(g1, 'i0_50', 'cy'));
+    });
+
+    it('Third plot has Log X axis', function(){
+      var g0 = getMaingG(combPlot, 0);
+      var g2 = getMaingG(combPlot, 2);
+      expect(getCircleAttr(g0, 'i0_0', 'cx')).toBe(getCircleAttr(g2, 'i0_0', 'cx'));
+      expect(getCircleAttr(g0, 'i0_50', 'cx')).toBeLessThan(getCircleAttr(g2, 'i0_50', 'cx'));
+    });
+  }); 
+
 });

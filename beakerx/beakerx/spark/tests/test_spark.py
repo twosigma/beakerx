cd9485e64437c9de44564f18d7c42ec74d509179
jaroslawmalekcodete <jaroslaw.malek@codete.com>
2020-02-19 11:38:18 +0100

    #8189: spark python magic command -> start and stop event tests

---
+++ beakerx/beakerx/spark/tests/test_spark.py
@@ -0,0 +1,143 @@
+# Copyright 2020 TWO SIGMA OPEN SOURCE, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+
+from beakerx import SparkUI2
+from ipykernel.comm import Comm
+
+
+class TestSparkUI(unittest.TestCase):
+
+    def test_should_send_completed_message_when_sc_starts(self):
+        # given
+        builder = BuilderMock()
+        ipython_manager = IpythonManagerMock()
+        spark_server_factory = SparkServerFactoryMock()
+        sui = SparkUI2(builder, ipython_manager, spark_server_factory)
+        sui.comm = CommMock()
+        msg_start = {
+            'event': 'start',
+            'payload': {
+                'spark.executor.memory': '8g',
+                'spark.master': 'local[10]',
+                'properties': []
+            }
+        }
+        # when
+        sui.handle_msg(sui, msg_start)
+        # then
+        self.assertTrue(sui.comm.message["method"] == "update")
+        event = sui.comm.message["event"]
+        self.assertTrue(event["start"] == "completed")
+
+    def test_should_not_create_sc_when_builder_is_None(self):
+        # given
+        builder = None
+        spark_server_factory = SparkServerFactoryMock()
+        ipython = IpythonManagerMock()
+        # when
+        try:
+            SparkUI2(builder, ipython, spark_server_factory)
+            self.fail("builder is None")
+        except Exception as err:
+            self.assertTrue("value can not be None" in str(err), "Should not create SparkUI when builder is None")
+        # then
+
+    def test_should_not_create_sc_when_ipython_is_None(self):
+        # given
+        builder = BuilderMock()
+        spark_server_factory = SparkServerFactoryMock()
+        ipython = None
+        # when
+        try:
+            SparkUI2(builder, ipython, spark_server_factory)
+            self.fail("ipython is None")
+        except Exception as err:
+            self.assertTrue("value can not be None" in str(err), "Should not create SparkUI when ipython is None")
+        # then
+
+    def test_should_not_create_sc_when_factory_is_None(self):
+        # given
+        builder = BuilderMock()
+        ipython = IpythonManagerMock()
+        spark_server_factory = None
+        # when
+        try:
+            SparkUI2(builder, ipython, spark_server_factory)
+            self.fail("spark server factory is None")
+        except Exception as err:
+            self.assertTrue("value can not be None" in str(err), "Should not create SparkUI when factory is None")
+        # then
+
+    def test_should_create_sc(self):
+        # given
+        spark_server_factory = SparkServerFactoryMock()
+        builder = BuilderMock()
+        ipython = IpythonManagerMock()
+        # when
+        spark_ui = SparkUI2(builder, ipython, spark_server_factory)
+        # then
+        self.assertTrue(spark_ui)
+
+
+class CommMock(Comm):
+
+    def __init__(self):
+        self.message = None
+
+    def send(self, *args, **kwargs):
+        self.message = kwargs["data"]
+
+
+class BuilderMock:
+
+    def __init__(self):
+        pass
+
+    def getOrCreate(self):
+        return SparkSessionMock()
+
+    def config(self, key=None, value=None, conf=None):
+        pass
+
+
+class SparkSessionMock:
+    def __init__(self):
+        pass
+
+    def sparkContext(self):
+        return
+
+
+class SparkContextMock:
+    def __init__(self):
+        pass
+
+
+class IpythonManagerMock:
+
+    def __init__(self):
+        pass
+
+    def configure(self, spark, spark_server):
+        pass
+
+
+class SparkServerFactoryMock:
+
+    def __init__(self):
+        pass
+
+    def run_new_instance(self, spark_context):
+        pass

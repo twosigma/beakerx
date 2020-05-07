76dcc633ee9db98db88eb0b4c09f67dc76db70bb
jaroslawmalekcodete <jaroslaw.malek@codete.com>
2020-05-07 19:42:37 +0200

    jarek/8265: Empty TableDisplay, revert js versions (#8273)
    * #8265: revert js versions
    
    * #8265 upgrade version of typescript and rimraf, move rimraf to dev dependencies
    
    * #8265 remove unused packages @types/mocha, @types/chai, sinon
    
    * #8265 update @jupyter-widgets/base, @jupyter-widgets/controls
    
    * #8265 update @types/node
    
    * #8265 update webpack, webpack-cli, webpack-merge
    
    * #8265 update @types/node again
    
    * #8265 update ts-loader
    
    * #8265 update file-loader
    
    * #8265 update url-loader
    
    * #8265 remove jsdom
    
    * #8265 update css-loader
    
    * #8265 update html-loader
    
    * #8265 update copy-webpack-plugin
    
    * #8265 update fork-ts-checker-webpack-plugin
    
    * #8265 update uglifyjs-webpack-plugin
    
    * #8265 update tsconfig-paths
    
    * #8265 update jquery, @types/jquery, @types/jqueryui
    
    * #8265 update style-loader
    
    * #8265 update @types/d3, d3
    
    * #8265 update flatpickr
    
    * #8265 remove reselect
    
    * #8265 update node-sass, sass-loader
    
    * #8265 update moment, moment-timezone
    
    * #8265 remove @phosphor/datagrid, @phosphor/datastore
    
    * #8265 upgrade @jupyterlab/coreutils, underscore
    
    * #8265 upgrade ts-node
    
    * #8265 update @phosphor/widgets
    
    * #8265 update lab-theme-* dependencies
    
    * #8265 [lab] update @jupyter-widgets/base, @jupyter-widgets/controls, @types/node
    
    * #8265 [lab] update dependencies
    
    * #8265 [beakerx_shared] update dependencies
    
    * #8265 [beakerx_tabledisplay] update @jupyter-widgets/*
    
    * #8265 [beakerx_tabledisplay] update @jupyterlab/coreutils
    
    * #8265 [beakerx_tabledisplay] update webpack*
    
    * #8265 [beakerx_tabledisplay] update webpack related packages
    
    * #8265 [beakerx_tabledisplay] move esm and rimraf to dev dependencies
    
    * #8265 [beakerx_tabledisplay] update rimraf
    
    * #8265 [beakerx_tabledisplay] update @types
    
    * #8265 [beakerx_tabledisplay] update moment-timezone
    
    * #8265 [beakerx_tabledisplay] update underscore, jquery
    
    * #8265 [beakerx_tabledisplay] update katex
    
    * #8265 [beakerx_tabledisplay] update mocha, sinon and related @types
    
    * #8265 [beakerx_tabledisplay] update jsdom
    
    * #8265 [beakerx_tabledisplay] update d3, reselect
    
    * #8265 [beakerx_tabledisplay] update typescript
    
    * #8265 [beakerx_tabledisplay] freeze working @phosphor versions
    
    * #8265 [beakerx_tabledisplay] fix tests
    
    * #8265 beakerx is right now not compatible with jupyterlab@2 so we have to pin jupyterlab version
    
    * #8265 we should build jupyterlab once
    
    * #8265 [docker] pin jupyterlab version; build jupyterlab once
    
    Co-authored-by: Łukasz Pior <piorek82@gmail.com>

--- docker/setup.sh
+++ docker/setup.sh
@@ -18,7 +18,10 @@ source activate beakerx
 (cd beakerx; pip install -r requirements.txt --verbose)
 beakerx install
 beakerx_databrowser install
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-(cd js/lab; jupyter labextension install .)
+jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
+(cd js/lab; jupyter labextension install . --no-build)
+(cd js/lab-theme-dark; jupyter labextension install . --no-build)
+(cd js/lab-theme-light; jupyter labextension install . --no-build)
+jupyter lab build
 
 rm -rf docker .DS_Store .git .gradle .idea jitpack.yml kernel RELEASE.md test .cache .yarn .local logs .ipynb_checkpoints
